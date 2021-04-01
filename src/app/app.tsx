f7dd6e270b9cb7315895612ec61c038fa3bb753e
Andrii Ivaniv <unixvb@gmail.com>
2021-04-01 11:30:09 +0300

    navigation dependencies installed

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,14 @@
+import 'react-native-gesture-handler';
+
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
 import { DemoScreen } from '../screens/demo-screen/demo-screen';
 
 export const App = () => {
-  return <DemoScreen />;
+  return (
+    <NavigationContainer>
+      <DemoScreen />
+    </NavigationContainer>
+  );
 };
