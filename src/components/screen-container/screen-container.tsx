f592d94e13312acec9c334beea99e1c920d7344d
Andrii Ivaniv <unixvb@gmail.com>
2021-04-08 13:31:13 +0300

    navigation back fixed

--- src/components/screen-container/screen-container.tsx
+++ src/components/screen-container/screen-container.tsx
@@ -3,13 +3,16 @@ import { Button, ScrollView, View } from 'react-native';
 
 import { InsetSubstitute } from '../inset-substitute/inset-substitute';
 import { ScreenContainerStyles } from './screen-container.styles';
+import { useNavigation } from '../../navigator/use-navigation.hook';
 
 export const ScreenContainer: FC = ({ children }) => {
+  const { goBack } = useNavigation();
+
   return (
     <View style={ScreenContainerStyles.root}>
       <View style={ScreenContainerStyles.header}>
         <InsetSubstitute />
-        <Button title="Back" onPress={() => void 0} />
+        <Button title="Back" onPress={goBack} />
       </View>
       <ScrollView contentContainerStyle={ScreenContainerStyles.scrollViewContentContainer}>{children}</ScrollView>
     </View>
