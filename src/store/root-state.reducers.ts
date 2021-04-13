a7f917890a7ae96d6f7f428dcc2eba83e4f1a0f0
Andrii Ivaniv <unixvb@gmail.com>
2021-04-13 09:41:58 +0300

    any fix

--- src/store/root-state.reducers.ts
+++ src/store/root-state.reducers.ts
@@ -1,10 +1,13 @@
-import { Reducer, ReducersMapObject } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
+import { Action, AnyAction, Reducer, ReducersMapObject } from 'redux';
 import { on, reducer } from 'ts-action';
 
 import { rootStateResetAction } from './root-state.actions';
-import { combineReducers } from '@reduxjs/toolkit';
 
-export const rootStateReducer = <S>(reducers: ReducersMapObject<S, any>): Reducer<S> => (appState, action) => {
+export const rootStateReducer = <S, A extends Action = AnyAction>(reducers: ReducersMapObject<S, A>): Reducer<S, A> => (
+  appState,
+  action
+) => {
   const rootReducer = reducer(
     appState,
     on(rootStateResetAction, _ => undefined)
