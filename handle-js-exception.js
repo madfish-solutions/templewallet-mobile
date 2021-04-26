7e466a62a1e908032fb3ab3316cce18fb5daa616
Andrii Ivaniv <unixvb@gmail.com>
2021-04-26 12:45:08 +0300

    js error handling added

---
+++ handle-js-exception.js
@@ -0,0 +1,24 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Alert } from 'react-native';
+import { setJSExceptionHandler } from 'react-native-exception-handler';
+
+const allowInDevMode = true;
+
+setJSExceptionHandler((error, isFatal) => {
+  // TODO: add Sentry error capturing
+
+  isFatal &&
+    Alert.alert(
+      'Ooops, something went wrong',
+      'We have reported this to our team! Please restart the app and try again!',
+      [
+        {
+          text: 'Close',
+          onPress: () => {
+            AsyncStorage.clear();
+            // TODO: setup CodePush and call restartApp() here
+          }
+        }
+      ]
+    );
+}, allowInDevMode);
