e5f801728442c4bc861cfc26cfbc072dd0eb9044
Andrii Ivaniv <unixvb@gmail.com>
2021-05-06 16:28:09 +0400

    removing redundant code

--- src/components/styled-numberic-input/styled-numeric-input.tsx
+++ src/components/styled-numberic-input/styled-numeric-input.tsx
@@ -1,5 +1,5 @@
 import { inRange } from 'lodash';
-import React, { FC, useCallback, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 
 import { EventFn } from '../../config/general';
 import { StyledTextInput } from '../styled-text-input/styled-text-input';
@@ -14,26 +14,18 @@ interface Props {
 export const StyledNumericInput: FC<Props> = ({ min = 0, max = Number.MAX_SAFE_INTEGER, value, onChange }) => {
   const [displayedValue, setDisplayedValue] = useState<string>(value.toString());
 
-  const handleChange = useCallback(
-    (changedValue: string) => {
-      const parsedNumber = +changedValue;
+  const handleChange = (changedValue: string) => {
+    const parsedNumber = +changedValue;
 
-      if (!isNaN(parsedNumber) && inRange(parsedNumber, min, max)) {
-        const isFloat = changedValue.includes('.');
+    if (!isNaN(parsedNumber) && inRange(parsedNumber, min, max)) {
+      const isFloat = changedValue.includes('.');
 
-        setDisplayedValue(isFloat ? changedValue : parsedNumber.toString());
-        onChange(parsedNumber);
-      }
-    },
-    [min, max, onChange]
-  );
-
-  useEffect(() => {
-    if (value !== parseFloat(displayedValue)) {
-      setDisplayedValue(value.toString());
-      console.log('displayed value updated');
+      setDisplayedValue(isFloat ? changedValue : parsedNumber.toString());
+      onChange(parsedNumber);
     }
-  }, [value]);
+  };
+
+  useEffect(() => void (value !== parseFloat(displayedValue) && setDisplayedValue(value.toString())), [value]);
 
   return <StyledTextInput keyboardType="numeric" value={displayedValue} onChangeText={handleChange} />;
 };
