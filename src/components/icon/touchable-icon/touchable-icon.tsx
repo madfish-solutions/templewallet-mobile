97a54e85f6013d01f58d6ce50f1209ddaa841752
michaelpautov <michael.pautov@gmail.com>
2021-05-09 15:31:15 +0400

    refactor(touchable-props): removed style prop, it extended

--- src/components/icon/touchable-icon/touchable-icon.tsx
+++ src/components/icon/touchable-icon/touchable-icon.tsx
@@ -1,6 +1,5 @@
 import { TouchableOpacity } from '@gorhom/bottom-sheet';
 import React, { FC } from 'react';
-import { ViewStyle } from 'react-native';
 
 import { EmptyFn } from '../../../config/general';
 import { step } from '../../../config/styles';
@@ -10,7 +9,6 @@ import { TouchableIconStyles } from './touchable-icon.styles';
 interface Props extends IconProps {
   iconSize?: number;
   onPress: EmptyFn;
-  style?: ViewStyle;
 }
 
 export const TouchableIcon: FC<Props> = ({ size = 3 * step, name, iconSize, color, onPress, style }) => (
