e8b786d53bee3e53c4e92affbf07726a94f00e45
Andrii Ivaniv <unixvb@gmail.com>
2021-05-09 14:59:58 +0400

    networks order fixes

--- src/utils/network/networks.ts
+++ src/utils/network/networks.ts
@@ -2,16 +2,6 @@ import { NetworkInterface } from '../../interfaces/network.interface';
 
 export const networks: NetworkInterface[] = [
   {
-    id: 'florencenet',
-    name: 'Florence Testnet',
-    description: 'Florence testnet',
-    lambdaContract: 'KT1BbTmNHmJp2NnQyw5qsAExEYmYuUpR2HdX',
-    type: 'test',
-    rpcBaseURL: 'https://florencenet.smartpy.io',
-    color: '#FFD88A',
-    disabled: false
-  },
-  {
     id: 'mainnet',
     name: 'tezosMainnet',
     nameI18nKey: 'tezosMainnet',
@@ -20,6 +10,16 @@ export const networks: NetworkInterface[] = [
     type: 'main',
     rpcBaseURL: 'https://mainnet-tezos.giganode.io',
     color: '#83b300',
+    disabled: false
+  },
+  {
+    id: 'florencenet',
+    name: 'Florence Testnet',
+    description: 'Florence testnet',
+    lambdaContract: 'KT1BbTmNHmJp2NnQyw5qsAExEYmYuUpR2HdX',
+    type: 'test',
+    rpcBaseURL: 'https://florencenet.smartpy.io',
+    color: '#FFD88A',
     disabled: false
   }
 ];
