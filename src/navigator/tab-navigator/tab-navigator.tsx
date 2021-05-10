7b277e63a2a46383567d4d9e770ce3ff1501b9c0
Andrii Ivaniv <unixvb@gmail.com>
2021-05-10 09:19:38 +0400

    navigation bg fixed

--- src/navigator/tab-navigator/tab-navigator.tsx
+++ src/navigator/tab-navigator/tab-navigator.tsx
@@ -12,45 +12,50 @@ import {
   SwapTabBarLabel,
   WalletTabBarLabel
 } from './tab-bar-options/tab-bar-label';
+import { useTabNavigatorStyles } from './tab-navigator.styles';
 import { WalletStackNavigator } from './wallet-stack-navigator';
 
 const Tab = createBottomTabNavigator<TabScreensParamList>();
 
-export const TabNavigator = () => (
-  <Tab.Navigator>
-    <Tab.Screen
-      name={ScreensEnum.Wallet}
-      options={{
-        tabBarLabel: WalletTabBarLabel,
-        tabBarIcon: WalletTabBarIcon
-      }}
-      component={WalletStackNavigator}
-    />
-    <Tab.Screen
-      name={ScreensEnum.DApps}
-      options={{
-        tabBarLabel: DAppsTabBarLabel,
-        tabBarIcon: DAppsTabBarIcon,
-        tabBarButton: DisabledTabBarButton
-      }}
-      component={emptyComponent}
-    />
-    <Tab.Screen
-      name={ScreensEnum.Swap}
-      options={{
-        tabBarLabel: SwapTabBarLabel,
-        tabBarIcon: SwapTabBarIcon,
-        tabBarButton: DisabledTabBarButton
-      }}
-      component={emptyComponent}
-    />
-    <Tab.Screen
-      name={ScreensEnum.Settings}
-      options={{
-        tabBarLabel: SettingsTabBarLabel,
-        tabBarIcon: SettingsTabBarIcon
-      }}
-      component={SettingsStackNavigator}
-    />
-  </Tab.Navigator>
-);
+export const TabNavigator = () => {
+  const styles = useTabNavigatorStyles();
+
+  return (
+    <Tab.Navigator tabBarOptions={{ style: styles.tabBar }}>
+      <Tab.Screen
+        name={ScreensEnum.Wallet}
+        options={{
+          tabBarLabel: WalletTabBarLabel,
+          tabBarIcon: WalletTabBarIcon
+        }}
+        component={WalletStackNavigator}
+      />
+      <Tab.Screen
+        name={ScreensEnum.DApps}
+        options={{
+          tabBarLabel: DAppsTabBarLabel,
+          tabBarIcon: DAppsTabBarIcon,
+          tabBarButton: DisabledTabBarButton
+        }}
+        component={emptyComponent}
+      />
+      <Tab.Screen
+        name={ScreensEnum.Swap}
+        options={{
+          tabBarLabel: SwapTabBarLabel,
+          tabBarIcon: SwapTabBarIcon,
+          tabBarButton: DisabledTabBarButton
+        }}
+        component={emptyComponent}
+      />
+      <Tab.Screen
+        name={ScreensEnum.Settings}
+        options={{
+          tabBarLabel: SettingsTabBarLabel,
+          tabBarIcon: SettingsTabBarIcon
+        }}
+        component={SettingsStackNavigator}
+      />
+    </Tab.Navigator>
+  );
+};
