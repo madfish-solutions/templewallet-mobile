af47dba9b6d15a2a16094f748a5a74f278dd2aad
michaelpautov <michael.pautov@gmail.com>
2021-05-10 13:28:59 +0400

    fix(typography): changed sizes

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,99 +1,98 @@
-import { step } from '../config/styles';
 import { FontsEnum } from './fonts-enum';
 import { formatSize } from './format-size';
 
 export const typography = {
   numbersRegular22: {
     fontFamily: FontsEnum.rubikRegular,
-    fontSize: step * 5.5
+    fontSize: formatSize(22)
   },
   numbersRegular20: {
     fontFamily: FontsEnum.rubikRegular,
-    fontSize: step * 5
+    fontSize: formatSize(20)
   },
   numbersRegular17: {
     fontFamily: FontsEnum.rubikRegular,
-    fontSize: step * 4.25
+    fontSize: formatSize(17)
   },
   numbersRegular15: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 3.75
+    fontSize: formatSize(15)
   },
   numbersRegular13: {
     fontFamily: FontsEnum.rubikRegular,
-    fontSize: step * 3.25
+    fontSize: formatSize(13)
   },
   numbersRegular11: {
     fontFamily: FontsEnum.rubikRegular,
-    fontSize: step * 2.75
+    fontSize: formatSize(11)
   },
   numbersMedium34: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 8.5
+    fontSize: formatSize(34)
   },
   numbersMedium28: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 7
+    fontSize: formatSize(28)
   },
   numbersMedium22: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 5.5
+    fontSize: formatSize(22)
   },
   numbersMedium20: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 5
+    fontSize: formatSize(20)
   },
   numbersMedium17: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 4.25
+    fontSize: formatSize(17)
   },
   numbersMedium15: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 3.75
+    fontSize: formatSize(15)
   },
   numbersMedium13: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 3.25
+    fontSize: formatSize(13)
   },
   numbersMedium11: {
     fontFamily: FontsEnum.rubikMedium,
-    fontSize: step * 4.25
+    fontSize: formatSize(17)
   },
   numberStatus8: {
     fontFamily: FontsEnum.rubikSemiBold,
-    fontSize: step * 2
+    fontSize: formatSize(8)
   },
   headline1Bold40: {
     fontFamily: FontsEnum.interBold,
-    fontSize: step * 10
+    fontSize: formatSize(40)
   },
   headline2Bold34: {
     fontFamily: FontsEnum.interBold,
-    fontSize: step * 8.5
+    fontSize: formatSize(34)
   },
   headline3Bold28: {
     fontFamily: FontsEnum.interBold,
-    fontSize: step * 28
+    fontSize: formatSize(28)
   },
   headline4Bold22: {
     fontFamily: FontsEnum.interBold,
-    fontSize: step * 5.5
+    fontSize: formatSize(22)
   },
   headline4Regular22: {
     fontFamily: FontsEnum.interRegular,
-    fontSize: step * 5.5
+    fontSize: formatSize(22)
   },
   body20Bold: {
     fontFamily: FontsEnum.interBold,
-    fontSize: step * 5
+    fontSize: formatSize(20)
   },
   body20Regular: {
     fontFamily: FontsEnum.interRegular,
-    fontSize: step * 5
+    fontSize: formatSize(20)
   },
   body17Semibold: {
     fontFamily: FontsEnum.interSemiBold,
-    fontSize: step * 4.25
+    fontSize: formatSize(17)
   },
   body17Regular: {
     fontFamily: FontsEnum.interRegular,
@@ -101,27 +100,27 @@ export const typography = {
   },
   body15Semibold: {
     fontFamily: FontsEnum.interSemiBold,
-    fontSize: step * 3
+    fontSize: formatSize(15)
   },
   body15Regular: {
     fontFamily: FontsEnum.interRegular,
-    fontSize: step * 3
+    fontSize: formatSize(15)
   },
   caption13Regular: {
     fontFamily: FontsEnum.interRegular,
-    fontSize: step * 3.25
+    fontSize: formatSize(13)
   },
   tagline13Tag: {
     fontFamily: FontsEnum.interSemiBold,
-    fontSize: step * 3.25
+    fontSize: formatSize(13)
   },
   caption11Regular: {
     fontFamily: FontsEnum.interRegular,
-    fontSize: step * 2.75
+    fontSize: formatSize(11)
   },
   tagline11Tag: {
     fontFamily: FontsEnum.interSemiBold,
-    fontSize: step * 2.75
+    fontSize: formatSize(11)
   }
 };
 
