2cb695d794fc4c191a0e0d506065f94f4e8a7ac4
Andrii Ivaniv <unixvb@gmail.com>
2021-05-10 21:11:26 +0400

    bottom sheet bg fixed

--- src/components/bottom-sheet/bottom-sheet-background/bottom-sheet-background.tsx
+++ src/components/bottom-sheet/bottom-sheet-background/bottom-sheet-background.tsx
@@ -1,10 +1,10 @@
 import { BottomSheetBackgroundProps } from '@gorhom/bottom-sheet';
-import React from 'react';
+import React, { FC } from 'react';
 import { View } from 'react-native';
 
 import { useBottomSheetBackgroundStyles } from './bottom-sheet-background.styles';
 
-export const BottomSheetBackground = ({ style }: BottomSheetBackgroundProps) => {
+export const BottomSheetBackground: FC<BottomSheetBackgroundProps> = ({ style }) => {
   const styles = useBottomSheetBackgroundStyles();
 
   return <View style={[style, styles.root]} />;
