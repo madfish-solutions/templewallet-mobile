03c1165f401080556a80fae1bbe093365aa2da4b
Michael <michael.pautov@gmail.com>
2021-05-17 16:34:52 +0600

    Style welcome screen (#39)
    * style(welcome): implemented base styles
    
    * style(welcome): styled
    
    * style(welcome): styled
    
    * refactor(comments): resolved
    
    * refactor(comments): resolved
    
    Co-authored-by: Michael Pautov <mpautov@deem.com>

--- src/components/button/button.styles.ts
+++ src/components/button/button.styles.ts
@@ -2,7 +2,6 @@ import { StyleSheet } from 'react-native';
 
 export const ButtonStyles = StyleSheet.create({
   containerStyle: {
-    flex: 1,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center'
--- src/screens/welcome/welcome.tsx
+++ src/screens/welcome/welcome.tsx
@@ -1,31 +1,52 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image, Text, View } from 'react-native';
 
 import { ButtonLargePrimary } from '../../components/button/button-large/button-large-primary/button-large-primary';
 import { ButtonLargeSecondary } from '../../components/button/button-large/button-large-secondary/button-large-secondary';
 import { IconNameEnum } from '../../components/icon/icon-name.enum';
-import { ScreenContainer } from '../../components/screen-container/screen-container';
-import { step } from '../../config/styles';
+import { InsetSubstitute } from '../../components/inset-substitute/inset-substitute';
+import { Label } from '../../components/label/label';
 import { ScreensEnum } from '../../navigator/screens.enum';
 import { useNavigation } from '../../navigator/use-navigation.hook';
+import { formatSize } from '../../styles/format-size';
+import { useWelcomeStyles } from './welcome.styles';
 
 export const Welcome = () => {
   const { navigate } = useNavigation();
+  const styles = useWelcomeStyles();
 
   return (
-    <ScreenContainer>
-      <Text>Welcome</Text>
-      <ButtonLargePrimary
-        title="Create a new Wallet"
-        iconName={IconNameEnum.PlusSquare}
-        marginBottom={step}
-        onPress={() => navigate(ScreensEnum.CreateAccount)}
-      />
-      <ButtonLargeSecondary
-        title="Import existing Wallet"
-        iconName={IconNameEnum.Download}
-        onPress={() => navigate(ScreensEnum.ImportAccount)}
-      />
-    </ScreenContainer>
+    <View style={styles.view}>
+      <View>
+        <InsetSubstitute />
+        <Text style={styles.headerTitle}>
+          <Text style={styles.headerTitleQuotes}>“</Text>The only function of economic forecasting is to make astrology
+          look more respectable.
+          <Text style={styles.headerTitleQuotes}>”</Text>
+        </Text>
+        <Text style={styles.headerSecondTitle}>John Kenneth Galbraith</Text>
+      </View>
+      <View style={styles.imageView}>
+        <Image style={styles.image} source={require('../../assets/temple-logo-with-text.png')} />
+      </View>
+      <View>
+        <ButtonLargePrimary
+          title="Create a new Wallet"
+          iconName={IconNameEnum.PlusSquare}
+          marginBottom={formatSize(8)}
+          onPress={() => navigate(ScreensEnum.CreateAccount)}
+        />
+        <Label description="New to Temple Wallet? Let's set it up! This will create a new wallet and seed phrase" />
+        <ButtonLargeSecondary
+          title="Import existing Wallet"
+          iconName={IconNameEnum.Download}
+          marginTop={formatSize(24)}
+          marginBottom={formatSize(8)}
+          onPress={() => navigate(ScreensEnum.ImportAccount)}
+        />
+        <Label description="Already have a seed phrase? Import your existing wallet using a 12 or more mnemonic words" />
+        <InsetSubstitute type="bottom" />
+      </View>
+    </View>
   );
 };
