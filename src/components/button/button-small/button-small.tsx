71ed85aa75efff06dfb1d5f8c0937ea2b9401c04
Michael Pautov <mpautov@deem.com>
2021-05-18 10:04:19 +0600

    refactor(comments): resolved

--- src/components/button/button-small/button-small.tsx
+++ src/components/button/button-small/button-small.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import { Button } from '../button';
 import { ButtonSharedProps } from '../button-shared.props';
@@ -8,7 +8,7 @@ interface Props extends Omit<ButtonSharedProps, 'iconName'> {
   title: string;
 }
 
-export const ButtonSmall: React.FC<Props> = props => {
+export const ButtonSmall: FC<Props> = props => {
   const styleConfig = useButtonSmallStyles();
 
   return <Button {...props} styleConfig={styleConfig} />;
