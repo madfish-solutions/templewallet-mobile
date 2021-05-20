78e95f8956d36a8fc904b06cc0b044dc982a2e84
Andrii Ivaniv <unixvb@gmail.com>
2021-05-20 17:27:30 +0300

    return shortened

--- src/screens/manage-accounts/manage-imported-accounts/manage-imported-accounts.tsx
+++ src/screens/manage-accounts/manage-imported-accounts/manage-imported-accounts.tsx
@@ -3,11 +3,9 @@ import React from 'react';
 import { DataPlaceholder } from '../../../components/data-placeholder/data-placeholder';
 import { InfoText } from '../info-text/info-text';
 
-export const ManageImportedAccounts = () => {
-  return (
-    <>
-      <InfoText />
-      <DataPlaceholder name="Imported Accounts" />
-    </>
-  );
-};
+export const ManageImportedAccounts = () => (
+  <>
+    <InfoText />
+    <DataPlaceholder name="Imported Accounts" />
+  </>
+);
