fb969316388dd391eb4b52b94ca61020a303102f
Andrii Ivaniv <unixvb@gmail.com>
2021-05-21 15:41:25 +0300

    test fix

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,9 @@
 import 'react-native-gesture-handler/jestSetup';
 
+global.crypto = {
+  getRandomValues: jest.fn()
+};
+
 import './src/mocks/react-native-async-storage.mock';
 import './src/mocks/react-native-bootsplash.mock';
 import './src/mocks/react-native-screens.mock';
--- src/components/styled-text-input/styled-text-input.tsx
+++ src/components/styled-text-input/styled-text-input.tsx
@@ -23,7 +23,6 @@ export const StyledTextInput = forwardRef<TextInput, StyledTextInputProps>(
       isPasswordInput = false,
       value,
       multiline,
-      secureTextEntry,
       ...props
     },
     ref
@@ -40,7 +39,6 @@ export const StyledTextInput = forwardRef<TextInput, StyledTextInputProps>(
             isError && styles.error,
             isPasswordInput && styles.passwordPadding
           ]}
-          secureTextEntry={secureTextEntry}
           multiline={multiline}
           placeholderTextColor={colors.gray3}
           selectionColor={colors.orange}
