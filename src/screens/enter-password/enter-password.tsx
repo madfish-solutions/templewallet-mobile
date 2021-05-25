24bafdd1ea104ecefaeca955ff3cf17a5d3874b7
Inokentii Mazhara <keshan3262@gmail.com>
2021-05-25 18:27:14 +0300

    Remove duplicated input

--- src/screens/enter-password/enter-password.tsx
+++ src/screens/enter-password/enter-password.tsx
@@ -19,7 +19,6 @@ import {
   enterPasswordValidationSchema
 } from './enter-password.form';
 import { useEnterPasswordStyles } from './enter-password.styles';
-import { ButtonsContainer } from '../../components/buttons-container/buttons-container';
 
 export const EnterPassword = () => {
   const styles = useEnterPasswordStyles();
