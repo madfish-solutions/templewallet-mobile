143df0777fd9606fe75aaaa03d5f844f341248d0
Andrii Ivaniv <unixvb@gmail.com>
2021-05-26 12:08:28 +0300

    unused components removed

--- src/components/bottom-sheet/bottom-sheet-background/bottom-sheet-background.styles.ts
+++
@@ -1,9 +0,0 @@
-import { createUseStyles } from '../../../styles/create-use-styles';
-import { formatSize } from '../../../styles/format-size';
-
-export const useBottomSheetBackgroundStyles = createUseStyles(({ colors }) => ({
-  root: {
-    backgroundColor: colors.pageBG,
-    marginTop: formatSize(16)
-  }
-}));
