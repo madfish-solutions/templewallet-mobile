57bf7c2b16b2f27821004781b3c6ac15b5d2d37d
Andrii Ivaniv <unixvb@gmail.com>
2021-05-26 13:45:45 +0300

    checkbox redundant container removed

--- src/components/styled-checkbox/styled-checkbox.tsx
+++ src/components/styled-checkbox/styled-checkbox.tsx
@@ -1,6 +1,5 @@
 import CheckBox, { CheckBoxProps } from '@react-native-community/checkbox';
 import React, { FC } from 'react';
-import { View } from 'react-native';
 
 import { isAndroid, isIOS } from '../../config/system';
 import { formatSize } from '../../styles/format-size';
@@ -14,25 +13,23 @@ export const StyledCheckbox: FC<Props> = ({ size = formatSize(20), ...props }) =
   const colors = useColors();
 
   return (
-    <View>
-      <CheckBox
-        {...props}
-        {...(isIOS && {
-          lineWidth: formatSize(1.5),
-          boxType: 'square',
-          tintColor: colors.orange,
-          animationDuration: 0.15,
-          onCheckColor: colors.orange,
-          onTintColor: colors.orange
-        })}
-        {...(isAndroid && {
-          tintColors: {
-            true: colors.orange,
-            false: colors.orange
-          }
-        })}
-        style={{ width: size, height: size }}
-      />
-    </View>
+    <CheckBox
+      {...props}
+      {...(isIOS && {
+        lineWidth: formatSize(1.5),
+        boxType: 'square',
+        tintColor: colors.orange,
+        animationDuration: 0.15,
+        onCheckColor: colors.orange,
+        onTintColor: colors.orange
+      })}
+      {...(isAndroid && {
+        tintColors: {
+          true: colors.orange,
+          false: colors.orange
+        }
+      })}
+      style={{ width: size, height: size }}
+    />
   );
 };
