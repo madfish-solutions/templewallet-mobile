d2afcd7c0c958ca32f322c7732cebebb15da70bf
Andrii Ivaniv <unixvb@gmail.com>
2021-06-01 15:53:31 +0300

    activity screen placeholder added

--- src/screens/activity/Activity.tsx
+++ src/screens/activity/Activity.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { Text } from 'react-native';
 
+import { DataPlaceholder } from '../../components/data-placeholder/data-placeholder';
 import { ScreenContainer } from '../../components/screen-container/screen-container';
+import { SearchInput } from '../../components/search-input/search-input';
 
 export const Activity = () => {
   return (
     <ScreenContainer>
-      <Text>Hi, boss. Activity</Text>
+      <SearchInput placeholder="Search by address" />
+      <DataPlaceholder text="Activity will be available soon" />
     </ScreenContainer>
   );
 };
