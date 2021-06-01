400575a42a784d42b20d713e4d32ecab29fff27c
Andrii Ivaniv <unixvb@gmail.com>
2021-06-01 15:53:41 +0300

    activity screen placeholder added

--- src/screens/activity/Activity.tsx
+++ src/screens/activity/Activity.tsx
@@ -4,11 +4,9 @@ import { DataPlaceholder } from '../../components/data-placeholder/data-placehol
 import { ScreenContainer } from '../../components/screen-container/screen-container';
 import { SearchInput } from '../../components/search-input/search-input';
 
-export const Activity = () => {
-  return (
-    <ScreenContainer>
-      <SearchInput placeholder="Search by address" />
-      <DataPlaceholder text="Activity will be available soon" />
-    </ScreenContainer>
-  );
-};
+export const Activity = () => (
+  <ScreenContainer>
+    <SearchInput placeholder="Search by address" />
+    <DataPlaceholder text="Activity will be available soon" />
+  </ScreenContainer>
+);
