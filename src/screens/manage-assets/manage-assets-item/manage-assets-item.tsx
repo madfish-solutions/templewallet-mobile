65026a31830bac4fe5b6aad15eec4b430b131daa
Andrii Ivaniv <unixvb@gmail.com>
2021-06-02 15:16:47 +0300

    temple switch created

--- src/screens/manage-assets/manage-assets-item/manage-assets-item.tsx
+++ src/screens/manage-assets/manage-assets-item/manage-assets-item.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
-import { Switch } from 'react-native';
 
 import { Divider } from '../../../components/divider/divider';
 import { IconNameEnum } from '../../../components/icon/icon-name.enum';
 import { TouchableIcon } from '../../../components/icon/touchable-icon/touchable-icon';
+import { Switch } from '../../../components/switch/switch';
 import { TokenContainer } from '../../../components/token-container/token-container';
 import { formatSize } from '../../../styles/format-size';
 import { TokenInterface } from '../../../token/interfaces/token.interface';
@@ -17,7 +17,7 @@ export const ManageAssetsItem: FC<Props> = ({ token }) => {
     <TokenContainer symbol={token.symbol} name={token.name} iconName={token.iconName}>
       <TouchableIcon name={IconNameEnum.Trash} size={formatSize(16)} onPress={() => null} />
       <Divider size={formatSize(16)} />
-      <Switch value={token.isShown} />
+      <Switch value={token.isShown} disabled={true} />
     </TokenContainer>
   );
 };
