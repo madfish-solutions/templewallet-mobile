c88c17f6fe7622072e03e2d4a4d57f585e4242fc
Andrii Ivaniv <unixvb@gmail.com>
2021-06-08 11:57:36 +0300

    qr scanner removed from token header

--- src/components/header/header-token-info/header-token-info.tsx
+++ src/components/header/header-token-info/header-token-info.tsx
@@ -3,6 +3,7 @@ import { Text, View } from 'react-native';
 
 import { formatSize } from '../../../styles/format-size';
 import { TokenMetadataInterface } from '../../../token/interfaces/token-metadata.interface';
+import { isString } from '../../../utils/is-string';
 import { Icon } from '../../icon/icon';
 import { IconNameEnum } from '../../icon/icon-name.enum';
 import { useHeaderTokenInfoStyles } from './header-token-info.styles';
@@ -14,12 +15,13 @@ interface Props {
 export const HeaderTokenInfo: FC<Props> = ({ token }) => {
   const styles = useHeaderTokenInfoStyles();
 
-  const { name, iconName = IconNameEnum.NoNameToken } = token;
+  const { name, symbol, iconName = IconNameEnum.NoNameToken } = token;
+  const title = isString(name) ? name : symbol;
 
   return (
     <View style={styles.container}>
       <Icon name={iconName} size={formatSize(32)} />
-      <Text style={styles.text}>{name}</Text>
+      <Text style={styles.text}>{title}</Text>
     </View>
   );
 };
