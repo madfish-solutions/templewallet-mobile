110729542ab7d817a7c0ee2f790c5139b7ec66c6
Inokentii Mazhara <keshan3262@gmail.com>
2021-06-11 19:00:09 +0300

    Add a hook for inner screen progress

--- src/screens/import-account/import-account.tsx
+++ src/screens/import-account/import-account.tsx
@@ -1,21 +1,13 @@
 import React, { useState } from 'react';
 
-import { HeaderProgress } from '../../components/header/header-progress/header-progress';
-import { useNavigationSetOptions } from '../../components/header/use-navigation-set-options.hook';
+import { useInnerScreenProgress } from '../../hooks/use-inner-screen-progress';
 import { CreateNewPassword } from './create-new-password/create-new-password';
 import { ImportWallet } from './import-wallet/import-wallet';
 import { ImportWalletFormValues } from './import-wallet/import-wallet.form';
 
 export const ImportAccount = () => {
-  const [innerScreenIndex, setInnerScreenIndex] = useState(0);
+  const { innerScreenIndex, setInnerScreenIndex } = useInnerScreenProgress(2);
   const [seedPhrase, setSeedPhrase] = useState('');
-
-  useNavigationSetOptions(
-    {
-      headerRight: () => <HeaderProgress current={innerScreenIndex + 1} total={2} />
-    },
-    [innerScreenIndex]
-  );
 
   const handleImportWalletFormSubmit = ({ seedPhrase: newSeedPhrase }: ImportWalletFormValues) => {
     setSeedPhrase(newSeedPhrase);
