d71a49cd19ac127101c79a049ba0393214b4847a
Andrii Ivaniv <unixvb@gmail.com>
2021-06-24 19:04:09 +0300

    touched validation fix dividers added

--- src/form/error-message/error-message.tsx
+++ src/form/error-message/error-message.tsx
@@ -13,7 +13,5 @@ export const ErrorMessage = <T extends unknown>({ meta }: Props<T>) => {
   const isError = hasError(meta);
   const error = (meta.touched && meta.error) || ' ';
 
-  console.log(meta.touched, meta.error !== undefined);
-
   return <Text style={[ErrorMessageStyles.root, isError && ErrorMessageStyles.rootVisible]}>{error}</Text>;
 };
