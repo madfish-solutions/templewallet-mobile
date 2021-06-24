890c0d25b1ec24bf0f6223d898c12c76f189baa4
Andrii Ivaniv <unixvb@gmail.com>
2021-06-24 18:42:59 +0300

    apy & animation fixes applied

--- src/config/general.ts
+++ src/config/general.ts
@@ -2,3 +2,5 @@ export type EventFn<T> = (event: T) => void;
 export type EmptyFn = () => void;
 export const emptyFn = () => void 0;
 export const emptyComponent = () => null;
+
+export const delegationApy = 6;
