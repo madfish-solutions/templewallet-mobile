5bfb7d48fc80653bcc3237503b8f25faec087b87
Andrii Ivaniv <unixvb@users.noreply.github.com>
2021-07-06 13:33:31 +0300

    Sentry setup (#97)
    * sentry added
    
    * log removed
    
    * jest fix

--- handle-js-exception.js
+++ handle-js-exception.js
@@ -1,11 +1,12 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as Sentry from '@sentry/react-native';
 import { Alert } from 'react-native';
 import { setJSExceptionHandler } from 'react-native-exception-handler';
 
 const allowInDevMode = false;
 
 setJSExceptionHandler((error, isFatal) => {
-  // TODO: add Sentry error capturing
+  Sentry.captureException(error);
 
   isFatal &&
     Alert.alert(
--- shim.js
+++ shim.js
@@ -16,7 +16,9 @@ if (!global.atob) {
 }
 
 if (!global.document) {
-  global.document = {};
+  global.document = {
+    addEventListener: () => null
+  };
 }
 
 if (typeof __dirname === 'undefined') {
