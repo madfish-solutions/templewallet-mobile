e15d42268cebc381206c473073d1c1c8839b29a9
Serhii Pashchenko <11996139+serh11p@users.noreply.github.com>
2021-07-06 13:30:00 +0300

    Setup well tezos toolkit (#98)

--- src/utils/network/network.util.ts
+++ src/utils/network/network.util.ts
@@ -1,10 +1,22 @@
-import { TezosToolkit } from '@taquito/taquito';
+import { TezosToolkit, MichelCodecPacker, CompositeForger, RpcForger } from '@taquito/taquito';
+import { localForger } from '@taquito/local-forging';
+import memoize from 'mem';
 import { BehaviorSubject } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { MAINNET_NETWORK } from './networks';
+import { FastRpcClient } from './fast-rpc';
 
-export const createTezosToolkit = (rpc: string) => new TezosToolkit(rpc);
+const michelEncoder = new MichelCodecPacker();
+
+export const createTezosToolkit = (rpc: string) => {
+  const tezos = new TezosToolkit(getFastRpcClient(rpc));
+  tezos.setPackerProvider(michelEncoder);
+  tezos.setForgerProvider(new CompositeForger([tezos.getFactory(RpcForger)(), localForger]));
+  return tezos;
+};
+
+export const getFastRpcClient = memoize((rpc: string) => new FastRpcClient(rpc));
 
 const currentNetwork$ = new BehaviorSubject(MAINNET_NETWORK);
 export const currentNetworkId$ = currentNetwork$.pipe(map(({ id }) => id));
