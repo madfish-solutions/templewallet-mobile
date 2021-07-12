7d8e8a2006dcaaf0423987aba504a82d20c822eb
Andrii Ivaniv <unixvb@gmail.com>
2021-07-12 15:49:28 +0300

    useActiveAppEffect hook updated

--- src/biometry/biometry-availability.provider.tsx
+++ src/biometry/biometry-availability.provider.tsx
@@ -1,8 +1,8 @@
 import { noop } from 'lodash';
-import React, { FC, useEffect, useRef, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import ReactNativeBiometrics, { BiometryType } from 'react-native-biometrics';
 
-import { useAppStateVisible } from '../hooks/use-app-state-visible.hook';
+import { useActiveAppEffect } from '../hooks/use-active-app-effect.hook';
 import { BiometryAvailabilityContext } from './biometry-availability.context';
 
 const NO_HARDWARE_ERROR = 'BIOMETRIC_ERROR_NO_HARDWARE';
@@ -10,9 +10,6 @@ const NO_HARDWARE_ERROR = 'BIOMETRIC_ERROR_NO_HARDWARE';
 export const BiometryAvailabilityProvider: FC = ({ children }) => {
   const [isHardwareAvailable, setIsHardwareAvailable] = useState(false);
   const [biometryType, setBiometryType] = useState<BiometryType>();
-
-  const appState = useAppStateVisible();
-  const prevAppStateRef = useRef(appState);
 
   const updateBiometryAvailability = () => {
     ReactNativeBiometrics.isSensorAvailable()
@@ -23,15 +20,8 @@ export const BiometryAvailabilityProvider: FC = ({ children }) => {
       .catch(noop);
   };
 
-  useEffect(() => updateBiometryAvailability(), []);
-
-  useEffect(() => {
-    const prevAppState = prevAppStateRef.current;
-    if (prevAppState.match(/inactive|background/) && appState === 'active') {
-      updateBiometryAvailability();
-    }
-    prevAppStateRef.current = appState;
-  }, [appState]);
+  useEffect(updateBiometryAvailability, []);
+  useActiveAppEffect(updateBiometryAvailability);
 
   return (
     <BiometryAvailabilityContext.Provider value={{ isHardwareAvailable, biometryType }}>
