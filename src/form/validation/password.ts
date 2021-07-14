87b9ac40481a3be149660967a5692208d8aacfad
Inokentii Mazhara <keshan3262@gmail.com>
2021-07-14 15:13:48 +0300

    Change text of an error

--- src/form/validation/password.ts
+++ src/form/validation/password.ts
@@ -6,7 +6,7 @@ const atLeastOneLowerCaseLetterError = 'At least 1 lowercase letter';
 const atLeastOneUpperCaseLetterError = 'At least 1 uppercase letter';
 const atLeastOneNumberError = 'At least 1 number';
 const minError = 'At least 8 characters';
-const onlyAsciiCharsError = 'Only Latin letters are allowed';
+const onlyAsciiCharsError = "Non-Latin letters aren't allowed";
 
 const PASSWORD_PATTERN_ONE_LOWER_CASE = new RegExp('(?=.*[a-z])');
 const PASSWORD_PATTERN_ONE_UPPER_CASE = new RegExp('(?=.*[A-Z])');
