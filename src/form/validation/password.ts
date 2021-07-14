f26606dac9acdf90c76011ed433a2988ee1132cf
Inokentii Mazhara <keshan3262@gmail.com>
2021-07-14 15:04:00 +0300

    Fix 'password' field errors priority

--- src/form/validation/password.ts
+++ src/form/validation/password.ts
@@ -1,5 +1,7 @@
 import { ref, string } from 'yup';
 
+import { requiredErrorMessage } from './messages';
+
 const atLeastOneLowerCaseLetterError = 'At least 1 lowercase letter';
 const atLeastOneUpperCaseLetterError = 'At least 1 uppercase letter';
 const atLeastOneNumberError = 'At least 1 number';
@@ -10,14 +12,14 @@ const PASSWORD_PATTERN_ONE_UPPER_CASE = new RegExp('(?=.*[A-Z])');
 const PASSWORD_PATTERN_ONE_NUMERIC = new RegExp('(?=.*[0-9])');
 
 export const passwordValidation = string()
-  .required()
+  .required(requiredErrorMessage)
+  .min(8, minError)
   .matches(PASSWORD_PATTERN_ONE_LOWER_CASE, atLeastOneLowerCaseLetterError)
   .matches(PASSWORD_PATTERN_ONE_UPPER_CASE, atLeastOneUpperCaseLetterError)
-  .matches(PASSWORD_PATTERN_ONE_NUMERIC, atLeastOneNumberError)
-  .min(8, minError);
+  .matches(PASSWORD_PATTERN_ONE_NUMERIC, atLeastOneNumberError);
 
 const repeatPasswordError = 'Must be equal to password above';
 
 export const passwordConfirmationValidation = string()
-  .required()
+  .required('Required')
   .oneOf([ref('password')], repeatPasswordError);
