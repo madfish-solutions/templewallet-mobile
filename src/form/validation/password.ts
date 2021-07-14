0f91231be07cb1c4fa2116de2ec4adf1292ec35b
Inokentii Mazhara <keshan3262@gmail.com>
2021-07-14 15:13:07 +0300

    Invalidate password if it contains non-Latin characters

--- src/form/validation/password.ts
+++ src/form/validation/password.ts
@@ -6,17 +6,20 @@ const atLeastOneLowerCaseLetterError = 'At least 1 lowercase letter';
 const atLeastOneUpperCaseLetterError = 'At least 1 uppercase letter';
 const atLeastOneNumberError = 'At least 1 number';
 const minError = 'At least 8 characters';
+const onlyAsciiCharsError = 'Only Latin letters are allowed';
 
 const PASSWORD_PATTERN_ONE_LOWER_CASE = new RegExp('(?=.*[a-z])');
 const PASSWORD_PATTERN_ONE_UPPER_CASE = new RegExp('(?=.*[A-Z])');
 const PASSWORD_PATTERN_ONE_NUMERIC = new RegExp('(?=.*[0-9])');
+const PASSWORD_PATTERN_ONLY_ASCII_CHARS = new RegExp('^[\u0021-\u007e]*$');
 
 export const passwordValidation = string()
   .required(requiredErrorMessage)
   .min(8, minError)
   .matches(PASSWORD_PATTERN_ONE_LOWER_CASE, atLeastOneLowerCaseLetterError)
   .matches(PASSWORD_PATTERN_ONE_UPPER_CASE, atLeastOneUpperCaseLetterError)
-  .matches(PASSWORD_PATTERN_ONE_NUMERIC, atLeastOneNumberError);
+  .matches(PASSWORD_PATTERN_ONE_NUMERIC, atLeastOneNumberError)
+  .matches(PASSWORD_PATTERN_ONLY_ASCII_CHARS, onlyAsciiCharsError);
 
 const repeatPasswordError = 'Must be equal to password above';
 
