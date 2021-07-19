58eedf3958395504403dc44b5d1fa8c59e42ad54
Inokentii Mazhara <keshan3262@gmail.com>
2021-07-19 14:14:19 +0300

    Make zones left and right to toast clickable

--- src/toast/toast-provider.styles.ts
+++ src/toast/toast-provider.styles.ts
@@ -1,9 +1,12 @@
 import { StyleSheet } from 'react-native';
 
 import { zIndexEnum } from '../enums/z-index.enum';
+import { formatSize } from '../styles/format-size';
 
 export const ToastProviderStyles = StyleSheet.create({
   toast: {
-    zIndex: zIndexEnum.Toast
+    zIndex: zIndexEnum.Toast,
+    left: formatSize(64),
+    right: formatSize(64)
   }
 });
