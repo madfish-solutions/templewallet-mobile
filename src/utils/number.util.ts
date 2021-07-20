2feb0593e2bbc3417263c0d4ed993e58b6706b48
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-07-20 14:46:24 +0300

    fixed issue with decimals

--- src/utils/number.util.ts
+++ src/utils/number.util.ts
@@ -5,7 +5,7 @@ import { isDefined } from './is-defined';
 export const formatAssetAmount = (
   amount: BigNumber,
   roundingMode: BigNumber.RoundingMode = BigNumber.ROUND_DOWN,
-  decimalPlace = 2
+  decimalPlace = 6
 ) => {
   return amount.decimalPlaces(amount.abs().lt(1000) ? decimalPlace : 2, roundingMode).toFixed();
 };
