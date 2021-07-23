c84361a81022f7e71d80e111b8e6568cad2c4669
Inokentii Mazhara <keshan3262@gmail.com>
2021-07-23 16:22:30 +0300

    Update TODO points

--- src/utils/i18n.utils.ts
+++ src/utils/i18n.utils.ts
@@ -1,4 +1,4 @@
-// TODO: replace all this functionality as soon as i18n that requires using hooks or store appears
+// TODO: replace all this functionality with i18n module
 export const makeRequiredErrorMessage = (fieldName: string) => `"${fieldName}" is a required field`;
 
 const ordinalFormatter = new Intl.PluralRules('en-US', {
