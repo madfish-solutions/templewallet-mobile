ba96a9496fd79eaf391289e1dbec693d41589aac
Innokentiy Mazhara <keshan3262@gmail.com>
2021-07-23 16:31:21 +0300

    Update create-account.tsx

--- src/screens/create-account/create-account.tsx
+++ src/screens/create-account/create-account.tsx
@@ -20,7 +20,7 @@ export const CreateAccount = () => {
   return (
     <>
       {innerScreenIndex === 0 && (
-        <CreateNewWallet onSubmit={handleImportWalletFormSubmit} initialSeedPhrase={seedPhrase} />
+        <CreateNewWallet initialSeedPhrase={seedPhrase} onSubmit={handleImportWalletFormSubmit} />
       )}
       {innerScreenIndex === 1 && (
         <VerifySeedPhrase
