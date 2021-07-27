867e181755ef6e4a408677a765da7a7bc0e5e205
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-07-27 17:40:55 +0300

    fixed jest error

--- src/mocks/react-native-quick-actions.mock.js
+++ src/mocks/react-native-quick-actions.mock.js
@@ -1,9 +1,6 @@
-jest.mock('react-native-quick-actions', () => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const QuickActions = require('react-native-quick-actions');
-
-  QuickActions.popInitialAction = jest.fn(() => Promise.resolve());
-  QuickActions.setShortcutItems = jest.fn();
-  QuickActions.clearShortcutItems = jest.fn();
-  QuickActions.isSupported = jest.fn();
-});
+jest.mock('react-native-quick-actions', () => ({
+  popInitialAction: jest.fn(() => Promise.resolve()),
+  setShortcutItems: jest.fn(),
+  clearShortcutItems: jest.fn(),
+  isSupported: jest.fn()
+}));
