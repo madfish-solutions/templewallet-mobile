0beaa841e3bc91516f6e7e1f801650c24f0187ff
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-07-28 13:21:13 +0300

    fixed error with tab bar

--- src/navigator/tab-bar/tab-bar.styles.ts
+++ src/navigator/tab-bar/tab-bar.styles.ts
@@ -1,12 +1,9 @@
-import { StyleSheet } from 'react-native';
-
-import { zIndexEnum } from '../../enums/z-index.enum';
 import { createUseStyles } from '../../styles/create-use-styles';
 import { formatSize } from '../../styles/format-size';
 
 export const useTabBarStyles = createUseStyles(({ colors }) => ({
   container: {
-    backgroundColor: colors.navigation,
+    backgroundColor: colors.navigation
   },
   buttonsContainer: {
     flexDirection: 'row',
