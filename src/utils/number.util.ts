036ee55c1b435d5abd9628d2da50dac58bb9c19d
Andrew Kryshtal <andrew.kryshtal@gmail.com>
2021-08-04 15:20:30 +0300

    fixed issue (#176)
    * fixed issue
    
    * fixed lint errors

--- src/utils/number.util.ts
+++ src/utils/number.util.ts
@@ -7,3 +7,6 @@ export const formatAssetAmount = (
 ) => amount.decimalPlaces(amount.abs().lt(1000) ? decimalPlace : 2, roundingMode).toFixed();
 
 export const invertSign = (amount: string) => new BigNumber(amount).times(-1).toString();
+
+export const kFormatter = (num: number): string =>
+  (Math.sign(num) * Math.round(Math.abs(num) / 1000)).toLocaleString() + ' K';
