81ccdc065f601f8cd72b1383b55278d29122e97b
Inokentii Mazhara <keshan3262@gmail.com>
2021-08-10 13:58:02 +0300

    Refactor tests

--- src/utils/activity.spec.ts
+++ src/utils/activity.spec.ts
@@ -50,78 +50,57 @@ describe('activity', () => {
       }
     }
   ];
-  const transfers: ActivityInterface[] = [
-    {
-      type: ActivityTypeEnum.Transaction,
-      status: ActivityStatusEnum.Pending,
-      hash: 'hash2',
-      amount: '1',
-      timestamp: 9,
-      entrypoint: '',
-      source: mockMember,
-      destination: {
-        address: 'address4',
-        alias: 'alias4'
-      }
-    },
-    {
-      type: ActivityTypeEnum.Transaction,
-      status: ActivityStatusEnum.Applied,
-      hash: 'hash4',
-      amount: '0',
-      timestamp: 8,
-      entrypoint: 'transfer',
-      source: {
-        address: 'address4',
-        alias: 'alias4'
-      },
-      destination: mockMember
-    }
-  ];
-  const activities = groupActivitiesByHash(operations, transfers);
 
   it('should sort operations and transfers by timestamp in descending order', () => {
-    const flatActivities = activities.flat();
-    expect(
-      flatActivities.reduce((previousValue, activity, index) => {
-        if (index === 0) {
-          return true;
-        }
-        if (!previousValue) {
-          return previousValue;
-        }
+    const transfers: ActivityInterface[] = [
+      {
+        type: ActivityTypeEnum.Transaction,
+        status: ActivityStatusEnum.Applied,
+        hash: 'hash4',
+        amount: '0',
+        timestamp: 8,
+        entrypoint: 'transfer',
+        source: {
+          address: 'address4',
+          alias: 'alias4'
+        },
+        destination: mockMember
+      }
+    ];
 
-        return activity.timestamp <= flatActivities[index - 1].timestamp;
-      }, true)
-    ).toEqual(true);
+    expect(groupActivitiesByHash(operations, transfers)).toMatchSnapshot();
   });
 
-  it('should push operations and transfers with the same hash into one group', () => {
-    const result = groupActivitiesByHash(operations, transfers);
-    const alreadyMetHashes: string[] = [];
-    expect(
-      result.reduce((previousValue, group) => {
-        if (!previousValue) {
-          return previousValue;
+  it('should push operations and transfers with the same hash into one group and not mutate activities', () => {
+    const transfers: ActivityInterface[] = [
+      {
+        type: ActivityTypeEnum.Transaction,
+        status: ActivityStatusEnum.Pending,
+        hash: 'hash2',
+        amount: '1',
+        timestamp: 9,
+        entrypoint: '',
+        source: mockMember,
+        destination: {
+          address: 'address4',
+          alias: 'alias4'
         }
-        const groupHash = group[0].hash;
-        const groupHasSameHashes = group.every(({ hash }) => hash === groupHash);
-        const groupIsUnique = !alreadyMetHashes.includes(groupHash);
-        alreadyMetHashes.push(groupHash);
+      },
+      {
+        type: ActivityTypeEnum.Transaction,
+        status: ActivityStatusEnum.Applied,
+        hash: 'hash4',
+        amount: '0',
+        timestamp: 8,
+        entrypoint: 'transfer',
+        source: {
+          address: 'address4',
+          alias: 'alias4'
+        },
+        destination: mockMember
+      }
+    ];
 
-        return groupHasSameHashes && groupIsUnique;
-      }, true)
-    ).toEqual(true);
-  });
-
-  it('should keep activities props intact', () => {
-    const flatActivities = activities.flat();
-    expect(flatActivities.sort((a, b) => b.timestamp - a.timestamp)).toEqual([
-      operations[1],
-      transfers[0],
-      transfers[1],
-      operations[2],
-      operations[0]
-    ]);
+    expect(groupActivitiesByHash(operations, transfers)).toMatchSnapshot();
   });
 });
