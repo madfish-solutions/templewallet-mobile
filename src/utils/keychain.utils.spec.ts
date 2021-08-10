481d48b6079fe1e20c61dd3cbcd4a0ff30171e9c
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-08-10 12:56:25 +0300

    added case with empty string

--- src/utils/keychain.utils.spec.ts
+++ src/utils/keychain.utils.spec.ts
@@ -4,4 +4,9 @@ describe('getKeychainOptions', () => {
   it('should return object if we passing non-empty string', () => {
     expect(getKeychainOptions('test')).toEqual({ service: `${APP_IDENTIFIER}/test` });
   });
+
+  it('should return object if we passing empty string', () => {
+    expect(getKeychainOptions('')).toEqual({ service: `${APP_IDENTIFIER}/` });
+  });
+
 });
