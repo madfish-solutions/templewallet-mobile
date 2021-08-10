04f480e0aa5dea5646dae0ec966c7416f885c043
Andrii Ivaniv <unixvb@gmail.com>
2021-08-10 13:30:05 +0300

    hook test library setup

---
+++ src/hooks/use-quick-actions.hook.spec.ts
@@ -0,0 +1,13 @@
+describe('useQuickActions', () => {
+  it('should set shortcut items on start', () => {
+  });
+
+  it('should add quick action shortcut listener on start', () => {
+  });
+
+  it('should clear shortcut items on destroy', () => {
+  });
+
+  it('should remove quick action shortcut listener on destroy', () => {
+  });
+});
