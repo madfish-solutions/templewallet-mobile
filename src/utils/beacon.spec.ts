eb7836fb98b37b46d2544d7caf26f5de8c5ef0cb
Inokentii Mazhara <keshan3262@gmail.com>
2021-08-10 16:31:29 +0300

    Create a separate file for mock data

--- src/utils/beacon.spec.ts
+++ src/utils/beacon.spec.ts
@@ -1,99 +1,20 @@
-import { TezosOperationType } from '@airgap/beacon-sdk';
+import {
+  mockOriginationOperation,
+  mockOtherTypesOperations,
+  mockTransactionOperation
+} from '../types/semi-partial-tezos-operation.mock';
+import { mapBeaconToTaquitoParams } from './beacon.utils';
 
-import { mapBeaconToTaquitoParams, SemiPartialTezosOperation } from './beacon.utils';
-
-/* eslint-disable @typescript-eslint/no-explicit-any */
 describe('mapBeaconToTaquitoParams', () => {
   it('should map origination params correctly', () => {
-    expect(
-      mapBeaconToTaquitoParams({
-        kind: TezosOperationType.ORIGINATION,
-        balance: '0',
-        script: { code: 'mockCode', storage: 'mockStorage' } as any,
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3',
-        source: 'account1',
-        counter: '100000'
-      })
-    ).toMatchSnapshot();
+    expect(mapBeaconToTaquitoParams(mockOriginationOperation)).toMatchSnapshot();
   });
 
   it('should map transaction params correctly', () => {
-    expect(
-      mapBeaconToTaquitoParams({
-        kind: TezosOperationType.TRANSACTION,
-        amount: '0',
-        destination: 'account1',
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3',
-        source: 'account1',
-        counter: '100000',
-        parameters: {
-          entrypoint: 'mockEntrypoint',
-          value: { int: '3' }
-        }
-      })
-    ).toMatchSnapshot();
+    expect(mapBeaconToTaquitoParams(mockTransactionOperation)).toMatchSnapshot();
   });
 
   it('should map params of other kinds correctly', () => {
-    const otherTypesTransactions: SemiPartialTezosOperation[] = [
-      {
-        kind: TezosOperationType.ACTIVATE_ACCOUNT,
-        pkh: 'account1',
-        secret: 'mockSecret',
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3'
-      },
-      {
-        kind: TezosOperationType.BALLOT,
-        source: 'account1',
-        period: '4',
-        proposal: 'mockProposal',
-        ballot: 'yay',
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3'
-      },
-      {
-        kind: TezosOperationType.DELEGATION,
-        delegate: 'mockDelegate',
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3'
-      },
-      {
-        kind: TezosOperationType.ENDORSEMENT,
-        level: '666',
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3'
-      },
-      {
-        kind: TezosOperationType.PROPOSALS,
-        period: '4',
-        proposals: ['mockProposal'],
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3'
-      },
-      {
-        kind: TezosOperationType.REVEAL,
-        public_key: 'mockPublicKey',
-        fee: '1',
-        gas_limit: '2',
-        storage_limit: '3'
-      },
-      {
-        kind: TezosOperationType.SEED_NONCE_REVELATION,
-        level: '666',
-        nonce: '667'
-      }
-    ];
-
-    expect(otherTypesTransactions.map(mapBeaconToTaquitoParams)).toMatchSnapshot();
+    expect(mockOtherTypesOperations.map(mapBeaconToTaquitoParams)).toMatchSnapshot();
   });
 });
