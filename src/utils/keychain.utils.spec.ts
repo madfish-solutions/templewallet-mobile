88fe6a45931680c2340e88463b2eeaef3bb47f18
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-08-10 13:05:30 +0300

    fixed comments

--- src/utils/keychain.utils.spec.ts
+++ src/utils/keychain.utils.spec.ts
@@ -1,11 +1,11 @@
-import { APP_IDENTIFIER, getKeychainOptions } from './keychain.utils';
+import { getKeychainOptions } from './keychain.utils';
 
 describe('getKeychainOptions', () => {
   it('should return object if we passing non-empty string', () => {
-    expect(getKeychainOptions('test')).toEqual({ service: `${APP_IDENTIFIER}/test` });
+    expect(getKeychainOptions('test')).toEqual({ service: 'com.madfish-solutions.temple-mobile/test' });
   });
 
   it('should return object if we passing empty string', () => {
-    expect(getKeychainOptions('')).toEqual({ service: `${APP_IDENTIFIER}/` });
+    expect(getKeychainOptions('')).toEqual({ service: 'com.madfish-solutions.temple-mobile/' });
   });
 });
