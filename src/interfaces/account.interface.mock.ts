5b6d3979fb5f68dcb74a6af915c808c6a0b78426
Andrii Ivaniv <unixvb@gmail.com>
2021-08-11 15:02:49 +0300

    mockAccountCredentials data reused

--- src/interfaces/account.interface.mock.ts
+++ src/interfaces/account.interface.mock.ts
@@ -1,9 +1,10 @@
 import { AccountTypeEnum } from '../enums/account-type.enum';
+import { mockAccountCredentials } from '../mocks/account-credentials.mock';
 import { AccountInterface } from './account.interface';
 
 export const mockHdAccount: AccountInterface = {
   name: 'Mock HD Account',
-  publicKey: 'mockHdAccountPublicKey',
   type: AccountTypeEnum.HD_ACCOUNT,
-  publicKeyHash: 'mockHdAccountPublicKeyHash'
+  publicKey: mockAccountCredentials.publicKey,
+  publicKeyHash: mockAccountCredentials.publicKeyHash
 };
