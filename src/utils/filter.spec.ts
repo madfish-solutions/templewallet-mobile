7ab21ee2d814af22116734c10b3748567861a864
Innokentiy Mazhara <keshan3262@gmail.com>
2021-08-11 12:53:19 +0300

    Add tests for filter.spec.ts (#207)

---
+++ src/utils/filter.spec.ts
@@ -0,0 +1,27 @@
+import { filterTezos } from './filter.util';
+
+describe('filterTezos', () => {
+  it('should return false if zero balances should be hidden and tezos balance is zero', () => {
+    expect(filterTezos('0', true)).toEqual(false);
+  });
+
+  it('should return true if zero balances should be hidden, tezos balance is not zero and search value is not specified', () => {
+    expect(filterTezos('1', true)).toEqual(true);
+  });
+
+  it('should return true if zero balances should not be hidden, tezos balance is zero and search value is not specified', () => {
+    expect(filterTezos('0', false)).toEqual(true);
+  });
+
+  it('should return true if tezos name includes search value (case-ignoring comparison)', () => {
+    expect(filterTezos('1', false, 'OS')).toEqual(true);
+  });
+
+  it('should return true if tezos symbol includes search value', () => {
+    expect(filterTezos('1', false, 'tez')).toEqual(true);
+  });
+
+  it('should return false if neither tezos symbol nor name include search value', () => {
+    expect(filterTezos('1', false, 'foo')).toEqual(false);
+  });
+});
