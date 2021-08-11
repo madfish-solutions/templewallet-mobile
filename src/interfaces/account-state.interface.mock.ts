4e5927ae99b06406cfeae8b27e493cc38b12e3fa
Andrii Ivaniv <unixvb@gmail.com>
2021-08-11 13:25:10 +0300

    pending activity groups mock added

--- src/interfaces/account-state.interface.mock.ts
+++ src/interfaces/account-state.interface.mock.ts
@@ -1,11 +1,11 @@
 import { createEntity } from '../store/create-entity';
 import { mockAccountTokens } from '../token/interfaces/account-token.interface.mock';
 import { AccountStateInterface } from './account-state.interface';
-import { mockActivityGroup } from './activity.interface.mock';
+import { mockAppliedActivityGroups, mockPendingActivityGroups } from './activity.interface.mock';
 
 export const mockAccountState: AccountStateInterface = {
   tezosBalance: createEntity('100'),
   tokensList: mockAccountTokens,
-  activityGroups: createEntity(mockActivityGroup),
-  pendingActivities: []
+  activityGroups: createEntity(mockAppliedActivityGroups),
+  pendingActivities: mockPendingActivityGroups
 };
--- src/interfaces/activity.interface.mock.ts
+++ src/interfaces/activity.interface.mock.ts
@@ -8,77 +8,91 @@ const mockMember = {
   alias: 'alias2'
 };
 
-export const mockOperations: ActivityInterface[] = [
-  {
-    type: ActivityTypeEnum.Transaction,
-    status: ActivityStatusEnum.Applied,
-    hash: 'hash1',
-    amount: '0',
+const mockAppliedOperation: ActivityInterface = {
+  type: ActivityTypeEnum.Transaction,
+  status: ActivityStatusEnum.Applied,
+  hash: 'hash1',
+  amount: '0',
+  address: 'address1',
+  timestamp: 1,
+  entrypoint: 'transfer',
+  source: mockMember,
+  destination: {
     address: 'address1',
-    timestamp: 1,
-    entrypoint: 'transfer',
-    source: mockMember,
-    destination: {
-      address: 'address1',
-      alias: 'alias1'
-    }
-  },
-  {
-    type: ActivityTypeEnum.Transaction,
-    status: ActivityStatusEnum.Pending,
-    hash: 'hash2',
-    amount: '1',
-    timestamp: 10,
-    entrypoint: '',
-    source: mockMember,
-    destination: {
-      address: 'address3',
-      alias: 'alias3'
-    }
-  },
-  {
-    type: ActivityTypeEnum.Delegation,
-    status: ActivityStatusEnum.Applied,
-    hash: 'hash3',
-    amount: '0',
-    timestamp: 6,
-    source: mockMember,
-    destination: {
-      address: 'address3',
-      alias: 'alias3'
-    }
+    alias: 'alias1'
   }
+};
+
+const mockPendingOperation: ActivityInterface = {
+  type: ActivityTypeEnum.Transaction,
+  status: ActivityStatusEnum.Pending,
+  hash: 'hash2',
+  amount: '1',
+  timestamp: 10,
+  entrypoint: '',
+  source: mockMember,
+  destination: {
+    address: 'address3',
+    alias: 'alias3'
+  }
+};
+
+const mockAppliedDelegationOperation: ActivityInterface = {
+  type: ActivityTypeEnum.Delegation,
+  status: ActivityStatusEnum.Applied,
+  hash: 'hash3',
+  amount: '0',
+  timestamp: 6,
+  source: mockMember,
+  destination: {
+    address: 'address3',
+    alias: 'alias3'
+  }
+};
+
+const mockPendingTransfer: ActivityInterface = {
+  type: ActivityTypeEnum.Transaction,
+  status: ActivityStatusEnum.Pending,
+  hash: 'hash2',
+  amount: '1',
+  timestamp: 9,
+  entrypoint: '',
+  source: mockMember,
+  destination: {
+    address: 'address4',
+    alias: 'alias4'
+  }
+};
+
+const mockAppliedTransfer: ActivityInterface = {
+  type: ActivityTypeEnum.Transaction,
+  status: ActivityStatusEnum.Applied,
+  hash: 'hash4',
+  amount: '0',
+  address: 'contractAddress1',
+  id: 0,
+  timestamp: 8,
+  entrypoint: 'transfer',
+  source: {
+    address: 'address4',
+    alias: 'alias4'
+  },
+  destination: mockMember
+};
+
+export const mockOperations: ActivityInterface[] = [
+  mockAppliedOperation,
+  mockPendingOperation,
+  mockAppliedDelegationOperation
 ];
 
-export const mockTransfers: ActivityInterface[] = [
-  {
-    type: ActivityTypeEnum.Transaction,
-    status: ActivityStatusEnum.Pending,
-    hash: 'hash2',
-    amount: '1',
-    timestamp: 9,
-    entrypoint: '',
-    source: mockMember,
-    destination: {
-      address: 'address4',
-      alias: 'alias4'
-    }
-  },
-  {
-    type: ActivityTypeEnum.Transaction,
-    status: ActivityStatusEnum.Applied,
-    hash: 'hash4',
-    amount: '0',
-    address: 'contractAddress1',
-    id: 0,
-    timestamp: 8,
-    entrypoint: 'transfer',
-    source: {
-      address: 'address4',
-      alias: 'alias4'
-    },
-    destination: mockMember
-  }
-];
+export const mockTransfers: ActivityInterface[] = [mockPendingTransfer, mockAppliedTransfer];
 
-export const mockActivityGroup: ActivityGroup[] = groupActivitiesByHash(mockOperations, mockTransfers);
+export const mockAppliedActivityGroups: ActivityGroup[] = groupActivitiesByHash(
+  [mockAppliedOperation, mockAppliedDelegationOperation],
+  [mockAppliedTransfer]
+);
+export const mockPendingActivityGroups: ActivityGroup[] = groupActivitiesByHash(
+  [mockPendingOperation],
+  [mockPendingTransfer]
+);
