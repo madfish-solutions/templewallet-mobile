ce9a55b77d8601fbbbe94ef864232a49e04ffdc4
Andrii Ivaniv <unixvb@gmail.com>
2021-08-11 15:01:29 +0300

    initial token metadata util tests

---
+++ src/utils/token-metadata.utils.spec.ts
@@ -0,0 +1,7 @@
+describe('', () => {
+  it('should return correct TokenMetadataInterface structure', () => {});
+  it('should set symbol from name if symbol is not provided', () => {});
+  it('should set default symbol if symbol and name are not provided', () => {});
+  it('should set name from symbol if name is not provided', () => {});
+  it('should set default name if symbol and name are not provided', () => {});
+});
