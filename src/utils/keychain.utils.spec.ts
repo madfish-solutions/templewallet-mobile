172221dce6b6492241e8f41782bf73ddf1d8f0f4
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-08-11 12:35:02 +0300

    added biometryKeychainOptions tests

--- src/utils/keychain.utils.spec.ts
+++ src/utils/keychain.utils.spec.ts
@@ -1,4 +1,10 @@
-import { getKeychainOptions } from './keychain.utils';
+import { biometryKeychainOptions, getKeychainOptions } from './keychain.utils';
+
+const mockBiometryKeychainOptions = {
+  service: 'com.madfish-solutions.temple-mobile/biometry-protected-app-password',
+  accessControl: 'BiometryCurrentSet',
+  authenticationType: 'AuthenticationWithBiometrics'
+};
 
 describe('getKeychainOptions', () => {
   it('should return object if we passing non-empty string', () => {
@@ -7,5 +13,11 @@ describe('getKeychainOptions', () => {
 
   it('should return object if we passing empty string', () => {
     expect(getKeychainOptions('')).toEqual({ service: 'com.madfish-solutions.temple-mobile/' });
+  });
+});
+
+describe('biometryKeychainOptions', () => {
+  it('should return keychain options object with hardcoded password storage key', () => {
+    expect(biometryKeychainOptions).toEqual(mockBiometryKeychainOptions);
   });
 });
