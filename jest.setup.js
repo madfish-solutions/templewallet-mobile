6942006e070c0fcf54f0159c6b67e20cf5bbb74d
Innokentiy Mazhara <keshan3262@gmail.com>
2021-08-11 13:07:44 +0300

    Add tests for crypto.util.ts (#204)
    * Add tests for crypto.util.ts
    
    * Some refactoring
    
    * Swap some 'expect' statements
    
    * Refactor jest setup
    
    * Make a separate file for crypto mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,8 +1,15 @@
+import { decode, encode } from 'base-64';
 import 'react-native-gesture-handler/jestSetup';
 
-global.crypto = {
-  getRandomValues: jest.fn()
-};
+if (typeof btoa === 'undefined') {
+  global.btoa = encode;
+}
+
+if (typeof atob === 'undefined') {
+  global.atob = decode;
+}
+
+import './src/mocks/crypto.mock';
 
 import './src/mocks/native-modules.mock';
 
--- src/mocks/native-modules.mock.js
+++ src/mocks/native-modules.mock.js
@@ -1,3 +1,16 @@
 import { NativeModules } from 'react-native';
 
+export const mockRandomKey = 'randomKey';
+export const mockPbkdf2Key = 'pbkdf2Key';
+export const mockEncryptedString = 'encryptedString';
+export const mockUnencryptedString = 'testString';
+
+export const mockNativeAes = {
+  pbkdf2: jest.fn(() => Promise.resolve(mockPbkdf2Key)),
+  encrypt: jest.fn(() => Promise.resolve(mockEncryptedString)),
+  decrypt: jest.fn(() => Promise.resolve(mockUnencryptedString)),
+  randomKey: jest.fn(() => Promise.resolve(mockRandomKey))
+};
+
 NativeModules.RNPermissions = {};
+NativeModules.Aes = mockNativeAes;
