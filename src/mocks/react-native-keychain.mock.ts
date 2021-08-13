680deb1ae840e0519c1c6c279a0f9446abcbb3dd
Andrii Ivaniv <unixvb@users.noreply.github.com>
2021-08-13 21:22:54 +0300

    Shelter unit tests (#220)
    * initial shelter tests
    
    * shelter spec
    
    * shelter tests added

---
+++ src/mocks/react-native-keychain.mock.ts
@@ -0,0 +1,20 @@
+import Keychain from 'react-native-keychain';
+
+export const mockCorrectPassword = 'mockCorrectPassword';
+export const mockCorrectUserCredentialsValue = 'mockCorrectUserCredentials';
+export const mockCorrectUserCredentials = {
+  username: 'mockUsername',
+  password: JSON.stringify(mockCorrectUserCredentialsValue),
+  service: 'mockService',
+  storage: 'mockStorage'
+};
+
+export const mockKeychain = {
+  ...jest.requireActual('react-native-keychain'),
+  getGenericPassword: jest.fn(() => Promise.resolve<Keychain.UserCredentials>(mockCorrectUserCredentials)),
+  setGenericPassword: jest.fn(() => Promise.resolve()),
+  getSupportedBiometryType: jest.fn(() => Promise.resolve('FaceID')),
+  resetGenericPassword: jest.fn(() => Promise.resolve())
+};
+
+jest.mock('react-native-keychain', () => mockKeychain);
