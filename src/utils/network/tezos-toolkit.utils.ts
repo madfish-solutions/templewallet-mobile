d1eda55d2f0126c71608a0d36989f959bb715678
serh11p <git@serh11p.com>
2021-08-18 18:51:26 +0300

    Add memoization for createReadOnlyTezosToolkit()

--- src/utils/network/tezos-toolkit.utils.ts
+++ src/utils/network/tezos-toolkit.utils.ts
@@ -2,6 +2,7 @@ import { localForger } from '@taquito/local-forging';
 import { TezosToolkit, MichelCodecPacker, CompositeForger, RpcForger } from '@taquito/taquito';
 import { Tzip12Module } from '@taquito/tzip12';
 import { Tzip16Module } from '@taquito/tzip16';
+import memoize from 'mem';
 
 import { AccountInterface } from '../../interfaces/account.interface';
 import { ReadOnlySigner } from '../read-only.signer.util';
@@ -11,8 +12,8 @@ import { currentRpc$ } from './rpc.utils';
 export const CURRENT_NETWORK_ID = 'mainnet';
 const michelEncoder = new MichelCodecPacker();
 
-export const createTezosToolkit = () => {
-  const tezosToolkit = new TezosToolkit(getFastRpcClient(currentRpc$.getValue().url));
+export const createTezosToolkit = (rpcUrl = currentRpc$.getValue().url) => {
+  const tezosToolkit = new TezosToolkit(getFastRpcClient(rpcUrl));
   tezosToolkit.setPackerProvider(michelEncoder);
   tezosToolkit.setForgerProvider(new CompositeForger([tezosToolkit.getFactory(RpcForger)(), localForger]));
   tezosToolkit.addExtension(new Tzip16Module());
@@ -21,9 +22,15 @@ export const createTezosToolkit = () => {
   return tezosToolkit;
 };
 
-export const createReadOnlyTezosToolkit = (sender: AccountInterface) => {
-  const readOnlyTezosToolkit = createTezosToolkit();
-  readOnlyTezosToolkit.setSignerProvider(new ReadOnlySigner(sender.publicKeyHash, sender.publicKey));
+export const createReadOnlyTezosToolkit = (sender: AccountInterface) =>
+  createReadOnlyTezosToolkitPure(currentRpc$.getValue().url, sender);
 
-  return readOnlyTezosToolkit;
-};
+const createReadOnlyTezosToolkitPure = memoize(
+  (rpcUrl: string, sender: AccountInterface) => {
+    const readOnlyTezosToolkit = createTezosToolkit(rpcUrl);
+    readOnlyTezosToolkit.setSignerProvider(new ReadOnlySigner(sender.publicKeyHash, sender.publicKey));
+
+    return readOnlyTezosToolkit;
+  },
+  { cacheKey: ([rpcUrl, sender]) => `${rpcUrl}_${sender.publicKeyHash}` }
+);
