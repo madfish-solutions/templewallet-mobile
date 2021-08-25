b875f7e8f4ceda4f4b304097d89e4eb460c16b09
Andrew Kryshtal <andrew.kryshtal@gmail.com>
2021-08-26 02:25:20 +0300

    fixed derivation validation (#237)
    * fixed derivation validation
    
    * fixed validation, got regexp from library

--- src/modals/import-account-modal/import-account-seed/import-account-seed.form.tsx
+++ src/modals/import-account-modal/import-account-seed/import-account-seed.form.tsx
@@ -1,3 +1,4 @@
+import { isValidPath } from 'ed25519-hd-key';
 import { mixed, object, SchemaOf, string } from 'yup';
 
 import { ImportAccountDerivationEnum } from '../../../enums/account-type.enum';
@@ -22,30 +23,5 @@ export const importAccountSeedValidationSchema: SchemaOf<ImportAccountSeedValues
   seedPhrase: seedPhraseValidation,
   password: string(),
   derivationType: mixed<ImportAccountDerivationEnum>().oneOf(Object.values(ImportAccountDerivationEnum)).required(),
-  derivationPath: string().test('validateDerivationPath', 'Invalid derivation path', p => {
-    if (p === undefined) {
-      return true;
-    }
-    if (p.slice(-1) !== "'") {
-      return false;
-    }
-    if (!p.startsWith('m')) {
-      return false;
-    }
-    if (p.length > 1 && p[1] !== '/') {
-      return false;
-    }
-
-    const parts = p.replace('m', '').split('/').filter(Boolean);
-
-    if (parts.length < 4) {
-      return false;
-    }
-
-    return parts.every((p: string) => {
-      const pNum = +(p.includes("'") ? p.replace("'", '') : p);
-
-      return Number.isSafeInteger(pNum) && pNum >= 0;
-    });
-  })
+  derivationPath: string().test('validateDerivationPath', 'Invalid derivation path', p => (p ? isValidPath(p) : false))
 });
