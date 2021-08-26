83adfa57e7840bfe34540acd17dff55e6acf4b4f
Andrii Ivaniv <unixvb@gmail.com>
2021-08-26 17:17:58 +0300

    unit tests fix

--- src/mocks/react-native-reanimated.mock.js
+++ src/mocks/react-native-reanimated.mock.js
@@ -1,3 +1,5 @@
+global.__reanimatedWorkletInit = jest.fn();
+
 jest.mock('react-native-reanimated', () => {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const Reanimated = require('react-native-reanimated/mock');
