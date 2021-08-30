54103ae6281ab9e34fdd5041d512eea784c9c4b6
Andrii Ivaniv <unixvb@gmail.com>
2021-08-30 19:19:42 +0300

    lint fix

--- src/components/bottom-sheet/bottom-sheet.tsx
+++ src/components/bottom-sheet/bottom-sheet.tsx
@@ -11,10 +11,10 @@ import { useOrientationChange } from 'react-native-orientation-locker';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 import { emptyComponent } from '../../config/general';
+import { useAppLock } from '../../shelter/use-app-lock.hook';
 import { formatSize } from '../../styles/format-size';
 import { useDropdownBottomSheetStyles } from './bottom-sheet.styles';
 import { BottomSheetControllerProps } from './use-bottom-sheet-controller';
-import { useAppLock } from '../../shelter/use-app-lock.hook';
 
 interface Props extends BottomSheetControllerProps {
   title: string;
