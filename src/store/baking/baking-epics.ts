11c41888722c708522938466e8b675f855f82c80
Andrii Ivaniv <unixvb@gmail.com>
2021-08-30 19:13:59 +0300

    empty baker fix

--- src/store/baking/baking-epics.ts
+++ src/store/baking/baking-epics.ts
@@ -1,11 +1,11 @@
 import { combineEpics } from 'redux-observable';
 import { from, Observable, of } from 'rxjs';
-import { catchError, filter, map, switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 import { Action } from 'ts-action';
 import { ofType } from 'ts-action-operators';
 
 import { bakingBadApi } from '../../api.service';
-import { BakerInterface } from '../../interfaces/baker.interface';
+import { BakerInterface, emptyBaker } from '../../interfaces/baker.interface';
 import { isDefined } from '../../utils/is-defined';
 import { createReadOnlyTezosToolkit } from '../../utils/network/tezos-toolkit.utils';
 import { withSelectedAccount } from '../../utils/wallet.utils';
@@ -18,9 +18,14 @@ const loadSelectedBakerAddressEpic = (action$: Observable<Action>, state$: Obser
     withSelectedAccount(state$),
     switchMap(([, selectedAccount]) =>
       from(createReadOnlyTezosToolkit(selectedAccount).rpc.getDelegate(selectedAccount.publicKeyHash)).pipe(
-        filter(isDefined),
-        switchMap(address => bakingBadApi.get<BakerInterface>(`/bakers/${address}`)),
-        map(({ data }) => loadSelectedBakerActions.success(data)),
+        switchMap(bakerAddress => {
+          if (isDefined(bakerAddress)) {
+            return from(bakingBadApi.get<BakerInterface>(`/bakers/${bakerAddress}`)).pipe(map(({ data }) => data));
+          } else {
+            return of(emptyBaker);
+          }
+        }),
+        map(baker => loadSelectedBakerActions.success(baker)),
         catchError(err => of(loadSelectedBakerActions.fail(err.message)))
       )
     )
