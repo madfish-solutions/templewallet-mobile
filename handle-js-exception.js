8a4b6d1a1d3552d498581b06c4c289ff609fe2eb
Andrii Ivaniv <unixvb@gmail.com>
2021-09-02 16:33:32 +0300

    MissedBlockDuringConfirmationError fix

--- handle-js-exception.js
+++ handle-js-exception.js
@@ -3,12 +3,16 @@ import * as Sentry from '@sentry/react-native';
 import { Alert } from 'react-native';
 import { setJSExceptionHandler } from 'react-native-exception-handler';
 
-const allowInDevMode = false;
+const allowInDevMode = true;
 
 setJSExceptionHandler((error, isFatal) => {
   Sentry.captureException(error);
 
+  // TODO: remove this when taquito will fix this issue https://github.com/ecadlabs/taquito/issues/1040
+  const isTaquitoMissedBlockError = error.name === 'MissedBlockDuringConfirmationError';
+
   isFatal &&
+    !isTaquitoMissedBlockError &&
     Alert.alert(
       'Ooops, something went wrong',
       'We have reported this to our team! Please restart the app and try again!',
