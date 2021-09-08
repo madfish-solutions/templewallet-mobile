0ee7db03aeb8c17aceb121b9a161e1833b631969
Andrii Ivaniv <unixvb@gmail.com>
2021-09-08 16:01:23 +0300

    collectible modal added

---
+++ src/components/collectible-icon/collectible-icon.props.ts
@@ -0,0 +1,6 @@
+import { TokenInterface } from '../../token/interfaces/token.interface';
+
+export interface CollectibleIconProps {
+  collectible?: TokenInterface;
+  size: number;
+}
--- src/screens/collectibles-home/collectibles-list/collectibles-list.tsx
+++ src/screens/collectibles-home/collectibles-list/collectibles-list.tsx
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { View } from 'react-native';
+
+import { DataPlaceholder } from '../../../components/data-placeholder/data-placeholder';
+import { useLayoutSizes } from '../../../hooks/use-layout-sizes.hook';
+import { useVisibleCollectiblesListSelector } from '../../../store/wallet/wallet-selectors';
+import { getTokenSlug } from '../../../token/utils/token.utils';
+import { CollectiblesListStyles } from './collectibles-list.styles';
+import { TouchableCollectibleIcon } from './touchable-collectible-icon/touchable-collectible-icon';
 
 export const CollectiblesList = () => {
-  return (
+  const visibleCollectiblesList = useVisibleCollectiblesListSelector();
+  const { layoutWidth, handleLayout } = useLayoutSizes();
+
+  const smallCardSize = (1 / 3) * layoutWidth - 0.1;
+  const bigCardSize = (2 / 3) * layoutWidth - 0.1;
+
+  const [first, second, third, ...rest] = visibleCollectiblesList;
+
+  return visibleCollectiblesList.length === 0 ? (
+    <DataPlaceholder text="Not found any NFT" />
+  ) : (
     <>
-      <Text>item</Text>
-      <Text>item</Text>
-      <Text>item</Text>
-      <Text>item</Text>
-      <Text>item</Text>
-      <Text>item</Text>
+      <View style={CollectiblesListStyles.rowContainer} onLayout={handleLayout}>
+        <TouchableCollectibleIcon collectible={first} size={bigCardSize} />
+        <View>
+          <TouchableCollectibleIcon collectible={second} size={smallCardSize} />
+          <TouchableCollectibleIcon collectible={third} size={smallCardSize} />
+        </View>
+      </View>
+      <View style={CollectiblesListStyles.rowContainer}>
+        {rest.map(collectible => (
+          <TouchableCollectibleIcon key={getTokenSlug(collectible)} collectible={collectible} size={smallCardSize} />
+        ))}
+      </View>
     </>
   );
 };
