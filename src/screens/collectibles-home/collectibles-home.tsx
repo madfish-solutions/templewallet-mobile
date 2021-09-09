7cfdde49ee76128cf49d4f624c680f2b1d337761
Andrii Ivaniv <unixvb@gmail.com>
2021-09-09 10:58:14 +0300

    search functionality hidden

--- src/screens/collectibles-home/collectibles-home.tsx
+++ src/screens/collectibles-home/collectibles-home.tsx
@@ -5,22 +5,18 @@ import { Divider } from '../../components/divider/divider';
 import { HeaderCard } from '../../components/header-card/header-card';
 import { Icon } from '../../components/icon/icon';
 import { IconNameEnum } from '../../components/icon/icon-name.enum';
-import { TouchableIcon } from '../../components/icon/touchable-icon/touchable-icon';
 import { ScreenContainer } from '../../components/screen-container/screen-container';
 import { SearchInput } from '../../components/search-input/search-input';
-import { emptyFn } from '../../config/general';
 import { useFilteredAssetsList } from '../../hooks/use-filtered-assets-list.hook';
 import { ScreensEnum } from '../../navigator/enums/screens.enum';
 import { useNavigation } from '../../navigator/hooks/use-navigation.hook';
-import { useCollectiblesListSelector, useVisibleCollectiblesListSelector } from '../../store/wallet/wallet-selectors';
+import { useVisibleCollectiblesListSelector } from '../../store/wallet/wallet-selectors';
 import { formatSize } from '../../styles/format-size';
-import { useColors } from '../../styles/use-colors';
 import { useCollectiblesHomeStyles } from './collectibles-home.styles';
 import { CollectiblesList } from './collectibles-list/collectibles-list';
 import { PromotionCarousel } from './promotion-carousel/promotion-carousel';
 
 export const CollectiblesHome = () => {
-  const colors = useColors();
   const styles = useCollectiblesHomeStyles();
   const { navigate } = useNavigation();
 
@@ -34,14 +30,12 @@ export const CollectiblesHome = () => {
       <HeaderCard hasInsetTop={true} style={styles.headerCard}>
         <View style={[styles.headerContainer, styles.widthPaddingHorizontal]}>
           <View style={styles.actionsContainer}>
-            <TouchableIcon
-              name={isSearchMode ? IconNameEnum.XSearch : IconNameEnum.Search}
-              disabled={true}
-              color={colors.disabled}
-              onPress={() => setIsSearchMode(!isSearchMode)}
-            />
-            <Divider size={formatSize(16)} />
-            <TouchableIcon name={IconNameEnum.Hummer} disabled={true} color={colors.disabled} onPress={emptyFn} />
+            {/*<TouchableIcon*/}
+            {/*  name={isSearchMode ? IconNameEnum.XSearch : IconNameEnum.Search}*/}
+            {/*  onPress={() => setIsSearchMode(!isSearchMode)}*/}
+            {/*/>*/}
+            {/*<Divider size={formatSize(16)} />*/}
+            {/*<TouchableIcon name={IconNameEnum.Hummer} disabled={true} color={colors.disabled} onPress={emptyFn} />*/}
           </View>
 
           <TouchableOpacity style={styles.walletNavigationButton} onPress={() => navigate(ScreensEnum.Wallet)}>
