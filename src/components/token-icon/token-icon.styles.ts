29845982d30c50299812fb8727b5d1dacb9a2368
LendiHop <mishin.danil2017@gmail.com>
2021-09-21 13:30:51 +0300

    fix-some-icons-are-square

--- src/components/token-icon/token-icon.styles.ts
+++ src/components/token-icon/token-icon.styles.ts
@@ -5,6 +5,8 @@ import { formatSize } from '../../styles/format-size';
 export const TokenIconStyles = StyleSheet.create({
   container: {
     marginVertical: formatSize(4),
-    marginHorizontal: formatSize(4)
+    marginHorizontal: formatSize(4),
+    borderRadius: formatSize(100),
+    overflow: 'hidden'
   }
 });
