ba17d5cd2c81c63c207387f4381da9c996234b5b
LendiHop <mishin.danil2017@gmail.com>
2021-09-22 13:41:02 +0300

    refactoring

--- src/components/token-icon/token-icon.styles.ts
+++ src/components/token-icon/token-icon.styles.ts
@@ -6,7 +6,6 @@ export const TokenIconStyles = StyleSheet.create({
   container: {
     marginVertical: formatSize(4),
     marginHorizontal: formatSize(4),
-    borderRadius: formatSize(100),
     overflow: 'hidden'
   }
 });
