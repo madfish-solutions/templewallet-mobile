206b90e3d7bca9d5a48ed0652dd4d209fcb79a94
Andrii Ivaniv <unixvb@gmail.com>
2021-09-24 13:32:37 +0300

    fixes

--- src/modals/send-modal/send-modal.tsx
+++ src/modals/send-modal/send-modal.tsx
@@ -1,7 +1,6 @@
 import { RouteProp, useRoute } from '@react-navigation/core';
-import { BigNumber } from 'bignumber.js';
 import { Formik } from 'formik';
-import React, { FC, useEffect, useMemo } from 'react';
+import React, { FC, useMemo } from 'react';
 import { Text, View } from 'react-native';
 import { useDispatch } from 'react-redux';
 
@@ -30,15 +29,11 @@ import {
 } from '../../store/wallet/wallet-selectors';
 import { formatSize } from '../../styles/format-size';
 import { showWarningToast } from '../../toast/toast.utils';
-import { TEZ_TOKEN_METADATA } from '../../token/data/tokens-metadata';
 import { emptyToken, TokenInterface } from '../../token/interfaces/token.interface';
 import { isDefined } from '../../utils/is-defined';
-import { mutezToTz } from '../../utils/tezos.util';
 import { SendModalFormValues, sendModalValidationSchema } from './send-modal.form';
 import { useSendModalStyles } from './send-modal.styles';
 
-// TODO: load real fee instead
-const TEZ_MAX_FEE = 0.1;
 
 export const SendModal: FC = () => {
   const dispatch = useDispatch();
@@ -100,66 +95,54 @@ export const SendModal: FC = () => {
       enableReinitialize={true}
       validationSchema={sendModalValidationSchema}
       onSubmit={onSubmit}>
-      {({ values, setFieldValue, submitForm }) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        useEffect(() => setFieldValue('amount', undefined), [values.assetAmount.asset]);
+      {({ values, submitForm }) => (
+        <ScreenContainer isFullScreenMode={true}>
+          <ModalStatusBar />
+          <View>
+            <Divider size={formatSize(8)} />
+            <FormAssetAmountInput name="assetAmount" label="Asset" assetsList={filteredAssetsListWithTez} />
+            <Divider />
 
-        const amountMaxValue = BigNumber.max(
-          mutezToTz(new BigNumber(values.assetAmount.asset.balance), values.assetAmount.asset.decimals).minus(
-            values.assetAmount.asset.symbol === TEZ_TOKEN_METADATA.symbol ? TEZ_MAX_FEE : 0
-          ),
-          0
-        );
-
-        return (
-          <ScreenContainer isFullScreenMode={true}>
-            <ModalStatusBar />
-            <View>
-              <Divider size={formatSize(8)} />
-              <FormAssetAmountInput name="assetAmount" label="Asset" assetsList={filteredAssetsListWithTez} />
-              <Divider />
-
-              <Label
-                label="To"
-                description={`Address or Tezos domain to send ${values.assetAmount.asset.symbol} funds to.`}
-              />
-              {values.transferBetweenOwnAccounts ? (
-                <>
-                  <AccountFormDropdown name="ownAccount" list={ownAccountsReceivers} />
-                  <Divider size={formatSize(10)} />
-                </>
-              ) : (
-                <FormAddressInput name="receiverPublicKeyHash" placeholder="e.g. address" />
-              )}
-              <View
-                onTouchStart={() =>
-                  void (
-                    transferBetweenOwnAccountsDisabled && showWarningToast({ description: 'Create one more account' })
-                  )
-                }>
-                <FormCheckbox
-                  disabled={transferBetweenOwnAccountsDisabled}
-                  name="transferBetweenOwnAccounts"
-                  size={formatSize(16)}>
-                  <Text style={styles.checkboxText}>Transfer between my accounts</Text>
-                </FormCheckbox>
-              </View>
-
-              <Divider />
+            <Label
+              label="To"
+              description={`Address or Tezos domain to send ${values.assetAmount.asset.symbol} funds to.`}
+            />
+            {values.transferBetweenOwnAccounts ? (
+              <>
+                <AccountFormDropdown name="ownAccount" list={ownAccountsReceivers} />
+                <Divider size={formatSize(10)} />
+              </>
+            ) : (
+              <FormAddressInput name="receiverPublicKeyHash" placeholder="e.g. address" />
+            )}
+            <View
+              onTouchStart={() =>
+                void (
+                  transferBetweenOwnAccountsDisabled && showWarningToast({ description: 'Create one more account' })
+                )
+              }>
+              <FormCheckbox
+                disabled={transferBetweenOwnAccountsDisabled}
+                name="transferBetweenOwnAccounts"
+                size={formatSize(16)}>
+                <Text style={styles.checkboxText}>Transfer between my accounts</Text>
+              </FormCheckbox>
             </View>
 
-            <View>
-              <ButtonsContainer>
-                <ButtonLargeSecondary title="Close" onPress={goBack} />
-                <Divider size={formatSize(16)} />
-                <ButtonLargePrimary title="Send" onPress={submitForm} />
-              </ButtonsContainer>
+            <Divider />
+          </View>
 
-              <InsetSubstitute type="bottom" />
-            </View>
-          </ScreenContainer>
-        );
-      }}
+          <View>
+            <ButtonsContainer>
+              <ButtonLargeSecondary title="Close" onPress={goBack} />
+              <Divider size={formatSize(16)} />
+              <ButtonLargePrimary title="Send" onPress={submitForm} />
+            </ButtonsContainer>
+
+            <InsetSubstitute type="bottom" />
+          </View>
+        </ScreenContainer>
+      )}
     </Formik>
   );
 };
