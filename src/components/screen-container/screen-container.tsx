a611f82e2f1d4a5729029d74f82a3cb6fe459bee
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2021-09-27 12:03:56 +0300

    Fix - Buttons on Android go up after closing keyboard (#257)
    * fix-some-icons-are-square
    
    * Fix - Buttons on Android go up after closing keyboard
    
    * refactoring
    
    * More refactoring

--- src/components/screen-container/screen-container.tsx
+++ src/components/screen-container/screen-container.tsx
@@ -22,7 +22,7 @@ export const ScreenContainer: FC<Props> = ({ isFullScreenMode = false, style, co
     <KeyboardAvoidingView
       style={[styles.scrollView, style]}
       keyboardVerticalOffset={keyboardVerticalOffset}
-      behavior="padding">
+      behavior={isAndroid ? 'height' : 'padding'}>
       <ScrollView
         contentContainerStyle={[
           styles.scrollViewContentContainer,
