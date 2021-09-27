a83abd33ceb72154ac2f9786aa9726cbba87a5f8
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-09-27 13:04:32 +0300

    added wallet-address component

---
+++ src/components/wallet-address/wallet-address.styles.ts
@@ -0,0 +1,25 @@
+import { createUseStyles } from "../../styles/create-use-styles";
+import { formatSize } from "../../styles/format-size";
+
+export const useWalletAddressStyles = createUseStyles(({ colors, typography }) => ({
+    iconContainer: {
+        marginLeft: formatSize(2),
+        paddingHorizontal: formatSize(4),
+        paddingVertical: formatSize(2),
+        backgroundColor: colors.blue10,
+        borderRadius: formatSize(4)
+    },
+    pkhWrapper: {
+        flexDirection: 'row',
+    },
+    domainNameContainer: {
+        paddingHorizontal: formatSize(4),
+        paddingVertical: formatSize(2),
+        backgroundColor: colors.blue10,
+        borderRadius: formatSize(4)
+    },
+    domainNameText: {
+        ...typography.numbersRegular15,
+        color: colors.blue
+    }
+}))
---
+++ src/components/wallet-address/wallet-address.tsx
@@ -0,0 +1,63 @@
+import React, { FC, useEffect, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { useSelectedAccountSelector } from "../../store/wallet/wallet-selectors";
+import { isTezosDomainNameValid, isTezosDomainsSupported } from "../../utils/dns.utils";
+import { isString } from "../../utils/is-string";
+import { createReadOnlyTezosToolkit } from "../../utils/network/tezos-toolkit.utils";
+import { Icon } from "../icon/icon";
+import { IconNameEnum } from "../icon/icon-name.enum";
+import { PublicKeyHashText } from "../public-key-hash-text/public-key-hash-text";
+import { useWalletAddressStyles } from "./wallet-address.styles";
+import memoize from 'mem';
+
+interface Props {
+    publicKeyHash: string;
+}
+
+export const WalletAddress: FC<Props> = ({ publicKeyHash }) => {
+    const styles = useWalletAddressStyles();
+    const [domainName, setDomainName] = useState('');
+    const [isShownDomainName, setIsShownDomainName] = useState(false);
+    const selectedAccount = useSelectedAccountSelector();
+    const tezos = createReadOnlyTezosToolkit(selectedAccount);
+    const { resolver: domainsResolver } = isTezosDomainsSupported(tezos);
+
+    const updateDomainReverseName = async (pkh: string) => {
+        const memoizedDomainName = memoize(async () => {
+            return await domainsResolver.resolveAddressToName(pkh)
+        }, {
+            cacheKey: () => pkh,
+        })
+        setDomainName(await memoizedDomainName() ?? '');
+    };
+
+    useEffect(() => {
+        setIsShownDomainName(false)
+        updateDomainReverseName(publicKeyHash);
+    }, [publicKeyHash]);
+
+
+    return (
+        <View style={styles.pkhWrapper}>
+            {isShownDomainName ? (
+                <View style={styles.domainNameContainer}>
+                    <Text style={styles.domainNameText}>{domainName}</Text>
+                </View>
+            ) : (
+                <PublicKeyHashText publicKeyHash={publicKeyHash} />
+            )}
+            {isString(domainName) ? (
+                <TouchableOpacity
+                    style={styles.iconContainer}
+                    onPress={e => {
+                        e.stopPropagation();
+                        setIsShownDomainName(!isShownDomainName);
+                    }}>
+                    <Icon name={isShownDomainName ? IconNameEnum.Diez : IconNameEnum.Globe} />
+                </TouchableOpacity>
+            ) : (
+                null
+            )}
+        </View>
+    )
+}
