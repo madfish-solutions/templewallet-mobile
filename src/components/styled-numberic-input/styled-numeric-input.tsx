eb8809ac5fe786f0d84681658830b281845e648d
Andrii Ivaniv <unixvb@gmail.com>
2021-10-05 16:50:24 +0300

    numeric input fixes

--- src/components/styled-numberic-input/styled-numeric-input.tsx
+++ src/components/styled-numberic-input/styled-numeric-input.tsx
@@ -5,6 +5,8 @@ import { useNumericInput } from '../../hooks/use-numeric-input.hook';
 import { TEZ_TOKEN_METADATA } from '../../token/data/tokens-metadata';
 import { StyledTextInput } from '../styled-text-input/styled-text-input';
 import { StyledNumericInputProps } from './styled-numeric-input.props';
+import { isAndroid } from '../../config/system';
+import { TextInput } from 'react-native';
 
 export const StyledNumericInput: FC<StyledNumericInputProps> = ({
   value,
@@ -32,6 +34,7 @@ export const StyledNumericInput: FC<StyledNumericInputProps> = ({
       value={stringValue}
       isError={isError}
       isShowCleanButton={isShowCleanButton}
+      autoCapitalize="words"
       keyboardType="numeric"
       onBlur={handleBlur}
       onFocus={handleFocus}
