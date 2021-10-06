350c00eed10e8ad5c990c979821246368b5bb48c
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-10-06 12:09:46 +0300

    fixed comments

--- src/hooks/use-visible-tokens-dollar-balance.ts
+++ src/hooks/use-visible-tokens-dollar-balance.ts
@@ -5,7 +5,6 @@ import { useExchangeRatesSelector } from '../store/currency/currency-selectors';
 import { useVisibleTokensListSelector, useTezosTokenSelector } from '../store/wallet/wallet-selectors';
 import { TEZ_TOKEN_METADATA } from '../token/data/tokens-metadata';
 import { getTokenSlug } from '../token/utils/token.utils';
-import { formatAssetAmount } from '../utils/number.util';
 import { mutezToTz, tzToMutez } from '../utils/tezos.util';
 
 export const useSummaryBalance = () => {
@@ -19,12 +18,12 @@ export const useSummaryBalance = () => {
     for (const token of visibleTokens) {
       const exchangeRate: number | undefined = exchangeRates[getTokenSlug(token)];
       const parsedAmount = mutezToTz(new BigNumber(token.balance), token.decimals).multipliedBy(exchangeRate);
-      dollarVal += Number(formatAssetAmount(parsedAmount, BigNumber.ROUND_DOWN, 2));
+      dollarVal += Number(parsedAmount);
     }
     const tezosParsedAmount = mutezToTz(new BigNumber(tezosToken.balance), tezosToken.decimals).multipliedBy(
       exchangeRates.tez
     );
-    dollarVal += Number(formatAssetAmount(tezosParsedAmount, BigNumber.ROUND_DOWN, 2));
+    dollarVal += Number(tezosParsedAmount);
     setSummaryValue(tzToMutez(new BigNumber(dollarVal / exchangeRates.tez), TEZ_TOKEN_METADATA.decimals));
   }, [visibleTokens, exchangeRates]);
 
