e594c5685190eb6aebdc9d3fdb7dfe501bd44b48
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2021-10-19 18:17:37 +0300

    integrated section removed

--- src/screens/d-apps/d-apps.styles.ts
+++ src/screens/d-apps/d-apps.styles.ts
@@ -1,19 +1,8 @@
 import { createUseStyles } from '../../styles/create-use-styles';
 import { formatSize } from '../../styles/format-size';
 
-export const useDAppsStyles = createUseStyles(({ colors, typography }) => ({
+export const useDAppsStyles = createUseStyles(() => ({
   container: {
-    marginHorizontal: formatSize(16)
-  },
-  marginLeft: {
     marginLeft: formatSize(16)
-  },
-  title: {
-    ...typography.body15Semibold,
-    color: colors.black,
-    marginLeft: formatSize(6)
-  },
-  list: {
-    height: formatSize(350)
   }
 }));
--- src/screens/d-apps/d-apps.tsx
+++ src/screens/d-apps/d-apps.tsx
@@ -1,23 +1,20 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, FlatList } from 'react-native';
 import { useDispatch } from 'react-redux';
 
+import { DataPlaceholder } from '../../components/data-placeholder/data-placeholder';
 import { Divider } from '../../components/divider/divider';
-import { IconNameEnum } from '../../components/icon/icon-name.enum';
 import { SearchInput } from '../../components/search-input/search-input';
 import { loadDAppsListActions } from '../../store/d-apps/d-apps-actions';
 import { useDAppsListSelector } from '../../store/d-apps/d-apps-selectors';
-import { useThemeSelector } from '../../store/settings/settings-selectors';
 import { formatSize } from '../../styles/format-size';
 import { useDAppsStyles } from './d-apps.styles';
-import { IntegratedDApp } from './integrated/integrated';
 import { OthersDApp } from './others/others';
 
 export const DApps = () => {
   const dispatch = useDispatch();
   useEffect(() => void dispatch(loadDAppsListActions.submit()), []);
 
-  const theme = useThemeSelector();
   const styles = useDAppsStyles();
 
   const DAppsList = useDAppsListSelector();
@@ -37,31 +34,16 @@ export const DApps = () => {
       <SearchInput placeholder="Search token" onChangeText={setSearchQuery} />
       <Divider size={formatSize(28)} />
       <View style={styles.container}>
-        <Text style={styles.title}>Integrated</Text>
-        <Divider size={formatSize(12)} />
-        <IntegratedDApp
-          iconName={theme === 'dark' ? IconNameEnum.QuipuSwapDark : IconNameEnum.QuipuSwap}
-          title="QuipuSwap"
-          description="The most efficient DApp for Tezos"
-          url="https://quipuswap.com"
-        />
-      </View>
-      <Divider size={formatSize(36)} />
-      <View style={styles.marginLeft}>
-        <Text style={styles.title}>Others</Text>
-        <Divider size={formatSize(12)} />
-        <View style={styles.list}>
-          {sortedDAppsList.length ? (
-            <FlatList
-              data={sortedDAppsList}
-              renderItem={item => <OthersDApp item={item} />}
-              keyExtractor={item => item.name}
-              numColumns={2}
-            />
-          ) : (
-            <Text style={styles.title}>Not found</Text>
-          )}
-        </View>
+        {sortedDAppsList.length ? (
+          <FlatList
+            data={sortedDAppsList}
+            renderItem={item => <OthersDApp item={item} />}
+            keyExtractor={item => item.name}
+            numColumns={2}
+          />
+        ) : (
+          <DataPlaceholder text="No records found." />
+        )}
       </View>
     </>
   );
