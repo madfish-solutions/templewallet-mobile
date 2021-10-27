14a2d16a5cedc6d69744707fa38dfb218723a721
Andrii Ivaniv <unixvb@gmail.com>
2021-10-27 11:49:21 +0300

    Collectibles home screen Tablet view fixed

--- src/screens/collectibles-home/collectibles-list/collectibles-list.tsx
+++ src/screens/collectibles-home/collectibles-list/collectibles-list.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC } from 'react';
 import { View } from 'react-native';
 
 import { DataPlaceholder } from '../../../components/data-placeholder/data-placeholder';
