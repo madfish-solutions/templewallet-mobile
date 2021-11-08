83d95b534d249d764e012e9958d66dcdea465525
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-08 10:35:51 +0200

    added spaces to amount string (#307)
    * added spaces to amount string
    
    * added more tests and removed export for local fn
    
    * fixed fn call on big num with many decimals

--- src/utils/number.util.spec.ts
+++ src/utils/number.util.spec.ts
@@ -14,11 +14,19 @@ describe('formatAssetAmount', () => {
   });
 
   it('should format positive bignumber more than 1000 value and return string with 2 decimals', () => {
-    expect(formatAssetAmount(bigNumberMoreThanThousand)).toEqual('10000.25');
+    expect(formatAssetAmount(bigNumberMoreThanThousand)).toEqual('10 000.25');
+  });
+
+  it('should format positive bignumber more than 10 000 000 value and return formatted string', () => {
+    expect(formatAssetAmount(new BigNumber(10123456))).toEqual('10 123 456');
+  });
+
+  it('should format positive bignumber more than 1 000 000 value and return string with 2 decimals', () => {
+    expect(formatAssetAmount(new BigNumber('123456789.25456'), BigNumber.ROUND_UP)).toEqual('123 456 789.26');
   });
 
   it('should format positive bignumber more than 1000 value and return string with 2 decimals using rounding mode up', () => {
-    expect(formatAssetAmount(bigNumberMoreThanThousand, BigNumber.ROUND_UP)).toEqual('10000.26');
+    expect(formatAssetAmount(bigNumberMoreThanThousand, BigNumber.ROUND_UP)).toEqual('10 000.26');
   });
 
   it('should format positive bignumber less than 1000 value and return string with 1 decimal using rounding mode up', () => {
