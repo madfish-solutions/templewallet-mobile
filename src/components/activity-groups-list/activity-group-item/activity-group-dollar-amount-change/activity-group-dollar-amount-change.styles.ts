c41b1f562a3e6386448e8730bec8f302f2331a5b
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2021-11-09 12:07:53 +0200

    Some-data-doesn't-adapt-on-Android (#297)
    * header fix
    
    * amount change history fixed
    
    * with memory leak
    
    * tests fixed
    
    * refactoring
    
    * lint fix
    
    * tests fix
    
    * refactoring

---
+++ src/components/activity-groups-list/activity-group-item/activity-group-dollar-amount-change/activity-group-dollar-amount-change.styles.ts
@@ -0,0 +1,17 @@
+import { createUseStyles } from '../../../../styles/create-use-styles';
+
+export const useActivityGroupDollarAmountChangeStyles = createUseStyles(({ colors, typography }) => ({
+  container: {
+    alignItems: 'flex-end',
+    flexShrink: 1
+  },
+  positiveAmountText: {
+    color: colors.adding
+  },
+  negativeAmountText: {
+    color: colors.destructive
+  },
+  valueText: {
+    ...typography.numbersRegular11
+  }
+}));
---
+++ src/components/activity-groups-list/activity-group-item/activity-group-dollar-amount-change/activity-group-dollar-amount-change.tsx
@@ -0,0 +1,33 @@
+import React, { FC } from 'react';
+import { View } from 'react-native';
+
+import { NonZeroAmounts } from '../../../../interfaces/non-zero-amounts.interface';
+import { conditionalStyle } from '../../../../utils/conditional-style';
+import { FormattedAmount } from '../../../formatted-amount';
+import { useActivityGroupDollarAmountChangeStyles } from './activity-group-dollar-amount-change.styles';
+
+interface Props {
+  nonZeroAmounts: NonZeroAmounts;
+}
+
+export const ActivityGroupDollarAmountChange: FC<Props> = ({ nonZeroAmounts }) => {
+  const styles = useActivityGroupDollarAmountChangeStyles();
+
+  return (
+    <View style={styles.container}>
+      {nonZeroAmounts.dollarSums.map((amount, index) => (
+        <FormattedAmount
+          key={index}
+          amount={amount}
+          isDollarValue={true}
+          showMinusSign={amount.isLessThan(0)}
+          showPlusSign={amount.isGreaterThan(0)}
+          style={[
+            styles.valueText,
+            conditionalStyle(amount.isGreaterThan(0), styles.positiveAmountText, styles.negativeAmountText)
+          ]}
+        />
+      ))}
+    </View>
+  );
+};
