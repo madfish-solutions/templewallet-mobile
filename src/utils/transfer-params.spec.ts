1e5bbd52d7ff9cb17cdb200cfdf1bcbee7323e5a
Andrii Ivaniv <unixvb@gmail.com>
2021-11-12 17:43:17 +0200

    tests fixed

--- src/utils/transfer-params.spec.ts
+++ src/utils/transfer-params.spec.ts
@@ -7,72 +7,80 @@ import { mockFA1_2TokenMetadata, mockFA2TokenMetadata } from '../token/interface
 import { rxJsTestingHelper } from './testing.utis';
 import { getTransferParams$ } from './transfer-params.utils';
 
-beforeEach(() => {
-  mockFA1_2Contract.methods.transfer.mockReset();
-  mockFA2Contract.methods.transfer.mockReset();
-});
+const mockRpcUrl = 'https://rpc-url.mock/mainnet';
 
-const rpcUrlMock = 'https://rpc-url.mock/mainnet';
+describe('getTransferParams$', () => {
+  beforeEach(() => {
+    mockFA1_2Contract.methods.transfer.mockReset();
+    mockFA2Contract.methods.transfer.mockReset();
+  });
 
-it('getTransferParams$ should create params for transferring TEZ', done => {
-  const mockInputAmount = new BigNumber(0.005);
+  it('should create params for transferring TEZ', done => {
+    const mockInputAmount = new BigNumber(0.005);
 
-  getTransferParams$(
-    TEZ_TOKEN_METADATA,
-    rpcUrlMock,
-    mockWalletAccount,
-    'receiverPublicKeyHash',
-    mockInputAmount
-  ).subscribe(
-    rxJsTestingHelper(params => {
-      expect(params).toEqual({ amount: mockInputAmount.toFixed(), to: 'receiverPublicKeyHash', mutez: true });
-    }, done)
-  );
-});
+    getTransferParams$(
+      TEZ_TOKEN_METADATA,
+      mockRpcUrl,
+      mockWalletAccount,
+      'receiverPublicKeyHash',
+      mockInputAmount
+    ).subscribe(
+      rxJsTestingHelper(params => {
+        expect(params).toEqual({ amount: mockInputAmount.toNumber(), to: 'receiverPublicKeyHash', mutez: true });
+      }, done)
+    );
+  });
 
-it('getTransferParams$ should create params for transferring FA1.2 tokens', done => {
-  mockFA1_2Contract.methods.transfer.mockReturnValueOnce({ toTransferParams: jest.fn(), send: jest.fn() });
-  mockToolkitMethods.contractAt.mockResolvedValueOnce(mockFA1_2Contract);
-  getTransferParams$(
-    mockFA1_2TokenMetadata,
-    rpcUrlMock,
-    mockWalletAccount,
-    'receiverPublicKeyHash',
-    new BigNumber(0.01)
-  ).subscribe(
-    rxJsTestingHelper(() => {
-      expect(mockFA1_2Contract.methods.transfer).toBeCalledWith(
-        mockWalletAccount.publicKeyHash,
-        'receiverPublicKeyHash',
-        new BigNumber(0.01)
-      );
-    }, done)
-  );
-});
+  it('should create params for transferring FA1.2 tokens', done => {
+    mockFA1_2Contract.methods.transfer.mockReturnValueOnce({ toTransferParams: jest.fn(), send: jest.fn() });
+    mockToolkitMethods.contractAt.mockResolvedValueOnce(mockFA1_2Contract);
 
-it('getTransferParams$ should create params for transferring FA2 tokens', done => {
-  mockFA2Contract.methods.transfer.mockReturnValueOnce({ toTransferParams: jest.fn(), send: jest.fn() });
-  mockToolkitMethods.contractAt.mockResolvedValueOnce(mockFA2Contract);
-  getTransferParams$(
-    mockFA2TokenMetadata,
-    rpcUrlMock,
-    mockWalletAccount,
-    'receiverPublicKeyHash',
-    new BigNumber(0.001)
-  ).subscribe(
-    rxJsTestingHelper(() => {
-      expect(mockFA2Contract.methods.transfer).toBeCalledWith([
-        {
-          from_: mockWalletAccount.publicKeyHash,
-          txs: [
-            {
-              to_: 'receiverPublicKeyHash',
-              token_id: mockFA2TokenMetadata.id,
-              amount: new BigNumber(0.001)
-            }
-          ]
-        }
-      ]);
-    }, done)
-  );
+    const mockInputAmount = new BigNumber(0.01);
+
+    getTransferParams$(
+      mockFA1_2TokenMetadata,
+      mockRpcUrl,
+      mockWalletAccount,
+      'receiverPublicKeyHash',
+      mockInputAmount
+    ).subscribe(
+      rxJsTestingHelper(() => {
+        expect(mockFA1_2Contract.methods.transfer).toBeCalledWith(
+          mockWalletAccount.publicKeyHash,
+          'receiverPublicKeyHash',
+          mockInputAmount
+        );
+      }, done)
+    );
+  });
+
+  it('should create params for transferring FA2 tokens', done => {
+    mockFA2Contract.methods.transfer.mockReturnValueOnce({ toTransferParams: jest.fn(), send: jest.fn() });
+    mockToolkitMethods.contractAt.mockResolvedValueOnce(mockFA2Contract);
+
+    const mockInputAmount = new BigNumber(0.001);
+
+    getTransferParams$(
+      mockFA2TokenMetadata,
+      mockRpcUrl,
+      mockWalletAccount,
+      'receiverPublicKeyHash',
+      mockInputAmount
+    ).subscribe(
+      rxJsTestingHelper(() => {
+        expect(mockFA2Contract.methods.transfer).toBeCalledWith([
+          {
+            from_: mockWalletAccount.publicKeyHash,
+            txs: [
+              {
+                to_: 'receiverPublicKeyHash',
+                token_id: mockFA2TokenMetadata.id,
+                amount: mockInputAmount
+              }
+            ]
+          }
+        ]);
+      }, done)
+    );
+  });
 });
