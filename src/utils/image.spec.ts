eaf53508798d427b3a5b49a961b848c420e3866d
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-12 14:19:45 +0200

    updated image specs

--- src/utils/image.spec.ts
+++ src/utils/image.spec.ts
@@ -1,5 +1,7 @@
 import { formatImgUri, formatNftUri } from './image.utils';
 
+const objktOrigin = 'https://assets.objkt.com/file/assets-001/';
+
 describe('image utils', () => {
   describe('formatImgUri', () => {
     it('should leave http:// uri intact', () => {
@@ -20,13 +22,18 @@ describe('image utils', () => {
   });
   describe('formatNftUri', () => {
     it('should convert asset KT and id to objkt.com URL for that asset', () => {
-      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '464017')).toEqual(
-        'https://assets.objkt.com/file/assets-001/KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/1/7/464017/thumb288'
+      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '464017', 120)).toEqual(
+        `${objktOrigin}KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/1/7/464017/thumb288`
+      );
+    });
+    it('should convert asset KT and id to objkt.com URL for that asset with big size thumb', () => {
+      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '464017', 320)).toEqual(
+        `${objktOrigin}KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/1/7/464017/thumb400`
       );
     });
     it('should convert asset KT and id with id < 10 to objkt.com URL for that asset', () => {
-      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '9')).toEqual(
-        'https://assets.objkt.com/file/assets-001/KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/0/9/9/thumb288'
+      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '9', 120)).toEqual(
+        `${objktOrigin}KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/0/9/9/thumb288`
       );
     });
   });
