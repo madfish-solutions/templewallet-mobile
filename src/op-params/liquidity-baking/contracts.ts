788c5173ed1e95650b3ab53a6f3102d175592e49
andrewkryshtal <andrew.kryshtal@gmail.com>
2021-11-12 12:23:11 +0200

    fixed comments

--- src/op-params/liquidity-baking/contracts.ts
+++ src/op-params/liquidity-baking/contracts.ts
@@ -7,6 +7,8 @@ import { isDefined } from '../../utils/is-defined';
 
 export const LIQUIDITY_BAKING_DEX_ADDRESS = 'KT1TxqZ8QtKvLu3V3JH7Gx58n7Co8pgtpQU5';
 export const LIQUIDITY_BAKING_LP_TOKEN_ADDRESS = 'KT1AafHA1C1vk959wvHWBispY9Y2f3fxBUUo';
+export const LIQUIDITY_BAKING_TOKEN_ADDRESS = 'KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn';
+export const LIQUIDITY_BAKING_TOKEN_ID = 0;
 
 export const useContract = <C extends ContractAbstraction<ContractProvider>, S>(
   address: string,
