28049ab000c3cb6ec22b8191cc6fa94d467cfccb
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-15 16:25:14 +0200

    review fixes

--- src/utils/image.spec.ts
+++ src/utils/image.spec.ts
@@ -1,4 +1,5 @@
-import { formatImgUri, formatNftUri } from './image.utils';
+import { TokenInterface } from '../token/interfaces/token.interface';
+import { formatImgUri, formatCollectibleUri } from './image.utils';
 
 const objktOrigin = 'https://assets.objkt.com/file/assets-001/';
 
@@ -20,19 +21,22 @@ describe('image utils', () => {
       expect(formatImgUri('ipfs://mockFileHash')).toEqual('https://ipfs.io/ipfs/mockFileHash/');
     });
   });
-  describe('formatNftUri', () => {
+  describe('formatCollectibleUri', () => {
     it('should convert asset KT and id to objkt.com URL for that asset', () => {
-      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '464017', 120)).toEqual(
+      const collectible = { address: 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', id: 464017 } as TokenInterface;
+      expect(formatCollectibleUri(collectible, 120)).toEqual(
         `${objktOrigin}KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/1/7/464017/thumb288`
       );
     });
     it('should convert asset KT and id to objkt.com URL for that asset with big size thumb', () => {
-      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '464017', 320)).toEqual(
+      const collectible = { address: 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', id: 464017 } as TokenInterface;
+      expect(formatCollectibleUri(collectible, 320)).toEqual(
         `${objktOrigin}KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/1/7/464017/thumb400`
       );
     });
     it('should convert asset KT and id with id < 10 to objkt.com URL for that asset', () => {
-      expect(formatNftUri('KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', '9', 120)).toEqual(
+      const collectible = { address: 'KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton', id: 9 } as TokenInterface;
+      expect(formatCollectibleUri(collectible, 120)).toEqual(
         `${objktOrigin}KT1RJ6PbjHpwc3M5rw5s2Nbmefwbuwbdxton/0/9/9/thumb288`
       );
     });
--- src/utils/image.utils.ts
+++ src/utils/image.utils.ts
@@ -1,3 +1,5 @@
+import { TokenInterface } from '../token/interfaces/token.interface';
+
 const IPFS_GATEWAY = 'ipfs.io';
 const IPFS_PROTOCOL_PREFIX = 'ipfs://';
 
@@ -11,13 +13,11 @@ export const formatImgUri = (origin: string) =>
     ? `https://${IPFS_GATEWAY}/ipfs/${origin.substring(IPFS_PROTOCOL_PREFIX.length)}/`
     : origin;
 
-export const formatNftUri = (address: string, id: string, size: number) => {
-  let imgSize = ImageSizeEnum.Small;
-  if (size > 288) {
-    imgSize = ImageSizeEnum.Big;
-  }
+export const formatCollectibleUri = (collectible: TokenInterface, size: number) => {
+  const { address } = collectible;
+  const id = `${collectible.id}`;
 
   return `https://assets.objkt.com/file/assets-001/${address}/${id.length > 1 ? id[id.length - 2] : 0}/${
     id[id.length - 1]
-  }/${id}/thumb${imgSize}`;
+  }/${id}/thumb${size > 288 ? ImageSizeEnum.Big : ImageSizeEnum.Small}`;
 };
