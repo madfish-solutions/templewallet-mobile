ebfff7bc391668ffc49ed849e6e2d9829fa6dcfe
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-15 15:55:21 +0200

    fix delegate button

--- src/components/token-screen-content-container/token-screen-content-container.tsx
+++ src/components/token-screen-content-container/token-screen-content-container.tsx
@@ -1,7 +1,15 @@
 import React, { FC, ReactElement, useState } from 'react';
 import { Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
+import { delegationApy } from '../../config/general';
+import { ScreensEnum } from '../../navigator/enums/screens.enum';
+import { useNavigation } from '../../navigator/hooks/use-navigation.hook';
+import { useSelectedBakerSelector } from '../../store/baking/baking-selectors';
 import { formatSize } from '../../styles/format-size';
+import { useColors } from '../../styles/use-colors';
+import { TEZ_TOKEN_METADATA } from '../../token/data/tokens-metadata';
+import { Icon } from '../icon/icon';
 import { IconNameEnum } from '../icon/icon-name.enum';
 import { ScreenContainer } from '../screen-container/screen-container';
 import { IconSegmentControl } from '../segmented-control/icon-segment-control/icon-segment-control';
@@ -16,6 +24,8 @@ const historyComponentIndex = 0;
 
 export const TokenScreenContentContainer: FC<Props> = ({ historyComponent, infoComponent }) => {
   const styles = useTokenScreenContentContainerStyles();
+  const { navigate } = useNavigation();
+  const [, isBakerSelected] = useSelectedBakerSelector();
 
   const [segmentedControlIndex, setSegmentedControlIndex] = useState(0);
   const showHistoryComponent = segmentedControlIndex === historyComponentIndex;
@@ -23,7 +33,16 @@ export const TokenScreenContentContainer: FC<Props> = ({ historyComponent, infoC
   return (
     <>
       <View style={styles.headerContainer}>
-        <Text style={styles.headerText}>{showHistoryComponent ? 'History' : 'Info'}</Text>
+        {/* <Text style={styles.headerText}>{showHistoryComponent ? 'History' : 'Info'}</Text> */}
+        <TouchableOpacity style={styles.delegateContainer} onPress={() => navigate(ScreensEnum.Delegation)}>
+          {!isBakerSelected ? (
+            <Text style={styles.delegateText}>Rewards & Redelegate</Text>
+          ) : (
+            <Text style={styles.delegateText}>
+              Delegate: <Text style={styles.apyText}>{delegationApy}% APY</Text>
+            </Text>
+          )}
+        </TouchableOpacity>
 
         <IconSegmentControl
           selectedIndex={segmentedControlIndex}
