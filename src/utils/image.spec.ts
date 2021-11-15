72503a1e637c58f0b4bfbfce572a2dee14bfa953
letier3110 <38981658+letier3110@users.noreply.github.com>
2021-11-15 16:29:10 +0200

    add tests for image uri is svg check

--- src/utils/image.spec.ts
+++ src/utils/image.spec.ts
@@ -1,19 +1,29 @@
-import { formatImgUri } from './image.utils';
+import { formatImgUri, isImgUriSvg } from './image.utils';
 
-describe('formatImgUri', () => {
-  it('should leave http:// uri intact', () => {
-    const mockHttpUri = 'http://mocklink.com';
+describe('image utils', () => {
+  describe('formatImgUri', () => {
+    it('should leave http:// uri intact', () => {
+      const mockHttpUri = 'http://mocklink.com';
 
-    expect(formatImgUri(mockHttpUri)).toEqual(mockHttpUri);
+      expect(formatImgUri(mockHttpUri)).toEqual(mockHttpUri);
+    });
+
+    it('should leave https:// uri intact', () => {
+      const mockHttpsUri = 'https://mocklink.com';
+
+      expect(formatImgUri(mockHttpsUri)).toEqual(mockHttpsUri);
+    });
+
+    it('should convert ipfs:// uri to URL to ipfs.io for that file', () => {
+      expect(formatImgUri('ipfs://mockFileHash')).toEqual('https://ipfs.io/ipfs/mockFileHash/');
+    });
   });
-
-  it('should leave https:// uri intact', () => {
-    const mockHttpsUri = 'https://mocklink.com';
-
-    expect(formatImgUri(mockHttpsUri)).toEqual(mockHttpsUri);
-  });
-
-  it('should convert ipfs:// uri to URL to ipfs.io for that file', () => {
-    expect(formatImgUri('ipfs://mockFileHash')).toEqual('https://ipfs.io/ipfs/mockFileHash/');
+  describe('is image svg', () => {
+    it('should return true for img address with .svg', () => {
+      expect(isImgUriSvg('https://facebook.com/favicon.svg')).toEqual(true);
+    });
+    it('should return false for img address with .png', () => {
+      expect(isImgUriSvg('https://facebook.com/favicon.png')).toEqual(false);
+    });
   });
 });
