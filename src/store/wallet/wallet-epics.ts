3a4cbacd2049e240bff2765d1f01fbc148eaf20e
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2021-12-22 13:11:35 +0200

    error handling

--- src/store/wallet/wallet-epics.ts
+++ src/store/wallet/wallet-epics.ts
@@ -18,6 +18,7 @@ import { StacksEnum } from '../../navigator/enums/stacks.enum';
 import { showErrorToast, showSuccessToast } from '../../toast/toast.utils';
 import { getTokenSlug } from '../../token/utils/token.utils';
 import { groupActivitiesByHash } from '../../utils/activity.utils';
+import { isDefined } from '../../utils/is-defined';
 import { mapOperationsToActivities } from '../../utils/operation.utils';
 import { paramsToPendingActions } from '../../utils/params-to-actions.util';
 import { createReadOnlyTezosToolkit, CURRENT_NETWORK_ID } from '../../utils/rpc/tezos-toolkit.utils';
@@ -163,6 +164,15 @@ const waitForOperationCompletionEpic = (action$: Observable<Action>, state$: Obs
     switchMap(([{ opHash, sender }, rpcUrl]) =>
       from(createReadOnlyTezosToolkit(rpcUrl, sender).operation.createOperation(opHash)).pipe(
         switchMap(operation => operation.confirmation(1)),
+        catchError((err, caught) => {
+          if (err.message === 'Confirmation polling timed out') {
+            return of(1);
+          } else if (isDefined(err.message)) {
+            throw new Error(err.message);
+          } else {
+            return caught;
+          }
+        }),
         delay(BCD_INDEXING_DELAY),
         concatMap(() => [
           loadTezosBalanceActions.submit(),
