4b94bbaf9ed95641822b14c820be75af86de7f64
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2022-01-12 15:50:30 +0200

    quipuswap nft loading properly

--- src/components/collectible-icon/collectible-icon.tsx
+++ src/components/collectible-icon/collectible-icon.tsx
@@ -2,7 +2,7 @@ import React, { FC, useState } from 'react';
 import { Image, View } from 'react-native';
 
 import { formatSize } from '../../styles/format-size';
-import { formatCollectibleUri } from '../../utils/image.utils';
+import { formatCollectibleUri, formatImgUri } from '../../utils/image.utils';
 import { isDefined } from '../../utils/is-defined';
 import { CollectibleIconProps } from './collectible-icon.props';
 import { useCollectibleIconStyles } from './collectible-icon.styles';
@@ -10,6 +10,12 @@ import { useCollectibleIconStyles } from './collectible-icon.styles';
 export const CollectibleIcon: FC<CollectibleIconProps> = ({ collectible, size }) => {
   const styles = useCollectibleIconStyles();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [fallback, setFallback] = useState(false);
+
+  let thumbnailUri;
+  if (isDefined(collectible) && isDefined(collectible.thumbnailUri)) {
+    thumbnailUri = fallback ? formatImgUri(collectible.thumbnailUri) : formatCollectibleUri(collectible);
+  }
 
   return isDefined(collectible) ? (
     <View
@@ -23,11 +29,12 @@ export const CollectibleIcon: FC<CollectibleIconProps> = ({ collectible, size })
         <Image
           style={styles.image}
           source={{
-            uri: formatCollectibleUri(collectible),
+            uri: thumbnailUri,
             width: size,
             height: size
           }}
           blurRadius={isLoaded ? 0 : 5}
+          onError={() => setFallback(true)}
           onLoadEnd={() => setIsLoaded(true)}
         />
       ) : null}
