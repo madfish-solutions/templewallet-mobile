e4d5530f8ccf2b8cf757d827c7e8f36245316185
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2022-01-17 10:54:02 +0200

    QuipuSwap nft not loading (#362)
    * quipuswap nft loading properly
    
    * refactoring
    
    * refactoring
    
    * refactoring

--- src/components/collectible-icon/collectible-icon.tsx
+++ src/components/collectible-icon/collectible-icon.tsx
@@ -2,7 +2,7 @@ import React, { FC, useState } from 'react';
 import { Image, View } from 'react-native';
 
 import { formatSize } from '../../styles/format-size';
-import { formatCollectibleUri } from '../../utils/image.utils';
+import { formatCollectibleUri, formatImgUri } from '../../utils/image.utils';
 import { isDefined } from '../../utils/is-defined';
 import { CollectibleIconProps } from './collectible-icon.props';
 import { useCollectibleIconStyles } from './collectible-icon.styles';
@@ -10,8 +10,9 @@ import { useCollectibleIconStyles } from './collectible-icon.styles';
 export const CollectibleIcon: FC<CollectibleIconProps> = ({ collectible, size }) => {
   const styles = useCollectibleIconStyles();
   const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoadingFailed, setIsLoadingFailed] = useState(false);
 
-  return isDefined(collectible) ? (
+  return isDefined(collectible) && isDefined(collectible.thumbnailUri) ? (
     <View
       style={{
         width: size,
@@ -23,11 +24,12 @@ export const CollectibleIcon: FC<CollectibleIconProps> = ({ collectible, size })
         <Image
           style={styles.image}
           source={{
-            uri: formatCollectibleUri(collectible),
+            uri: isLoadingFailed ? formatImgUri(collectible.thumbnailUri) : formatCollectibleUri(collectible),
             width: size,
             height: size
           }}
           blurRadius={isLoaded ? 0 : 5}
+          onError={() => setIsLoadingFailed(true)}
           onLoadEnd={() => setIsLoaded(true)}
         />
       ) : null}
