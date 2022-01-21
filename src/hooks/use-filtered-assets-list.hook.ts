11f7e5566a3111261ad8f12783e67faadf438930
Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>
2022-01-21 17:11:32 +0200

    refactoring

--- src/hooks/use-filtered-assets-list.hook.ts
+++ src/hooks/use-filtered-assets-list.hook.ts
@@ -1,38 +1,15 @@
-import { BigNumber } from 'bignumber.js';
 import { useEffect, useState } from 'react';
 
-import { useExchangeRatesSelector } from '../store/currency/currency-selectors';
 import { TokenInterface } from '../token/interfaces/token.interface';
-import { getTokenSlug } from '../token/utils/token.utils';
-import { isDefined } from '../utils/is-defined';
 import { isString } from '../utils/is-string';
-import { isNonZeroBalance, mutezToTz } from '../utils/tezos.util';
+import { isNonZeroBalance } from '../utils/tezos.util';
 
 export const useFilteredAssetsList = (assetsList: TokenInterface[], initialIsHideZeroBalance = false) => {
-  const exchangeRates = useExchangeRatesSelector();
-
   const [isHideZeroBalance, setIsHideZeroBalance] = useState(initialIsHideZeroBalance);
   const [nonZeroBalanceAssetsList, setNonZeroBalanceAssetsList] = useState<TokenInterface[]>([]);
 
   const [searchValue, setSearchValue] = useState<string>();
   const [filteredAssetsList, setFilteredAssetsList] = useState<TokenInterface[]>([]);
-
-  useEffect(() => {
-    assetsList.sort((asset1, asset2) => {
-      const zero = new BigNumber(0);
-
-      const exchangeRate1: number | undefined = exchangeRates[getTokenSlug(asset1)];
-      const exchangeRate2: number | undefined = exchangeRates[getTokenSlug(asset2)];
-
-      const parsedAmount1 = mutezToTz(new BigNumber(asset1.balance), asset1.decimals);
-      const parsedAmount2 = mutezToTz(new BigNumber(asset2.balance), asset2.decimals);
-
-      const dollarAmount1 = isDefined(exchangeRate1) ? parsedAmount1.multipliedBy(exchangeRate1) : zero;
-      const dollarAmount2 = isDefined(exchangeRate2) ? parsedAmount2.multipliedBy(exchangeRate2) : zero;
-
-      return dollarAmount2.minus(dollarAmount1).toNumber();
-    });
-  }, [assetsList]);
 
   useEffect(() => {
     const result: TokenInterface[] = assetsList.filter(asset => isNonZeroBalance(asset));
---
+++ src/hooks/use-sorted-assets-list.hook.ts
@@ -0,0 +1,31 @@
+import { BigNumber } from 'bignumber.js';
+import { useMemo } from 'react';
+
+import { useExchangeRatesSelector } from '../store/currency/currency-selectors';
+import { TokenInterface } from '../token/interfaces/token.interface';
+import { getTokenSlug } from '../token/utils/token.utils';
+import { isDefined } from '../utils/is-defined';
+import { mutezToTz } from '../utils/tezos.util';
+
+export const useSortedAssetsList = (filteredAssetsList: TokenInterface[]) => {
+  const exchangeRates = useExchangeRatesSelector();
+
+  return useMemo(
+    () =>
+      filteredAssetsList.sort((asset1, asset2) => {
+        const zero = new BigNumber(0);
+
+        const exchangeRate1: number | undefined = exchangeRates[getTokenSlug(asset1)];
+        const exchangeRate2: number | undefined = exchangeRates[getTokenSlug(asset2)];
+
+        const parsedAmount1 = mutezToTz(new BigNumber(asset1.balance), asset1.decimals);
+        const parsedAmount2 = mutezToTz(new BigNumber(asset2.balance), asset2.decimals);
+
+        const dollarAmount1 = isDefined(exchangeRate1) ? parsedAmount1.multipliedBy(exchangeRate1) : zero;
+        const dollarAmount2 = isDefined(exchangeRate2) ? parsedAmount2.multipliedBy(exchangeRate2) : zero;
+
+        return dollarAmount2.minus(dollarAmount1).toNumber();
+      }),
+    [filteredAssetsList]
+  );
+};
