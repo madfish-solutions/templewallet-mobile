ae01a8b8c62bd596695ce1d38ef2e7f6fcbdcf4f
Andrii Ivaniv <unixvb@users.noreply.github.com>
2022-02-15 18:55:55 +0200

    Appstore account switched to Madfish one (#370)
    * Appstore account switched to Madfish one
    
    * e2e changes revert

--- src/utils/keychain.utils.spec.ts
+++ src/utils/keychain.utils.spec.ts
@@ -1,18 +1,18 @@
 import { biometryKeychainOptions, getKeychainOptions } from './keychain.utils';
 
 const mockBiometryKeychainOptions = {
-  service: 'com.madfish-solutions.temple-mobile/biometry-protected-app-password',
+  service: 'com.madfish.temple-wallet/biometry-protected-app-password',
   accessControl: 'BiometryCurrentSet',
   authenticationType: 'AuthenticationWithBiometrics'
 };
 
 describe('getKeychainOptions', () => {
   it('should return object if we passing non-empty string', () => {
-    expect(getKeychainOptions('test')).toEqual({ service: 'com.madfish-solutions.temple-mobile/test' });
+    expect(getKeychainOptions('test')).toEqual({ service: 'com.madfish.temple-wallet/test' });
   });
 
   it('should return object if we passing empty string', () => {
-    expect(getKeychainOptions('')).toEqual({ service: 'com.madfish-solutions.temple-mobile/' });
+    expect(getKeychainOptions('')).toEqual({ service: 'com.madfish.temple-wallet/' });
   });
 });
 
