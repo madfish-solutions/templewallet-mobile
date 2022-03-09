260593ca76efb45793aa830a18c848725d209ba5
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-03-09 17:24:18 +0000

    added passcode flag to persist keychain

--- src/utils/keychain.utils.ts
+++ src/utils/keychain.utils.ts
@@ -6,7 +6,8 @@ export const PASSWORD_CHECK_KEY = 'app-password';
 export const PASSWORD_STORAGE_KEY = 'biometry-protected-app-password';
 
 export const getKeychainOptions = (key: string): Keychain.Options => ({
-  service: `${APP_IDENTIFIER}/${key}`
+  service: `${APP_IDENTIFIER}/${key}`,
+  accessible: Keychain.ACCESSIBLE.WHEN_PASSCODE_SET_THIS_DEVICE_ONLY
 });
 
 export const biometryKeychainOptions: Keychain.Options = {
