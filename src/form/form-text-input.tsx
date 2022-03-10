681c235a65649bb77e1121bcb9b313b5c7a2828e
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-03-10 10:11:15 +0000

    review fix

--- src/form/form-text-input.tsx
+++ src/form/form-text-input.tsx
@@ -3,6 +3,7 @@ import React, { FC } from 'react';
 
 import { StyledTextInput } from '../components/styled-text-input/styled-text-input';
 import { StyledTextInputProps } from '../components/styled-text-input/styled-text-input.props';
+import { isAndroid } from '../config/system';
 import { hasError } from '../utils/has-error';
 import { ErrorMessage } from './error-message/error-message';
 
@@ -18,9 +19,10 @@ export const FormTextInput: FC<Props> = ({ name, editable, placeholder, isShowCl
   return (
     <>
       <StyledTextInput
-        keyboardType="visible-password"
         autoCorrect={false}
         autoComplete="off"
+        spellCheck={false}
+        {...(isAndroid ? { keyboardType: 'visible-password' } : {})}
         value={field.value}
         editable={editable}
         placeholder={placeholder}
