81bb0623c6b99f1c01767e2360c668a9cfaa669e
letier3110 <letier3110@gmail.com>
2022-03-16 19:31:10 +0000

    added splash for ios

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -8,7 +8,6 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { BiometryAvailabilityProvider } from '../biometry/biometry-availability.provider';
 import { HIDE_SPLASH_SCREEN_TIMEOUT } from '../config/animation';
-// import { emptyFn } from '../config/general';
 import { HideBalanceProvider } from '../hooks/hide-balance/hide-balance.provider';
 import { useAppStateStatus } from '../hooks/use-app-state-status.hook';
 import { useDelayedEffect } from '../hooks/use-delayed-effect.hook';
@@ -22,18 +21,10 @@ enableScreens();
 LogBox.ignoreAllLogs();
 
 export const App = () => {
-  useAppStateStatus(
-    () => {
-      // show();
-      console.log('first func');
-      setTimeout(() => hide(), HIDE_SPLASH_SCREEN_TIMEOUT);
-    },
-    () => {
-      show();
-      console.log('second func');
-      // setTimeout(() => hide(), HIDE_SPLASH_SCREEN_TIMEOUT);
-    }
-  );
+  useAppStateStatus({
+    onAppInactiveState: () => show(),
+    onAppActiveState: () => hide()
+  });
   useDelayedEffect(HIDE_SPLASH_SCREEN_TIMEOUT, () => void hide({ fade: true }), []);
 
   return (
