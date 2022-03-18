fb69bfcf1890d13f7736e7a2e3ab6152364f2e5a
Andrii Ivaniv <unixvb@gmail.com>
2022-03-18 14:15:20 +0200

    crypto utils tests added

--- src/mocks/react-native-themis.mock.ts
+++ src/mocks/react-native-themis.mock.ts
@@ -1,7 +1,23 @@
 export const mockSymmetricKey64 = 'BQD4WGMsvr/AICOKuua79o9jorgLmGS3QmLjtR3NrSs=';
+export const mockUnencryptedString = 'testString';
+export const mockEncryptedString = 'encryptedString';
+export const mockPassphrase = 'correctPassword';
 
+/* eslint-disable  @typescript-eslint/no-unused-vars */
 export const mockReactNativeThemis = {
-  symmetricKey64: jest.fn(() => Promise.resolve(mockSymmetricKey64))
+  symmetricKey64: jest.fn(() => Promise.resolve(mockSymmetricKey64)),
+  secureCellSealWithPassphraseEncrypt64: jest.fn((passphrase: string, plaintext: string, context: string) =>
+    Promise.resolve(mockEncryptedString)
+  ),
+  secureCellSealWithPassphraseDecrypt64: jest.fn((passphrase: string, encrypted64: string, context: string) => {
+    return new Promise((resolve, reject) => {
+      if (passphrase === mockPassphrase) {
+        resolve(mockUnencryptedString);
+      } else {
+        reject(undefined);
+      }
+    });
+  })
 };
 
 jest.mock('react-native-themis', () => mockReactNativeThemis);
