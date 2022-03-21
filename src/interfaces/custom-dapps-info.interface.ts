db112e0bab7b5da4bccef4e91fdef1ffe9ed5e34
Andrew Marushko <andrewmarushko@gmail.com>
2022-03-21 15:54:24 +0200

    delete slug enum

--- src/interfaces/custom-dapps-info.interface.ts
+++ src/interfaces/custom-dapps-info.interface.ts
@@ -6,31 +6,12 @@ enum DappType {
   Collectibles = 'Collectibles',
   Other = 'Other'
 }
-
-enum DataTypeSlug {
-  Quipuswap = 'quipuswap',
-  Objkt = 'objkt.com',
-  Hen = 'hen',
-  Pixelpotus = 'pixelpotus',
-  Dogami = 'dogami',
-  Tezotopia = 'tezotopia',
-  Tezosdomains = 'tezosdomains',
-  Plenty = 'plenty',
-  Kalamint = 'kalamint',
-  Smartlink = 'smartlink',
-  Youves = 'youves',
-  Crunchy = 'crunchy',
-  Aliensfarm = 'aliensfarm',
-  Flamedefi = 'flamedefi',
-  Kolibri = 'kolibri'
-}
-
 export interface CustomDAppInfo {
   name: string;
   dappUrl: string;
   type: DappType;
   logo: string;
-  slug: DataTypeSlug;
+  slug: string;
   category: [DappType];
 }
 
