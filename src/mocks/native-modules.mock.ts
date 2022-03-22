50b9b1cc9ce83bafebf2c7cc2ca44018866423ad
Oleksii Radetskyi <alexey.radetsky@gmail.com>
2022-03-22 21:20:01 +0200

    remove react-native-aes-crypto dep

--- src/mocks/native-modules.mock.ts
+++ src/mocks/native-modules.mock.ts
@@ -1,10 +1,5 @@
 import { NativeModules } from 'react-native';
 
-export const mockPbkdf2Key = 'pbkdf2Key';
-
-export const mockNativeAes = {
-  pbkdf2: jest.fn(() => Promise.resolve(mockPbkdf2Key))
-};
+// export const mockPbkdf2Key = 'pbkdf2Key';
 
 NativeModules.RNPermissions = {};
-NativeModules.Aes = mockNativeAes;
