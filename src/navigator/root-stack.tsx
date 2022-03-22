8e98c4609839ceb24f3f06881ab08793f3ad5276
letier3110 <letier3110@gmail.com>
2022-03-22 12:40:06 +0000

    fix inactive-active state flow

--- src/navigator/root-stack.tsx
+++ src/navigator/root-stack.tsx
@@ -2,8 +2,10 @@ import { PortalProvider } from '@gorhom/portal';
 import { DefaultTheme, NavigationContainer, NavigationContainerRef, Theme } from '@react-navigation/native';
 import { createStackNavigator, StackNavigationOptions, TransitionPresets } from '@react-navigation/stack';
 import React, { createRef, useMemo, useState } from 'react';
+import { hide, show } from 'react-native-bootsplash';
 
 import { useModalOptions } from '../components/header/use-modal-options.util';
+import { useAppStateStatus } from '../hooks/use-app-state-status.hook';
 import { useQuickActions } from '../hooks/use-quick-actions.hook';
 import { useResetKeychainOnInstall } from '../hooks/use-reset-keychain-on-install.hook';
 import { AddCustomRpcModal } from '../modals/add-custom-rpc-modal/add-custom-rpc-modal';
@@ -40,6 +42,10 @@ export const RootStackScreen = () => {
   const { isLocked } = useAppLock();
   const isAuthorised = useIsAuthorisedSelector();
   const colors = useColors();
+  useAppStateStatus({
+    onAppInactiveState: () => show(),
+    onAppActiveState: () => hide()
+  });
 
   const [currentRouteName, setCurrentRouteName] = useState<ScreensEnum>(ScreensEnum.Welcome);
 
