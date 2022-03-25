da3ea538dbecc52737ac77525f05154e74fae69c
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-03-25 11:39:55 +0000

    Topup balance (#423)
    * added new screen
    
    * added tabs
    
    * added moonpay link
    
    * reused old component for disclaimer
    
    * remove unused

---
+++ src/screens/buy/debit.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, TouchableOpacity, View } from 'react-native';
+
+import { Divider } from '../../components/divider/divider';
+import { Icon } from '../../components/icon/icon';
+import { IconNameEnum } from '../../components/icon/icon-name.enum';
+import { WhiteContainer } from '../../components/white-container/white-container';
+import { formatSize } from '../../styles/format-size';
+import { openUrl } from '../../utils/linking.util';
+import { useBuyStyles } from './buy.styles';
+import { useSignedMoonPayUrl } from './useSignedUrl';
+
+export const Debit = () => {
+  const styles = useBuyStyles();
+  const url = useSignedMoonPayUrl();
+
+  return (
+    <>
+      <Divider size={formatSize(16)} />
+      <WhiteContainer>
+        <View style={styles.providerLogo}>
+          <Icon name={IconNameEnum.MoonPay} size={formatSize(160)} />
+        </View>
+        <View style={styles.divider} />
+        <TouchableOpacity style={styles.textContainer} onPress={() => openUrl(url)} disabled={!url}>
+          <Text style={styles.actionsContainer}>Buy TEZ with MoonPay</Text>
+        </TouchableOpacity>
+      </WhiteContainer>
+      <Divider size={formatSize(16)} />
+    </>
+  );
+};
