11e6ba80740085d9718157f5bfc1039e6800f181
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-03-25 13:50:48 +0000

    revert manual installation of RNScrypt

--- android/app/src/main/java/com/templewallet/MainActivity.java
+++ android/app/src/main/java/com/templewallet/MainActivity.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 import android.content.Intent;
-import com.crypho.scrypt.RNScryptPackage;
 import android.content.res.Configuration;
 
 public class MainActivity extends ReactActivity {
