9f0cbbb18170ab09116c45329abb376335e47796
Andrii Ivaniv <unixvb@gmail.com>
2022-04-07 14:38:14 +0300

    ios Firebase check setup

--- ios/TempleWallet/AppDelegate.m
+++ ios/TempleWallet/AppDelegate.m
@@ -1,3 +1,4 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 #import "Orientation.h"
 
@@ -38,6 +39,7 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+[FIRApp configure];
 #ifdef FB_SONARKIT_ENABLED
   InitializeFlipper(application);
 #endif
