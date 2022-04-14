0b7c0f9d3917d3bd07a868a4402766775e4b79b7
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-04-14 10:48:03 +0100

    added more general case for error transform to general type

--- src/toast/toast.utils.ts
+++ src/toast/toast.utils.ts
@@ -15,7 +15,13 @@ const DEFAULT_ERROR_MESSAGE = 'Warning! The transaction is likely to fail!';
 const TAQUITO_MISSED_BLOCK_ERROR_MESSAGE =
   'Taquito missed a block while waiting for operation confirmation and was not able to find the operation';
 const TAQUITO_500_ERROR_MESSAGE = 'Http error response: (500)';
-const JSON_PARSE_ERROR = 'JSON Parse error: Unexpected token';
+/**
+ * there are actually two errors:
+ * "JSON Parse error: Unexpected token"
+ * "JSON Parse error: Unexpected identifier"
+ * in the JSON_PARSE_ERROR
+ */
+const JSON_PARSE_ERROR = 'JSON Parse error: Unexpected';
 
 export const showErrorToast = ({ description, title, onPress }: ToastProps) => {
   const slicedErrorMessage = description.slice(0, 26);
