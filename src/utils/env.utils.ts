12b1377603db495b36ee280fb17817f22407f089
Andrii Ivaniv <unixvb@users.noreply.github.com>
2022-04-14 13:16:43 +0300

    env variables fixed (#475)

--- src/utils/env.utils.ts
+++ src/utils/env.utils.ts
@@ -1,4 +1,13 @@
 // eslint-disable-next-line import/no-named-as-default
 import Config from 'react-native-config';
 
-export const getEnv = (key: string): string => Config[key] ?? '';
+const getEnv = (key: string): string => Config[key] ?? '';
+
+export const READ_ONLY_SIGNER_PUBLIC_KEY = getEnv('READ_ONLY_SIGNER_PUBLIC_KEY');
+export const READ_ONLY_SIGNER_PUBLIC_KEY_HASH = getEnv('READ_ONLY_SIGNER_PUBLIC_KEY_HASH');
+
+export const FIREBASE_PROJECT_ID = getEnv('FIREBASE_PROJECT_ID');
+export const IOS_APP_ID = getEnv('IOS_APP_ID');
+export const ANDROID_APP_ID = getEnv('ANDROID_APP_ID');
+
+export const TEMPLE_WALLET_API = getEnv('TEMPLE_WALLET_API');
