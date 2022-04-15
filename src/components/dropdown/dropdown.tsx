a09c097ec3bf0131b895cd613978a4701de61b84
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-04-15 16:42:16 +0100

    attempt to add autoscroll to accounts

--- src/components/dropdown/dropdown.tsx
+++ src/components/dropdown/dropdown.tsx
@@ -1,9 +1,10 @@
 import { TouchableOpacity as BottomSheetTouchableOpacity } from '@gorhom/bottom-sheet';
-import React, { FC } from 'react';
+import React, { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { TouchableOpacity, useWindowDimensions, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 import { emptyComponent, EmptyFn, EventFn } from '../../config/general';
+import { isDefined } from '../../utils/is-defined';
 import { BottomSheet } from '../bottom-sheet/bottom-sheet';
 import { useBottomSheetController } from '../bottom-sheet/use-bottom-sheet-controller';
 import { DropdownItemContainer } from './dropdown-item-container/dropdown-item-container';
@@ -17,12 +18,15 @@ export interface DropdownProps<T> {
   renderListItem: DropdownListItemComponent<T>;
   renderActionButtons?: DropdownActionButtonsComponent;
   onLongPress?: EmptyFn;
+  autoScroll?: boolean;
 }
 
 export interface DropdownValueProps<T> {
   value?: T;
   list: T[];
   disabled?: boolean;
+  comparator?: keyof T;
+  autoScroll?: boolean;
   onValueChange: EventFn<T | undefined>;
 }
 
@@ -52,6 +56,8 @@ export const Dropdown = <T extends unknown>({
   list,
   title,
   disabled = false,
+  autoScroll = false,
+  comparator,
   equalityFn,
   renderValue,
   renderListItem,
@@ -60,8 +66,30 @@ export const Dropdown = <T extends unknown>({
   onLongPress
 }: DropdownProps<T> & DropdownValueProps<T>) => {
   const styles = useDropdownStyles();
+  const scrollRef = useRef<ScrollView>(null);
   const dropdownBottomSheetController = useBottomSheetController();
   const contentHeight = 0.7 * useWindowDimensions().height;
+  const [dataSourceCords, setDataSourceCords] = useState<Array<number>>([]);
+
+  const onPressTouch = useCallback(() => {
+    console.log(autoScroll, comparator, value);
+    if (!autoScroll || !isDefined(comparator) || !isDefined(value)) {
+      return;
+    }
+    const scrollToIndex = list.findIndex(x => x[comparator] === value[comparator]);
+    console.log('scroll', scrollToIndex, Boolean(scrollRef.current), dataSourceCords);
+    if (dataSourceCords.length > scrollToIndex && isDefined(scrollRef.current)) {
+      scrollRef.current.scrollTo({
+        x: 0,
+        y: dataSourceCords[scrollToIndex - 1],
+        animated: true
+      });
+    }
+  }, [scrollRef]);
+
+  useEffect(() => {
+    scrollRef.current ? onPressTouch() : setTimeout(onPressTouch, 50);
+  }, [scrollRef]);
 
   const createDropdownItemPressHandler = (item: T) => () => {
     onValueChange(item);
@@ -80,13 +108,22 @@ export const Dropdown = <T extends unknown>({
       </TouchableOpacity>
 
       <BottomSheet title={title} contentHeight={contentHeight} controller={dropdownBottomSheetController}>
-        <ScrollView style={styles.scrollView}>
+        <ScrollView style={styles.scrollView} ref={scrollRef}>
           <View style={styles.contentContainer}>
             {list.map((item, index) => {
               const isSelected = equalityFn(item, value);
 
               return (
-                <BottomSheetTouchableOpacity key={index} onPress={createDropdownItemPressHandler(item)}>
+                <BottomSheetTouchableOpacity
+                  key={index}
+                  onLayout={event => {
+                    const layout = event.nativeEvent.layout;
+                    // console.log(layout);
+                    dataSourceCords[index] = layout.height * index;
+                    setDataSourceCords(dataSourceCords);
+                  }}
+                  onPress={createDropdownItemPressHandler(item)}
+                >
                   <DropdownItemContainer hasMargin={true} isSelected={isSelected}>
                     {renderListItem({ item, isSelected })}
                   </DropdownItemContainer>
