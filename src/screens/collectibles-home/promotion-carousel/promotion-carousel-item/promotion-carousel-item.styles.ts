44ae88de8b8e5ec3e7c545699c8c69badee1e8a2
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-04-19 10:37:05 +0100

    added new promotional banners

--- src/screens/collectibles-home/promotion-carousel/promotion-carousel-item/promotion-carousel-item.styles.ts
+++ src/screens/collectibles-home/promotion-carousel/promotion-carousel-item/promotion-carousel-item.styles.ts
@@ -1,26 +1,15 @@
 import { createUseStyles } from '../../../../styles/create-use-styles';
 import { formatSize } from '../../../../styles/format-size';
 
-export const usePromotionCarouselItemStyles = createUseStyles(({ colors, typography }) => ({
+export const usePromotionCarouselItemStyles = createUseStyles(() => ({
   container: {
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingHorizontal: formatSize(35),
-    paddingVertical: formatSize(32),
-    backgroundColor: colors.blue10,
-    borderRadius: 10
+    borderRadius: formatSize(10)
   },
-  textContainer: {
-    flexDirection: 'column',
-    maxWidth: formatSize(185)
-  },
-  title: {
-    ...typography.body17Semibold,
-    color: colors.black
-  },
-  description: {
-    ...typography.caption11Regular,
-    color: colors.black
+  bannerImage: {
+    aspectRatio: 343 / 112,
+    maxWidth: '100%'
   }
 }));
---
+++ src/screens/collectibles-home/promotion-carousel/promotion-carousel.data.ts
@@ -0,0 +1,27 @@
+import { ImageSourcePropType } from 'react-native';
+
+import { madfishLink, quipuLink, supportUkraine, yupanaLink } from '../../../config/socials';
+
+export interface PromotionCarouselItemInterface {
+  src: ImageSourcePropType;
+  link: string;
+}
+
+export const promotionCarouselData: PromotionCarouselItemInterface[] = [
+  {
+    src: require('./banners/ukraine.png'),
+    link: supportUkraine
+  },
+  {
+    src: require('./banners/yupana.png'),
+    link: yupanaLink
+  },
+  {
+    src: require('./banners/quipu.png'),
+    link: quipuLink
+  },
+  {
+    src: require('./banners/madfish.png'),
+    link: madfishLink
+  }
+];
