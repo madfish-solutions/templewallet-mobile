0de0630c8c7b003c0ac77f1e79e687f48ffe2211
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-04-22 14:16:41 +0100

    fix buttons fixed on bottom of screen

--- src/modals/import-account-modal/import-account.styles.ts
+++ src/modals/import-account-modal/import-account.styles.ts
@@ -1,12 +1,20 @@
 import { createUseStyles } from '../../styles/create-use-styles';
 import { formatSize } from '../../styles/format-size';
 
-export const useImportAccountStyles = createUseStyles(() => ({
+export const useImportAccountStyles = createUseStyles(({ colors }) => ({
   container: {
     marginHorizontal: formatSize(20),
     marginVertical: formatSize(24)
   },
   seedPhraseInputContainer: {
     flexGrow: 1
+  },
+  submitButton: {
+    borderTopWidth: formatSize(0.5),
+    borderColor: colors.lines,
+    paddingTop: formatSize(8),
+    paddingBottom: formatSize(16),
+    paddingHorizontal: formatSize(8),
+    backgroundColor: colors.pageBG
   }
 }));
