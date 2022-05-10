20bc5f2f7ec659e957beb5d2228ae271be76491c
Andrii Ivaniv <unixvb@gmail.com>
2022-05-10 16:54:12 +0300

    redundant rerenders removed

--- src/utils/wallet-account-state.utils.ts
+++ src/utils/wallet-account-state.utils.ts
@@ -1,6 +1,6 @@
 import { omit } from 'lodash-es';
 
-import { WalletAccountStateInterface } from '../interfaces/wallet-account-state.interface';
+import { initialWalletAccountState, WalletAccountStateInterface } from '../interfaces/wallet-account-state.interface';
 import { initialWalletAccount, WalletAccountInterface } from '../interfaces/wallet-account.interface';
 
 export const walletAccountStateToWalletAccount = (
@@ -8,4 +8,12 @@ export const walletAccountStateToWalletAccount = (
 ): WalletAccountInterface => ({
   ...initialWalletAccount,
   ...omit(walletAccountState, ['activityGroups', 'pendingActivities'])
+});
+
+export const getWalletAccountState = (
+  accounts: WalletAccountStateInterface[],
+  selectedAccountPublicKeyHash: string
+): WalletAccountStateInterface => ({
+  ...initialWalletAccountState,
+  ...accounts.find(({ publicKeyHash }) => publicKeyHash === selectedAccountPublicKeyHash)
 });
