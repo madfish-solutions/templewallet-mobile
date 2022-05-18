3d9a8ddc77b6e07395b566322283f6a7b4f087af
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-05-18 13:31:06 +0100

    refactor dimensions

--- src/components/token-icon/token-icon.tsx
+++ src/components/token-icon/token-icon.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import { Image, View } from 'react-native';
 import { SvgCssUri } from 'react-native-svg';
 
-import { useFormatScaledSize } from '../../styles/format-scaled-size';
 import { formatSize } from '../../styles/format-size';
 import { TokenMetadataInterface } from '../../token/interfaces/token-metadata.interface';
 import { formatImgUri, isImgUriSvg } from '../../utils/image.utils';
@@ -19,20 +18,19 @@ interface Props {
 
 export const TokenIcon: FC<Props> = ({ token, size = formatSize(32) }) => {
   const { iconName, thumbnailUri } = token;
-  const imageSize = useFormatScaledSize(size);
 
   return (
-    <View style={[TokenIconStyles.container, { borderRadius: imageSize / 2 }]}>
+    <View style={[TokenIconStyles.container, { borderRadius: size / 2 }]}>
       {isDefined(iconName) ? (
-        <Icon name={iconName} size={imageSize} />
+        <Icon name={iconName} size={size} />
       ) : isString(thumbnailUri) ? (
         isImgUriSvg(thumbnailUri) ? (
-          <SvgCssUri width={imageSize} height={imageSize} uri={thumbnailUri} />
+          <SvgCssUri width={size} height={size} uri={thumbnailUri} />
         ) : (
-          <Image source={{ uri: formatImgUri(thumbnailUri), width: imageSize, height: imageSize }} />
+          <Image source={{ uri: formatImgUri(thumbnailUri), width: size, height: size }} />
         )
       ) : (
-        <Icon name={IconNameEnum.NoNameToken} size={imageSize} />
+        <Icon name={IconNameEnum.NoNameToken} size={size} />
       )}
     </View>
   );
--- src/styles/format-scaled-size.ts
+++
@@ -1,7 +0,0 @@
-import { useWindowDimensions } from 'react-native';
-
-export const useFormatScaledSize = (size: number): number => {
-  const { fontScale } = useWindowDimensions();
-
-  return size * fontScale;
-};
