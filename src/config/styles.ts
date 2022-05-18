ff0834c6e6541a094303b6c15ef345968fe9288e
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-05-18 16:27:18 +0100

    fix token-icon size

--- src/config/styles.ts
+++ src/config/styles.ts
@@ -3,8 +3,7 @@ import { Dimensions } from 'react-native';
 
 const basicScreenWidth = 375;
 const maxLayoutScale = 1.1;
-export const layoutScale =
-  clamp(Dimensions.get('screen').width / basicScreenWidth, maxLayoutScale) * Dimensions.get('window').fontScale;
+export const layoutScale = clamp(Dimensions.get('screen').width / basicScreenWidth, maxLayoutScale);
 
 export const greyLight = '#707070';
 export const greyLight200 = '#E5E5EA';
