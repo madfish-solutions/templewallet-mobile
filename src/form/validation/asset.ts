fc2f149057ebb1441a904fb98e6ff01bfaca198a
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-05-25 08:43:39 +0100

    Swap validation revert and improve (#545)
    * improvement for validation
    
    * refactored validation for token

---
+++ src/form/validation/asset.ts
@@ -0,0 +1,18 @@
+import { object, SchemaOf } from 'yup';
+
+import { tokenEqualityFn } from '../../components/token-dropdown/token-equality-fn';
+import { emptyTezosLikeToken, TokenInterface } from '../../token/interfaces/token.interface';
+import { isDefined } from '../../utils/is-defined';
+import { makeRequiredErrorMessage } from './messages';
+
+export const assetValidation: SchemaOf<TokenInterface> = object()
+  .shape({})
+  .required(makeRequiredErrorMessage('Token'))
+  .test('is-equal', 'Asset must be selected', (value: TokenInterface) => {
+    const isEqual = tokenEqualityFn(value, emptyTezosLikeToken);
+    if (isEqual || !isDefined(value.address) || !isDefined(value.symbol)) {
+      return false;
+    }
+
+    return true;
+  }) as SchemaOf<TokenInterface>;
