24afb1f1d859c7b8ce697ffe2b42d90714aac5ab
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-08 13:18:10 +0100

    fix tests

--- src/utils/activity.spec.ts
+++ src/utils/activity.spec.ts
@@ -3,10 +3,10 @@ import { groupActivitiesByHash } from './activity.utils';
 
 describe('groupActivitiesByHash', () => {
   it('should sort operations and transfers by timestamp in descending order', () => {
-    expect(groupActivitiesByHash(mockOperations, [], mockTransfers.slice(1))).toMatchSnapshot();
+    expect(groupActivitiesByHash(mockOperations, [], [], mockTransfers.slice(1))).toMatchSnapshot();
   });
 
   it('should push operations and transfers with the same hash into one group and not mutate activities', () => {
-    expect(groupActivitiesByHash(mockOperations, [], mockTransfers)).toMatchSnapshot();
+    expect(groupActivitiesByHash(mockOperations, [], [], mockTransfers)).toMatchSnapshot();
   });
 });
