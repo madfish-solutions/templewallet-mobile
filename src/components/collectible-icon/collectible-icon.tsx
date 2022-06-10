8764e13177cc998573b468c6a1bb3b80491a8e79
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-10 15:22:09 +0100

    fix urls

--- src/components/collectible-icon/collectible-icon.tsx
+++ src/components/collectible-icon/collectible-icon.tsx
@@ -2,15 +2,60 @@ import React, { FC, useState } from 'react';
 import { Image, View } from 'react-native';
 
 import { formatSize } from '../../styles/format-size';
-import { formatCollectibleUri, formatImgUri } from '../../utils/image.utils';
+import { formatCollectibleObjktBigUri, formatCollectibleObjktMediumUri, formatImgUri } from '../../utils/image.utils';
 import { isDefined } from '../../utils/is-defined';
 import { CollectibleIconProps } from './collectible-icon.props';
 import { useCollectibleIconStyles } from './collectible-icon.styles';
 
+interface LoadStrategy {
+  type: string;
+  uri: (value: string) => string;
+  field: 'thumbnailUri' | 'artifactUri' | 'displayUri' | 'assetSlug';
+}
+
+const collectibleLoadStrategy: Array<LoadStrategy> = [
+  { type: 'objktBig', uri: formatCollectibleObjktBigUri, field: 'assetSlug' },
+  { type: 'objktMed', uri: formatCollectibleObjktMediumUri, field: 'assetSlug' },
+  { type: 'displayUri', uri: formatImgUri, field: 'displayUri' },
+  { type: 'artifactUri', uri: formatImgUri, field: 'artifactUri' },
+  { type: 'thumbnailUri', uri: formatImgUri, field: 'thumbnailUri' }
+];
+
+type ImageRequestObject = { assetSlug: string; displayUri?: string; artifactUri?: string; thumbnailUri?: string };
+
+const getFirstFallback = (
+  strategy: Array<LoadStrategy>,
+  currentState: Record<string, boolean>,
+  metadata: ImageRequestObject
+): LoadStrategy => {
+  for (const strategyItem of strategy) {
+    const isArtifactUri = isDefined(metadata.artifactUri) && strategyItem.field === 'artifactUri';
+    const isDisplayUri = isDefined(metadata.displayUri) && strategyItem.field === 'displayUri';
+    const isThumbnailUri = isDefined(metadata.thumbnailUri) && strategyItem.field === 'thumbnailUri';
+    if ((isArtifactUri || isDisplayUri || isThumbnailUri) && !currentState[strategyItem.type]) {
+      return strategyItem;
+    }
+  }
+
+  return strategy[0];
+};
+
 export const CollectibleIcon: FC<CollectibleIconProps> = ({ collectible, size }) => {
   const styles = useCollectibleIconStyles();
   const [isLoaded, setIsLoaded] = useState(false);
-  const [isLoadingFailed, setIsLoadingFailed] = useState(false);
+  // const [isLoadingFailed, setIsLoadingFailed] = useState(false);
+  const [isLoadingFailed, setIsLoadingFailed] = useState(
+    collectibleLoadStrategy.reduce<Record<string, boolean>>((acc, cur) => ({ ...acc, [cur.type]: false }), {})
+  );
+  const assetSlug = `${collectible?.address}_${collectible?.id}`;
+
+  const imageRequestObject = { ...collectible, assetSlug };
+  const currentFallback = getFirstFallback(collectibleLoadStrategy, isLoadingFailed, imageRequestObject);
+  const imageSrc = currentFallback.uri(imageRequestObject[currentFallback.field] ?? assetSlug);
+
+  const handleLoadingFailed = () => {
+    setIsLoadingFailed(prevState => ({ ...prevState, [currentFallback.type]: true }));
+  };
 
   return (
     <View
@@ -24,12 +69,12 @@ export const CollectibleIcon: FC<CollectibleIconProps> = ({ collectible, size })
         <Image
           style={styles.image}
           source={{
-            uri: isLoadingFailed ? formatImgUri(collectible.thumbnailUri) : formatCollectibleUri(collectible),
+            uri: imageSrc,
             width: size,
             height: size
           }}
           blurRadius={isLoaded ? 0 : 5}
-          onError={() => setIsLoadingFailed(true)}
+          onError={handleLoadingFailed}
           onLoadEnd={() => setIsLoaded(true)}
         />
       )}
--- src/utils/image.utils.ts
+++ src/utils/image.utils.ts
@@ -1,8 +1,8 @@
-import { TokenInterface } from '../token/interfaces/token.interface';
-
 const IPFS_GATEWAY = 'cloudflare-ipfs.com';
 const IPFS_PROTOCOL_PREFIX = 'ipfs://';
-const OBJKT_ORIGIN = 'https://assets.objkt.media/file/assets-001';
+const OBJKT_ORIGIN = 'https://assets.objkt.media/file';
+const OBJKT_RESIZE_1 = 'assets-001';
+const OBJKT_RESIZE_3 = 'assets-003';
 
 export const formatImgUri = (origin: string) =>
   origin.startsWith(IPFS_PROTOCOL_PREFIX)
@@ -11,9 +11,16 @@ export const formatImgUri = (origin: string) =>
 
 export const isImgUriSvg = (url: string) => url.endsWith('.svg');
 
-export const formatCollectibleUri = (collectible: TokenInterface) => {
-  const { address } = collectible;
-  const id = `${collectible.id}`;
+export const formatCollectibleObjktBigUri = (assetSlug: string) => {
+  const [address, id] = assetSlug.split('_');
 
-  return `${OBJKT_ORIGIN}/${address}/${id.length > 1 ? id[id.length - 2] : 0}/${id[id.length - 1]}/${id}/thumb400`;
+  return `${OBJKT_ORIGIN}/${OBJKT_RESIZE_1}/${address}/${id.length > 1 ? id[id.length - 2] : 0}/${
+    id[id.length - 1]
+  }/${id}/thumb400`;
+};
+
+export const formatCollectibleObjktMediumUri = (assetSlug: string) => {
+  const [address, id] = assetSlug.split('_');
+
+  return `${OBJKT_ORIGIN}/${OBJKT_RESIZE_3}/${address}/${id}/thumb288`;
 };
