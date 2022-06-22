3be5213ce51bcf31077393e8dd494d28db2fa6de
letier3110 <38981658+letier3110@users.noreply.github.com>
2022-06-22 11:44:57 +0100

    implement saved in redux state with 0 balances

--- src/screens/manage-assets/manage-tokens/manage-tokens.tsx
+++ src/screens/manage-assets/manage-tokens/manage-tokens.tsx
@@ -20,7 +20,7 @@ export const ManageTokens = () => {
   const { navigate } = useNavigation();
 
   const tokensList = useTokensListSelector();
-  const { filteredAssetsList, setSearchValue } = useFilteredAssetsList(tokensList);
+  const { filteredAssetsList, setSearchValue } = useFilteredAssetsList(tokensList, false);
 
   return (
     <>
