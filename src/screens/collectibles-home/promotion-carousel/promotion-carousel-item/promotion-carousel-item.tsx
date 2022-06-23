a65df933e7429c25dbb4d13cf3f5d347c76ee850
Andrii Ivaniv <unixvb@users.noreply.github.com>
2022-06-23 12:46:22 +0300

    Images render optimizing (#573)
    * tezos token loadable & error state removed to decrease renders
    
    * TezosTokenHistory redundant initial render removed
    
    * Token icon image rerenders decreased
    
    * redundant render removed
    
    * token list rendered with flat list
    
    * token list empty state render fix
    
    * isShownDomainName migrated to settings
    
    * quipuApy migrated to settings dapps state
    
    * tokens metadata state extracted
    
    * activityGroups loading state removed
    
    * tokens metadata state
    
    * tokens metadata state
    
    * accounts state flatted
    
    * activities selector fixed
    
    * data fetching reduced
    
    * reverting ios changes
    
    * lint fixes
    
    * images render optimization

--- src/screens/collectibles-home/promotion-carousel/promotion-carousel-item/promotion-carousel-item.tsx
+++ src/screens/collectibles-home/promotion-carousel/promotion-carousel-item/promotion-carousel-item.tsx
@@ -1,16 +1,17 @@
 import React, { FC, memo } from 'react';
-import { Image, TouchableOpacity } from 'react-native';
+import { TouchableOpacity } from 'react-native';
+import FastImage from 'react-native-fast-image';
 
 import { openUrl } from '../../../../utils/linking.util';
 import { PromotionCarouselItemInterface } from '../promotion-carousel.data';
 import { usePromotionCarouselItemStyles } from './promotion-carousel-item.styles';
 
-export const PromotionCarouselItem: FC<PromotionCarouselItemInterface> = memo(({ src, link }) => {
+export const PromotionCarouselItem: FC<PromotionCarouselItemInterface> = memo(({ source, link }) => {
   const styles = usePromotionCarouselItemStyles();
 
   return (
     <TouchableOpacity style={styles.container} onPress={() => openUrl(link)}>
-      <Image style={styles.bannerImage} source={src} />
+      <FastImage style={styles.bannerImage} source={source} />
     </TouchableOpacity>
   );
 });
