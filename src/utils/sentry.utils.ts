d4ab837b1b2caeb0284a5f6a515fa3e4a8450c3a
LendiHop <mishin.danil2017@gmail.com>
2022-06-27 14:38:09 +0200

    Sentry update

--- src/utils/sentry.utils.ts
+++ src/utils/sentry.utils.ts
@@ -1,4 +1,4 @@
 import * as Sentry from '@sentry/react-native';
 
 export const initSentry = () =>
-  !__DEV__ && Sentry.init({ dsn: 'https://bdcaf035c39848a3a6203b837d431618@o946691.ingest.sentry.io/5895740' });
+  !__DEV__ && Sentry.init({ dsn: 'https://e486e060eb034ad28b4261e41520871c@o911994.ingest.sentry.io/5848633' });
