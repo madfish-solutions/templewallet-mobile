2f49f9befbd49b5199254482d69e79a7aaf97260
LendiHop <mishin.danil2017@gmail.com>
2022-07-07 18:51:13 +0200

    force update version check fix

--- src/modals/confirmation-modal/d-app-operations-confirmation/loading-request/loading-request.tsx
+++ src/modals/confirmation-modal/d-app-operations-confirmation/loading-request/loading-request.tsx
@@ -2,12 +2,11 @@ import React, { FC } from 'react';
 
 import { ButtonLargeSecondary } from '../../../../components/button/button-large/button-large-secondary/button-large-secondary';
 import { Disclaimer } from '../../../../components/disclaimer/disclaimer';
-import { Divider } from '../../../../components/divider/divider';
 import { LoadingPlaceholder } from '../../../../components/loading-placeholder/loading-placeholder';
 import { ModalButtonsContainer } from '../../../../components/modal-buttons-container/modal-buttons-container';
 import { ScreenContainer } from '../../../../components/screen-container/screen-container';
 import { useNavigation } from '../../../../navigator/hooks/use-navigation.hook';
-import { formatSize } from '../../../../styles/format-size';
+import { useLoadingRequestStyles } from './loading-request.styles';
 
 const texts = [
   '1. go back to your browser;',
@@ -17,13 +16,13 @@ const texts = [
 
 export const LoadingRequest: FC = () => {
   const { goBack } = useNavigation();
+  const styles = useLoadingRequestStyles();
 
   return (
     <>
-      <ScreenContainer>
-        <Divider size={formatSize(16)} />
-        <Disclaimer title="Attention! If loading takes too long:" texts={texts} />
+      <ScreenContainer contentContainerStyle={styles.container}>
         <LoadingPlaceholder text="Operation is loading..." />
+        <Disclaimer title="If loading takes too long:" texts={texts} />
       </ScreenContainer>
       <ModalButtonsContainer>
         <ButtonLargeSecondary title="Close" onPress={goBack} />
