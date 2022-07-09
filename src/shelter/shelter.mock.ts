17990c68dc3cc95f53db88124415eb970e533a1a
Andrii Ivaniv <unixvb@gmail.com>
2022-07-09 11:52:05 +0300

    android build fix

--- src/shelter/shelter.mock.ts
+++ src/shelter/shelter.mock.ts
@@ -1,4 +1,4 @@
-import * as Keychain from 'react-native-keychain';
+import Keychain from 'react-native-keychain';
 import { BehaviorSubject, of } from 'rxjs';
 
 import { mockHdAccount } from '../interfaces/account.interface.mock';
