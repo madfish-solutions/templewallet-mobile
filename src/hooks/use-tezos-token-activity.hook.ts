39ee9a3105442d3b0beb451f0c82a87e1cd06c9b
letier3110 <letier3110@gmail.com>
2022-08-04 11:02:55 +0100

    wip

--- src/hooks/use-tezos-token-activity.hook.ts
+++ src/hooks/use-tezos-token-activity.hook.ts
@@ -1,11 +1,10 @@
+import { uniqBy } from 'lodash-es';
 import { useCallback, useEffect, useState } from 'react';
 
 import { ActivityGroup } from '../interfaces/activity.interface';
 import { OperationInterface, OperationLiquidityBakingInterface } from '../interfaces/operation.interface';
 import { UseActivityInterface } from '../interfaces/use-activity.interface';
 import { useSelectedAccountSelector } from '../store/wallet/wallet-selectors';
-import { transformActivityInterfaceToActivityGroups } from '../utils/activity.utils';
-import { deduplicate } from '../utils/array.utils';
 import { isDefined } from '../utils/is-defined';
 import { mapOperationsToActivities } from '../utils/operation.utils';
 import { getOperationGroupByHash, getTezosOperations } from '../utils/token-operations.util';
@@ -20,20 +19,20 @@ export const useTezosTokenActivity = (): UseActivityInterface => {
     async (lastId: number | null) => {
       const operations = await getTezosOperations(publicKeyHash, lastId);
 
-      const filteredOperations = deduplicate<OperationInterface>(operations, (a, b) => a.hash === b.hash);
+      const filteredOperations = uniqBy<OperationInterface>(operations, 'hash');
 
       setIsAllLoaded(filteredOperations.length === 0);
 
-      const operationGroups = (
-        await Promise.all(
-          filteredOperations.map(x =>
-            getOperationGroupByHash<OperationLiquidityBakingInterface>(x.hash).then(x => x.data)
-          )
+      const operationGroups = await Promise.all(
+        filteredOperations.map(x =>
+          getOperationGroupByHash<OperationLiquidityBakingInterface>(x.hash).then(x => x.data)
         )
-      ).flat();
+      );
 
-      const loadedActivities = mapOperationsToActivities(publicKeyHash, operationGroups);
-      const activityGroups = transformActivityInterfaceToActivityGroups(loadedActivities);
+      const activityGroups: Array<ActivityGroup> = [];
+      for (const group of operationGroups) {
+        activityGroups.push(mapOperationsToActivities(publicKeyHash, group));
+      }
 
       setActivities(prevValue => [...prevValue, ...activityGroups]);
     },
--- src/utils/token-operations.util.ts
+++ src/utils/token-operations.util.ts
@@ -7,10 +7,16 @@ import { isDefined } from './is-defined';
 export const getOperationGroupByHash = <T>(hash: string) => tzktApi.get<Array<T>>(`operations/${hash}`);
 
 export const getContractOperations = <T>(account: string, contractAddress: string, lastLevel: number | null) =>
-  tzktApi.get<Array<T>>(
-    `accounts/${contractAddress}/operations?type=transaction&limit=40&sort=1&quote=usd&initiator=${account}&entrypoint=mintOrBurn` +
-      (isDefined(lastLevel) ? `&level.lt=${lastLevel}` : '')
-  );
+  tzktApi.get<Array<T>>(`accounts/${contractAddress}/operations`, {
+    params: {
+      type: 'transaction',
+      limit: OPERATION_LIMIT,
+      sort: '1',
+      initiator: account,
+      entrypoint: 'mintOrBurn',
+      ...(isDefined(lastLevel) ? { 'level.lt': lastLevel } : undefined)
+    }
+  });
 
 export const getTokenFa2Operations = async (
   account: string,
@@ -19,48 +25,83 @@ export const getTokenFa2Operations = async (
   lastLevel: number | null
 ) =>
   (
-    await tzktApi.get<Array<OperationFa2Interface>>(
-      `operations/transactions?limit=${OPERATION_LIMIT}&entrypoint=transfer&parameter.[*].in=[{"from_":"${account}","txs":[{"token_id":"${tokenId}"}]},{"txs":[{"to_":"${account}","token_id":"${tokenId}"}]}]&sort.desc=level&target=${contractAddress}` +
-        (isDefined(lastLevel) ? `&level.lt=${lastLevel}` : '')
-    )
+    await tzktApi.get<Array<OperationFa2Interface>>('operations/transactions', {
+      params: {
+        limit: OPERATION_LIMIT,
+        entrypoint: 'transfer',
+        'sort.desc': 'level',
+        target: contractAddress,
+        'parameter.[*].in': `[{"from_":"${account}","txs":[{"token_id":"${tokenId}"}]},{"txs":[{"to_":"${account}","token_id":"${tokenId}"}]}]`,
+        ...(isDefined(lastLevel) ? { 'level.lt': lastLevel } : undefined)
+      }
+    })
   ).data;
 
 export const getTokenFa12Operations = async (account: string, contractAddress: string, lastLevel: number | null) =>
   (
-    await tzktApi.get<Array<OperationFa12Interface>>(
-      `operations/transactions?limit=${OPERATION_LIMIT}&entrypoint=transfer&parameter.in=[{"from":"${account}"},{"to":"${account}"}]&sort.desc=level&target=${contractAddress}` +
-        (isDefined(lastLevel) ? `&level.lt=${lastLevel}` : '')
-    )
+    await tzktApi.get<Array<OperationFa12Interface>>('operations/transactions', {
+      params: {
+        limit: OPERATION_LIMIT,
+        entrypoint: 'transfer',
+        'sort.desc': 'level',
+        target: contractAddress,
+        'parameter.in': `[{"from":"${account}"},{"to":"${account}"}]`,
+        ...(isDefined(lastLevel) ? { 'level.lt': lastLevel } : undefined)
+      }
+    })
   ).data;
 
 export const getTezosOperations = async (account: string, lastLevel: number | null) =>
   (
-    await tzktApi.get<Array<OperationInterface>>(
-      `accounts/${account}/operations?limit=${OPERATION_LIMIT}&type=${ActivityTypeEnum.Transaction}&sort=1&parameter.null` +
-        (isDefined(lastLevel) ? `&lastId=${lastLevel}` : '')
-    )
+    await tzktApi.get<Array<OperationInterface>>(`accounts/${account}/operations`, {
+      params: {
+        limit: OPERATION_LIMIT,
+        type: ActivityTypeEnum.Transaction,
+        sort: '1',
+        'parameter.null': true,
+        ...(isDefined(lastLevel) ? { lastId: lastLevel } : undefined)
+      }
+    })
   ).data;
 
 export const getTokenOperations = async (account: string, lastId: number | null) =>
   (
-    await tzktApi.get<Array<OperationInterface>>(
-      `accounts/${account}/operations?limit=${OPERATION_LIMIT}&sort=1&type=${ActivityTypeEnum.Delegation},${ActivityTypeEnum.Origination},${ActivityTypeEnum.Transaction}` +
-        (isDefined(lastId) ? `&lastId=${lastId}` : '')
-    )
+    await tzktApi.get<Array<OperationInterface>>(`accounts/${account}/operations`, {
+      params: {
+        limit: OPERATION_LIMIT,
+        type: `${ActivityTypeEnum.Delegation},${ActivityTypeEnum.Origination},${ActivityTypeEnum.Transaction}`,
+        sort: '1',
+        ...(isDefined(lastId) ? { lastId } : undefined)
+      }
+    })
   ).data;
 
 export const getFa12IncomingOperations = async (account: string, lowerId: number, upperId: number | null) =>
   (
-    await tzktApi.get<Array<OperationFa12Interface>>(
-      `operations/transactions?sender.ne=${account}&target.ne=${account}&initiator.ne=${account}&id.gt=${lowerId}&entrypoint=transfer&parameter.to=${account}` +
-        (isDefined(upperId) ? `&id.lt=${upperId}` : '')
-    )
+    await tzktApi.get<Array<OperationFa12Interface>>('operations/transactions', {
+      params: {
+        'sender.ne': account,
+        'target.ne': account,
+        'initiator.ne': account,
+        'id.gt': lowerId,
+        entrypoint: 'transfer',
+        'parameter.to': account,
+        ...(isDefined(upperId) ? { 'id.lt': upperId } : undefined)
+      }
+    })
   ).data;
 
 export const getFa2IncomingOperations = async (account: string, lowerId: number, upperId: number | null) =>
   (
-    await tzktApi.get<Array<OperationFa2Interface>>(
-      `operations/transactions?sender.ne=${account}&target.ne=${account}&initiator.ne=${account}&id.gt=${lowerId}&entrypoint=transfer&parameter.[*].txs.[*].to_=${account}` +
-        (isDefined(upperId) ? `&id.lt=${upperId}` : '')
-    )
+    await tzktApi.get<Array<OperationFa2Interface>>('operations/transactions', {
+      params: {
+        'sender.ne': account,
+        'target.ne': account,
+        'initiator.ne': account,
+        'id.gt': lowerId,
+        entrypoint: 'transfer',
+        'parameter.[*].txs.[*].to_': account,
+        ...(isDefined(upperId) ? { 'id.lt': upperId } : undefined)
+      }
+    })
   ).data;
