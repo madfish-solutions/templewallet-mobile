460466d4389b655a7f7c2c2afa6a5d19d77dbe43
letier3110 <letier3110@gmail.com>
2022-08-08 16:04:07 +0100

    fix mock and tests

--- src/token/interfaces/account-token.interface.mock.ts
+++ src/token/interfaces/account-token.interface.mock.ts
@@ -1,7 +1,48 @@
 import { VisibilityEnum } from '../../enums/visibility.enum';
-import { MAINNET_TOKENS_METADATA } from '../data/tokens-metadata';
 import { getTokenSlug } from '../utils/token.utils';
 import { AccountTokenInterface } from './account-token.interface';
+
+export const MAINNET_TOKENS_METADATA = [
+  {
+    id: 0,
+    address: 'KT193D4vozYnhGJQVtw7CoxxqphqUEEwK6Vb',
+    name: 'Quipuswap governance token',
+    symbol: 'QUIPU',
+    decimals: 6,
+    thumbnailUri: 'https://quipuswap.com/tokens/quipu.png'
+  },
+  {
+    id: 0,
+    address: 'KT1PWx2mnDueood7fEmfbBDKx1D9BAnnXitn',
+    name: 'Tezos BTC',
+    symbol: 'tzBTC',
+    decimals: 8
+  },
+  {
+    id: 19,
+    address: 'KT18fp5rcTW7mbWDmzFwjLDUhs5MeJmagDSZ',
+    name: 'Wrapped WBTC',
+    symbol: 'wWBTC',
+    decimals: 8,
+    thumbnailUri: 'ipfs://Qmdj6n9T48LDWex8NkBMKUQJfZgardxZVdtRRibYQVzLCJ'
+  },
+  {
+    id: 0,
+    address: 'KT1K9gCRgaLRFKTErYt1wVxA3Frb9FjasjTV',
+    name: 'Kolibri',
+    symbol: 'kUSD',
+    decimals: 18,
+    thumbnailUri: 'https://kolibri-data.s3.amazonaws.com/logo.png'
+  },
+  {
+    id: 0,
+    address: 'KT1XRPEPXbZK25r3Htzp2o1x7xdMMmfocKNW',
+    name: 'youves uUSD',
+    symbol: 'uUSD',
+    decimals: 12,
+    thumbnailUri: 'ipfs://QmbvhanNCxydZEbGu1RdqkG3LcpNGv7XYsCHgzWBXnmxRd'
+  }
+];
 
 export const mockAccountTokens: AccountTokenInterface[] = MAINNET_TOKENS_METADATA.map(token => ({
   slug: getTokenSlug(token),
