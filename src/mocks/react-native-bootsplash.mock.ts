6048d60d84f08e298b3a1bea1b36c5df472c954c
letier3110 <letier3110@gmail.com>
2022-08-08 12:21:29 +0100

    added splash

--- src/mocks/react-native-bootsplash.mock.ts
+++ src/mocks/react-native-bootsplash.mock.ts
@@ -1,7 +1,6 @@
 export const mockReactNativeBootsplash = {
   ...jest.requireActual('react-native-bootsplash'),
-  hide: jest.fn(),
-  show: jest.fn()
+  hide: jest.fn()
 };
 
 jest.mock('react-native-bootsplash', () => mockReactNativeBootsplash);
