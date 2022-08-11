7cabc1c8f8c69f6733bce5c0dc272338d84c6578
lendihop <mishin.danil2017@gmail.com>
2022-08-11 12:42:37 +0200

    TW-159: Display icons for tezDAO, Upsorber

--- src/utils/image.utils.ts
+++ src/utils/image.utils.ts
@@ -1,6 +1,5 @@
 const IPFS_PROTOCOL_PREFIX = 'ipfs://';
 const OBJKT_ORIGIN = 'https://assets.objkt.media/file';
-const OBJKT_RESIZE_1 = 'assets-001';
 const OBJKT_RESIZE_3 = 'assets-003';
 
 const MEDIA_HOST = 'https://static.tcinfra.net';
@@ -19,14 +18,6 @@ export const formatImgUri = (origin = '') => {
 };
 
 export const isImgUriSvg = (url: string) => url.endsWith('.svg');
-
-export const formatCollectibleObjktBigUri = (assetSlug: string) => {
-  const [address, id] = assetSlug.split('_');
-
-  return `${OBJKT_ORIGIN}/${OBJKT_RESIZE_1}/${address}/${id.length > 1 ? id[id.length - 2] : 0}/${
-    id[id.length - 1]
-  }/${id}/thumb400`;
-};
 
 export const formatCollectibleObjktMediumUri = (assetSlug: string) => {
   const [address, id] = assetSlug.split('_');
