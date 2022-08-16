3865ebda3adc74cfb4d1637afc3ab52d522cdbcb
lendihop <mishin.danil2017@gmail.com>
2022-08-16 14:25:41 +0200

    analytics added

--- src/screens/collectibles-home/promotion-carousel/promotion-carousel.data.ts
+++ src/screens/collectibles-home/promotion-carousel/promotion-carousel.data.ts
@@ -1,26 +1,32 @@
 import { Source } from 'react-native-fast-image';
 
 import { madfishLink, quipuLink, supportUkraine, yupanaLink } from '../../../config/socials';
+import { PromotionCarouselSelectors } from './promotion-carousel.selectors';
 
 export interface PromotionCarouselItemInterface {
+  testID: string;
   source: Source;
   link: string;
 }
 
 export const promotionCarouselData: PromotionCarouselItemInterface[] = [
   {
+    testID: PromotionCarouselSelectors.MadFishBlog,
     source: require('./banners/madfish.png'),
     link: madfishLink
   },
   {
+    testID: PromotionCarouselSelectors.Yupana,
     source: require('./banners/yupana.png'),
     link: yupanaLink
   },
   {
+    testID: PromotionCarouselSelectors.QuipuSwap,
     source: require('./banners/quipu.png'),
     link: quipuLink
   },
   {
+    testID: PromotionCarouselSelectors.SupportUkraine,
     source: require('./banners/ukraine.png'),
     link: supportUkraine
   }
