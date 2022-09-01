9ff090a75888d1591f50b06ccc4aaa61d3d9c67b
Andrii Ivaniv <unixvb@users.noreply.github.com>
2022-09-01 17:08:04 +0300

    TW-222: Change Temple rpc url (#657) (#658)
    * TW-222: Change Temple rpc url
    
    * TW-222: Change Temple rpc url
    
    Co-authored-by: Danyl Mishyn <35381314+LendiHop@users.noreply.github.com>

--- src/utils/rpc/rpc-list.ts
+++ src/utils/rpc/rpc-list.ts
@@ -5,11 +5,15 @@ export const DCP_RPC: RpcInterface = {
   url: 'https://rpc.decentralized.pictures'
 };
 
+export const TEMPLE_RPC: RpcInterface = {
+  name: 'Temple Default',
+  url: 'https://uoi3x99n7c.tezosrpc.midl.dev'
+};
+
+export const OLD_TEMPLE_RPC_URL = 'https://mainnet-node.madfish.solutions';
+
 export const RpcList: RpcInterface[] = [
-  {
-    name: 'Temple Default',
-    url: 'https://mainnet-node.madfish.solutions'
-  },
+  TEMPLE_RPC,
   {
     name: 'Tezos Giga Node',
     url: 'https://mainnet-tezos.giganode.io'
