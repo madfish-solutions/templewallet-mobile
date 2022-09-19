82a6905452916cb6682b3c01f4c7fabc8e8c5e83
Andrii Ivaniv <unixvb@gmail.com>
2022-09-19 17:27:54 +0300

    camera fix

--- postinstall.sh
+++ postinstall.sh
@@ -5,3 +5,7 @@ replace_string="implementation 'com.facebook.react:react-native:+'"
 
 sed -i "" "s/$search_string/$replace_string/" node_modules/react-native-os/android/build.gradle
 sed -i "" "s/$search_string/$replace_string/" node_modules/react-native-scrypt/android/build.gradle
+
+
+sed -i "" "s/  ViewPropTypes,//" node_modules/react-native-camera/src/RNCamera.js
+sed -i "" "s/} from 'react-native';/} from 'react-native'\nimport { ViewPropTypes } from 'deprecated-react-native-prop-types'/" node_modules/react-native-camera/src/RNCamera.js
--- src/screens/scan-qr-code/scan-qr-code.tsx
+++ src/screens/scan-qr-code/scan-qr-code.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import { View } from 'react-native';
-import { useCameraDevices, Camera } from 'react-native-vision-camera';
-import { BarcodeFormat, useScanBarcodes } from 'vision-camera-code-scanner';
+import React from 'react';
+import { BarCodeReadEvent } from 'react-native-camera';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 import { beaconDeepLinkHandler } from '../../beacon/use-beacon-handler.hook';
 import { useNavigationSetOptions } from '../../components/header/use-navigation-set-options.hook';
@@ -14,7 +13,6 @@ import { useIsAuthorisedSelector, useSelectedAccountTezosTokenSelector } from '.
 import { showErrorToast } from '../../toast/toast.utils';
 import { usePageAnalytic } from '../../utils/analytics/use-analytics.hook';
 import { isBeaconPayload } from '../../utils/beacon.utils';
-import { isDefined } from '../../utils/is-defined';
 import { isSyncPayload } from '../../utils/sync.utils';
 import { isValidAddress } from '../../utils/tezos.util';
 import CustomMarker from './custom-marker.svg';
@@ -28,65 +26,53 @@ export const ScanQrCode = () => {
 
   const { metadata } = useNetworkInfo();
 
-  const [hasPermission, setHasPermission] = useState(false);
-
-  const devices = useCameraDevices();
-  const device = devices.back;
-  const isCameraAvailable = device != null && hasPermission;
-
-  const [frameProcessor, qrCodes] = useScanBarcodes([BarcodeFormat.QR_CODE], {
-    checkInverted: true
-  });
-
   usePageAnalytic(ScreensEnum.ScanQrCode);
-  useNavigationSetOptions({ headerTransparent: true }, []);
 
-  useEffect(() => void Camera.requestCameraPermission().then(status => setHasPermission(status === 'authorized')), []);
-
-  useEffect(() => {
-    const data = qrCodes[0]?.displayValue;
-
-    if (hasPermission && isDefined(data)) {
-      goBack();
-
-      if (isAuthorised) {
-        if (isValidAddress(data)) {
-          if (Number(tezosToken.balance) > 0) {
-            navigate(ModalsEnum.Send, { token: metadata, receiverPublicKeyHash: data });
-          } else {
-            showErrorToast({ description: `You need to have ${metadata.symbol} to pay gas fee` });
+  const handleRead = ({ data }: BarCodeReadEvent) => {
+    goBack();
+    if (isAuthorised) {
+      if (isValidAddress(data)) {
+        if (Number(tezosToken.balance) > 0) {
+          navigate(ModalsEnum.Send, { token: metadata, receiverPublicKeyHash: data });
+        } else {
+          showErrorToast({ description: `You need to have ${metadata.symbol} to pay gas fee` });
+        }
+      } else if (isBeaconPayload(data)) {
+        beaconDeepLinkHandler(
+          data,
+          () =>
+            navigate(ModalsEnum.Confirmation, {
+              type: ConfirmationTypeEnum.DAppOperations,
+              message: null,
+              loading: true
+            }),
+          errorMessage => {
+            goBack();
+            showErrorToast({ description: errorMessage });
           }
-        } else if (isBeaconPayload(data)) {
-          beaconDeepLinkHandler(
-            data,
-            () =>
-              navigate(ModalsEnum.Confirmation, {
-                type: ConfirmationTypeEnum.DAppOperations,
-                message: null,
-                loading: true
-              }),
-            errorMessage => {
-              goBack();
-              showErrorToast({ description: errorMessage });
-            }
-          );
-        } else {
-          showErrorToast({ description: 'Invalid QR code' });
-        }
+        );
       } else {
-        if (isSyncPayload(data)) {
-          navigate(ScreensEnum.ConfirmSync, { payload: data });
-        } else {
-          showErrorToast({ description: 'Invalid QR code' });
-        }
+        showErrorToast({ description: 'Invalid QR code' });
+      }
+    } else {
+      if (isSyncPayload(data)) {
+        navigate(ScreensEnum.ConfirmSync, { payload: data });
+      } else {
+        showErrorToast({ description: 'Invalid QR code' });
       }
     }
-  }, [qrCodes.length]);
+  };
 
-  return isCameraAvailable ? (
-    <View>
-      <Camera style={styles.camera} device={device} isActive frameProcessor={frameProcessor} frameProcessorFps={5} />
-      <CustomMarker />
-    </View>
-  ) : null;
+  useNavigationSetOptions({ headerTransparent: true }, []);
+
+  return (
+    <>
+      <QRCodeScanner
+        cameraStyle={styles.camera}
+        showMarker={true}
+        customMarker={<CustomMarker />}
+        onRead={handleRead}
+      />
+    </>
+  );
 };
