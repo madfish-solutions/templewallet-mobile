fcfc90602efd63c0581716cdfb6c52277041d17c
letier3110 <letier3110@gmail.com>
2022-09-20 17:23:05 +0100

    add timer clearing

--- src/hooks/use-refresh.hook.ts
+++ src/hooks/use-refresh.hook.ts
@@ -1,14 +1,26 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+
+import { isDefined } from '../utils/is-defined';
 
 export const useRefresh = () => {
   const [isRefreshing, setRefreshing] = useState(false);
 
+  const timerRef = useRef<NodeJS.Timeout>();
+
   const handleRefresh = () => {
     setRefreshing(true);
-    setTimeout(() => {
+    timerRef.current = setTimeout(() => {
       setRefreshing(false);
     }, Math.random() * 2000 + 2000);
   };
+
+  useEffect(() => {
+    return () => {
+      if (isDefined(timerRef.current)) {
+        clearTimeout(timerRef.current);
+      }
+    };
+  }, []);
 
   return {
     isRefreshing,
