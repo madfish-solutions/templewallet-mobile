c59d0dc3a7d811a7fc037d130203c70bd998415a
letier3110 <letier3110@gmail.com>
2022-10-05 09:00:42 +0100

    fix: clear loader on change selected account

--- src/hooks/use-refresh.hook.ts
+++ src/hooks/use-refresh.hook.ts
@@ -1,5 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
+import { useSelectedAccountSelector } from '../store/wallet/wallet-selectors';
 import { useActiveTimer } from './use-active-timer.hook';
 
 const FIXED_TIME = 2000;
@@ -8,12 +9,18 @@ const VARIABLE_TIME = 2000;
 export const useRefresh = () => {
   const [isRefreshing, setRefreshing] = useState(false);
   const { activeTimer, clearActiveTimer } = useActiveTimer();
+  const { publicKeyHash } = useSelectedAccountSelector();
 
   const handleRefresh = () => {
     clearActiveTimer();
     setRefreshing(true);
     activeTimer.current = setTimeout(() => setRefreshing(false), Math.random() * VARIABLE_TIME + FIXED_TIME);
   };
+
+  useEffect(() => {
+    setRefreshing(false);
+    clearActiveTimer();
+  }, [publicKeyHash]);
 
   return {
     isRefreshing,
