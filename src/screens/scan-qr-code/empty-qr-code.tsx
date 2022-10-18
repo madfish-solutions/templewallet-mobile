f9c311eddaae8e96409e94e1658e52d1e93de804
letier3110 <letier3110@gmail.com>
2022-10-18 13:18:47 +0100

    fix eslint

--- src/screens/scan-qr-code/empty-qr-code.tsx
+++ src/screens/scan-qr-code/empty-qr-code.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { View } from 'react-native';
 import { appDetailsSettings } from 'react-native-android-open-settings';
+
 import { ButtonLargePrimary } from '../../components/button/button-large/button-large-primary/button-large-primary';
 import { ButtonsFloatingContainer } from '../../components/button/buttons-floating-container/buttons-floating-container';
-
 import { Disclaimer } from '../../components/disclaimer/disclaimer';
 import { Divider } from '../../components/divider/divider';
 import { isAndroid } from '../../config/system';
@@ -18,15 +18,14 @@ export const EmptyQrCode = () => {
 
   usePageAnalytic(ScreensEnum.ScanQrCode);
 
-  const handlePress = () => isAndroid ? appDetailsSettings() : openUrl('App-Prefs:Security');
+  const handlePress = () => (isAndroid ? appDetailsSettings() : openUrl('App-Prefs:Security'));
 
   return (
     <View style={styles.emptyScreen}>
-        <View style={styles.disclaimer}>
-      <Disclaimer texts={['There is no access to the Camera.', 'Please, give access in the phone Setting page.']} />
-      <Divider size={formatSize(16)} />
-
-        </View>
+      <View style={styles.disclaimer}>
+        <Disclaimer texts={['There is no access to the Camera.', 'Please, give access in the phone Setting page.']} />
+        <Divider size={formatSize(16)} />
+      </View>
       <ButtonsFloatingContainer>
         <ButtonLargePrimary title="Open Settings" onPress={handlePress} />
       </ButtonsFloatingContainer>
