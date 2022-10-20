1762a78b48a870d03882e5b55a6e552f9068ed83
letier3110 <letier3110@gmail.com>
2022-10-20 15:02:17 +0100

    add warning if transfering more tez than have balance

--- src/modals/confirmation-modal/operations-confirmation/operations-confirmation.tsx
+++ src/modals/confirmation-modal/operations-confirmation/operations-confirmation.tsx
@@ -1,11 +1,13 @@
 import { OpKind, ParamsWithKind } from '@taquito/taquito';
-import { Formik } from 'formik';
-import React, { FC } from 'react';
+import { BigNumber } from 'bignumber.js';
+import { FormikProvider, useFormik } from 'formik';
+import React, { FC, useMemo } from 'react';
 import { Text, View } from 'react-native';
 
 import { AccountDropdownItem } from '../../../components/account-dropdown/account-dropdown-item/account-dropdown-item';
 import { ButtonLargePrimary } from '../../../components/button/button-large/button-large-primary/button-large-primary';
 import { ButtonLargeSecondary } from '../../../components/button/button-large/button-large-secondary/button-large-secondary';
+import { Disclaimer } from '../../../components/disclaimer/disclaimer';
 import { Divider } from '../../../components/divider/divider';
 import { LoadingPlaceholder } from '../../../components/loading-placeholder/loading-placeholder';
 import { ModalButtonsContainer } from '../../../components/modal-buttons-container/modal-buttons-container';
@@ -15,7 +17,9 @@ import { useNetworkInfo } from '../../../hooks/use-network-info.hook';
 import { AccountInterface } from '../../../interfaces/account.interface';
 import { TestIdProps } from '../../../interfaces/test-id.props';
 import { useNavigation } from '../../../navigator/hooks/use-navigation.hook';
+import { useSelectedAccountTezosTokenSelector } from '../../../store/wallet/wallet-selectors';
 import { formatSize } from '../../../styles/format-size';
+import { TEZ_TOKEN_METADATA } from '../../../token/data/tokens-metadata';
 import { AnalyticsEventCategory } from '../../../utils/analytics/analytics-event.enum';
 import { useAnalytics } from '../../../utils/analytics/use-analytics.hook';
 import { isDefined } from '../../../utils/is-defined';
@@ -35,6 +39,7 @@ interface Props extends TestIdProps {
 }
 
 export const OperationsConfirmation: FC<Props> = ({ sender, opParams, isLoading, onSubmit, children, testID }) => {
+  const tezosToken = useSelectedAccountTezosTokenSelector();
   const styles = useOperationsConfirmationStyles();
   const { goBack } = useNavigation();
 
@@ -84,60 +89,83 @@ export const OperationsConfirmation: FC<Props> = ({ sender, opParams, isLoading,
     onSubmit(params);
   };
 
+  const formik = useFormik<FeeFormInputValues>({
+    enableReinitialize: true,
+    initialValues: formInitialValues,
+    validationSchema: formValidationSchema,
+    onSubmit: handleSubmit
+  });
+
+  const { values, isValid, setFieldValue, submitForm } = formik;
+
+  const tezFee = useMemo(() => {
+    if (
+      isDefined(opParams) &&
+      opParams.length === 1 &&
+      opParams[0].kind === OpKind.TRANSACTION &&
+      isDefined(values.gasFeeSum) &&
+      values.gasFeeSum instanceof BigNumber
+    ) {
+      const gasFee = tzToMutez(values.gasFeeSum, TEZ_TOKEN_METADATA.decimals).plus(opParams[0].amount);
+      const tezBalance = tezosToken.balance;
+
+      return gasFee.gt(tezBalance);
+    }
+
+    return false;
+  }, [values, opParams]);
+
   return (
-    <Formik<FeeFormInputValues>
-      enableReinitialize={true}
-      initialValues={formInitialValues}
-      validationSchema={formValidationSchema}
-      onSubmit={handleSubmit}
-    >
-      {({ values, isValid, setFieldValue, submitForm }) => (
-        <>
-          <ScreenContainer>
-            {children}
-            {estimations.isLoading ? (
-              <LoadingPlaceholder text="Operation is loading..." />
-            ) : (
-              <>
-                <Text style={styles.sectionTitle}>Account</Text>
-                <Divider />
-
-                <AccountDropdownItem account={sender} />
-                <Divider />
-
-                <Text style={styles.sectionTitle}>Preview</Text>
-                <Divider size={formatSize(12)} />
-
-                <View style={styles.divider} />
-                <Divider size={formatSize(8)} />
-
-                <OperationsPreview opParams={opParamsWithFees} />
-                <Divider />
-
-                <FeeFormInput
-                  values={values}
-                  basicFees={basicFees}
-                  estimationWasSuccessful={estimationWasSuccessful}
-                  onlyOneOperation={onlyOneOperation}
-                  minimalFeePerStorageByteMutez={minimalFeePerStorageByteMutez}
-                  setFieldValue={setFieldValue}
-                />
-              </>
-            )}
+    <FormikProvider value={formik}>
+      <ScreenContainer>
+        {children}
+        {estimations.isLoading ? (
+          <LoadingPlaceholder text="Operation is loading..." />
+        ) : (
+          <>
+            <Text style={styles.sectionTitle}>Account</Text>
             <Divider />
-          </ScreenContainer>
 
-          <ModalButtonsContainer>
-            <ButtonLargeSecondary title="Back" disabled={isLoading} onPress={goBack} />
-            <Divider size={formatSize(16)} />
-            <ButtonLargePrimary
-              title="Confirm"
-              disabled={estimations.isLoading || isLoading || !isValid}
-              onPress={submitForm}
+            <AccountDropdownItem account={sender} />
+            <Divider />
+
+            <Text style={styles.sectionTitle}>Preview</Text>
+            <Divider size={formatSize(12)} />
+
+            <View style={styles.divider} />
+            <Divider size={formatSize(8)} />
+
+            <OperationsPreview opParams={opParamsWithFees} />
+            <Divider />
+
+            <FeeFormInput
+              values={values}
+              basicFees={basicFees}
+              estimationWasSuccessful={estimationWasSuccessful}
+              onlyOneOperation={onlyOneOperation}
+              minimalFeePerStorageByteMutez={minimalFeePerStorageByteMutez}
+              setFieldValue={setFieldValue}
             />
-          </ModalButtonsContainer>
-        </>
-      )}
-    </Formik>
+          </>
+        )}
+        <Divider />
+        {tezFee && (
+          <Disclaimer
+            title="The transaction is likely to fail."
+            texts={['Send amount and fees are greater than your balance.']}
+          />
+        )}
+      </ScreenContainer>
+
+      <ModalButtonsContainer>
+        <ButtonLargeSecondary title="Back" disabled={isLoading} onPress={goBack} />
+        <Divider size={formatSize(16)} />
+        <ButtonLargePrimary
+          title="Confirm"
+          disabled={estimations.isLoading || isLoading || !isValid}
+          onPress={submitForm}
+        />
+      </ModalButtonsContainer>
+    </FormikProvider>
   );
 };
