150aba74b365ea2aa9ce11f2ad4c18c7b2658cc8
letier3110 <letier3110@gmail.com>
2022-10-21 20:15:06 +0100

    fix: remove unused file

--- src/screens/backup-settings/backup-settings.selectors.ts
+++
@@ -1,5 +0,0 @@
-export enum BackupSettingsSelectors {
-  AnalyticsSwitch = 'Analytics Switch on Secure page is Settings',
-  BiometricsSwitch = 'Biometrics Switch on Secure page is Settings',
-  HideModeOnLaunchSwitch = 'Hide Mode on Launch Switch on Secure page is Settings'
-}
