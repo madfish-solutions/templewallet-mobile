e2f988bdde7eac65cec89a52330a34d534e21240
Mykola Medvedenko <mukojla@ukr.net>
2022-10-27 17:21:25 +0300

    Fix on account click at home page for Android

---
+++ src/components/account-dropdown/current-account-dropdown.styles.ts
@@ -0,0 +1,7 @@
+import { StyleSheet } from 'react-native';
+
+export const CurrentAccountDropdownStyles = StyleSheet.create({
+  root: {
+    flex: 1
+  }
+});
--- src/components/account-dropdown/current-account-dropdown.tsx
+++ src/components/account-dropdown/current-account-dropdown.tsx
@@ -1,21 +1,31 @@
 import React, { FC } from 'react';
+import { View } from 'react-native';
 
+import { isAndroid } from '../../config/system';
 import { AccountInterface } from '../../interfaces/account.interface';
 import { DropdownValueComponent, DropdownValueProps } from '../dropdown/dropdown';
 import { IconNameEnum } from '../icon/icon-name.enum';
 import { AccountDropdownBase } from './account-dropdown-base';
 import { AccountDropdownItem, renderAccountListItem } from './account-dropdown-item/account-dropdown-item';
+import { CurrentAccountDropdownStyles } from './current-account-dropdown.styles';
 
 const renderAccountValue: DropdownValueComponent<AccountInterface> = ({ value }) => (
-  <AccountDropdownItem account={value} showFullData={false} actionIconName={IconNameEnum.TriangleDown} />
+  <AccountDropdownItem
+    account={value}
+    showFullData={false}
+    actionIconName={IconNameEnum.TriangleDown}
+    isPublicKeyHashTextDisabled={isAndroid}
+  />
 );
 
 export const CurrentAccountDropdown: FC<DropdownValueProps<AccountInterface>> = ({ value, list, onValueChange }) => (
-  <AccountDropdownBase
-    value={value}
-    list={list}
-    renderValue={renderAccountValue}
-    renderAccountListItem={renderAccountListItem}
-    onValueChange={onValueChange}
-  />
+  <View style={CurrentAccountDropdownStyles.root}>
+    <AccountDropdownBase
+      value={value}
+      list={list}
+      renderValue={renderAccountValue}
+      renderAccountListItem={renderAccountListItem}
+      onValueChange={onValueChange}
+    />
+  </View>
 );
