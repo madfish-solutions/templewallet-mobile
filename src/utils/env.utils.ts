2b3e18864219f80f28cb89fc22465c7846961c7b
Andrii Ivaniv <unixvb@gmail.com>
2022-11-07 17:15:52 +0200

    API URLs changed to DO one

--- src/utils/env.utils.ts
+++ src/utils/env.utils.ts
@@ -6,7 +6,6 @@ const getEnv = (key: string): string => Config[key] ?? '';
 export const READ_ONLY_SIGNER_PUBLIC_KEY = getEnv('READ_ONLY_SIGNER_PUBLIC_KEY');
 export const READ_ONLY_SIGNER_PUBLIC_KEY_HASH = getEnv('READ_ONLY_SIGNER_PUBLIC_KEY_HASH');
 
-export const TEMPLE_WALLET_API = getEnv('TEMPLE_WALLET_API');
 export const SEGMENT_ANALYTICS_KEY = getEnv('SEGMENT_ANALYTICS_KEY');
 
 export const TEMPLE_WALLET_EXOLIX_API_KEY = getEnv('TEMPLE_WALLET_EXOLIX_API_KEY');
