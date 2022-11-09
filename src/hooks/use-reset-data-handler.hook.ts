5bcab5e98495a289cff1bc45defb950cc83b440b
Andrii Ivaniv <unixvb@gmail.com>
2022-11-09 18:05:43 +0200

    reset wallet text improvement

--- src/hooks/use-reset-data-handler.hook.ts
+++ src/hooks/use-reset-data-handler.hook.ts
@@ -2,20 +2,28 @@ import { Alert } from 'react-native';
 import { useDispatch } from 'react-redux';
 
 import { resetApplicationAction } from '../store/root-state.actions';
+import { useIsManualBackupMadeSelector } from '../store/settings/settings-selectors';
 
 export const useResetDataHandler = () => {
   const dispatch = useDispatch();
+  const isManualBackupMade = useIsManualBackupMadeSelector();
 
   return () =>
-    Alert.alert('Are you sure you want to reset the Temple Wallet?', 'As a result, all your data will be deleted.', [
-      {
-        text: 'Cancel',
-        style: 'cancel'
-      },
-      {
-        text: 'Reset',
-        style: 'destructive',
-        onPress: () => dispatch(resetApplicationAction.submit())
-      }
-    ]);
+    Alert.alert(
+      'Are you sure you want to reset the Temple Wallet?',
+      isManualBackupMade
+        ? 'As a result, all your data will be deleted.'
+        : 'Your wallet is NOT backed up. As a result, you will lost access to all your money!',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel'
+        },
+        {
+          text: 'Reset',
+          style: 'destructive',
+          onPress: () => dispatch(resetApplicationAction.submit())
+        }
+      ]
+    );
 };
