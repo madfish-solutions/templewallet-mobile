174cda5ee65accf4ebbadb9487daccf85ba4c9e7
Andrii Ivaniv <unixvb@gmail.com>
2022-11-30 18:59:30 +0200

    lint fixed

--- src/utils/notifications.util.ts
+++ src/utils/notifications.util.ts
@@ -6,13 +6,12 @@ import { NotificationPlatformType } from '../enums/notification-platform-type.en
 import { NotificationStatus } from '../enums/notification-status.enum';
 import { NotificationInterface } from '../interfaces/notification.interface';
 
-// TODO: remove mocked data
 export const loadNotifications$ = (startFromTime: number) =>
   from(
     templeWalletApi.get<NotificationInterface[]>('/notifications', {
       params: {
         platform: NotificationPlatformType.Mobile,
-        startFromTime: new Date('2022-11-29T12:00:00.000Z').getTime()
+        startFromTime
       }
     })
   ).pipe(map(response => response.data.map(notification => ({ ...notification, status: NotificationStatus.New }))));
