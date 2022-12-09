430228e2f0268897dd8bef5c10a14bbae8964bfb
Andrii Ivaniv <unixvb@users.noreply.github.com>
2022-12-09 17:28:03 +0200

    Seed verification is required (#712)
    * seed verification is required
    
    * lint fix
    
    * fix
    
    * seed phrase backup overlay fixed
    
    * removing redundant changes

--- src/screens/wallet/wallet.tsx
+++ src/screens/wallet/wallet.tsx
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View } from 'react-native';
 import { useDispatch } from 'react-redux';
 
 import { CurrentAccountDropdown } from '../../components/account-dropdown/current-account-dropdown';
-import { BottomSheet } from '../../components/bottom-sheet/bottom-sheet';
-import { BottomSheetActionButton } from '../../components/bottom-sheet/bottom-sheet-action-button/bottom-sheet-action-button';
-import { useBottomSheetController } from '../../components/bottom-sheet/use-bottom-sheet-controller';
 import { Divider } from '../../components/divider/divider';
 import { HeaderCardActionButtons } from '../../components/header-card-action-buttons/header-card-action-buttons';
 import { HeaderCard } from '../../components/header-card/header-card';
@@ -15,8 +12,6 @@ import { TokenEquityValue } from '../../components/token-equity-value/token-equi
 import { useWalletOpenTacker } from '../../hooks/use-wallet-open-tacker.hook';
 import { ScreensEnum } from '../../navigator/enums/screens.enum';
 import { useNavigation } from '../../navigator/hooks/use-navigation.hook';
-import { useAppLock } from '../../shelter/app-lock/app-lock';
-import { useIsOpenBackupBottomSheetSelector } from '../../store/settings/settings-selectors';
 import { setSelectedAccountAction } from '../../store/wallet/wallet-actions';
 import {
   useSelectedAccountSelector,
@@ -25,6 +20,7 @@ import {
 } from '../../store/wallet/wallet-selectors';
 import { formatSize } from '../../styles/format-size';
 import { usePageAnalytic } from '../../utils/analytics/use-analytics.hook';
+import { BackupYourWalletOverlay } from './backup-your-wallet-overlay/backup-your-wallet-overlay';
 import { CollectiblesHomeSwipeButton } from './collectibles-home-swipe-button/collectibles-home-swipe-button';
 import { NotificationsBell } from './notifications-bell/notifications-bell';
 import { TokenList } from './token-list/token-list';
@@ -33,28 +29,13 @@ import { WalletStyles } from './wallet.styles';
 export const Wallet = () => {
   const dispatch = useDispatch();
   const { navigate } = useNavigation();
-  const { isLocked } = useAppLock();
 
   const selectedAccount = useSelectedAccountSelector();
   const visibleAccounts = useVisibleAccountsListSelector();
   const tezosToken = useSelectedAccountTezosTokenSelector();
 
-  const isOpenBackupBottomSheet = useIsOpenBackupBottomSheetSelector();
-  const backupBottomSheetController = useBottomSheetController();
-
   useWalletOpenTacker();
   usePageAnalytic(ScreensEnum.Wallet);
-
-  useEffect(() => {
-    if (isOpenBackupBottomSheet && !isLocked) {
-      setTimeout(backupBottomSheetController.open, 500);
-    }
-  }, [isOpenBackupBottomSheet, isLocked]);
-
-  const handleManualBackupButtonPress = () => {
-    navigate(ScreensEnum.ManualBackup);
-    backupBottomSheetController.close();
-  };
 
   return (
     <>
@@ -83,15 +64,7 @@ export const Wallet = () => {
       </HeaderCard>
       <TokenList />
 
-      <BottomSheet
-        title="Backup your wallet"
-        description={'Don’t lose your wallet! Save your access \nto accounts.'}
-        cancelButtonText="Not now"
-        contentHeight={formatSize(186)}
-        controller={backupBottomSheetController}
-      >
-        <BottomSheetActionButton title="Manual backup" onPress={handleManualBackupButtonPress} />
-      </BottomSheet>
+      <BackupYourWalletOverlay />
     </>
   );
 };
