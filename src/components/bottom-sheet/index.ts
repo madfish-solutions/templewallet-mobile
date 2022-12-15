27ffdaa56070c1971b243d3aa21ba4f4ac4e8aa3
Dmytro <herkodmytro@gmail.com>
2022-12-15 19:03:25 +0200

    TW-386 Fixed exports

--- src/components/bottom-sheet/index.ts
+++ src/components/bottom-sheet/index.ts
@@ -1,3 +1,3 @@
-export * from './bottom-sheet';
-export * from './use-bottom-sheet-controller';
-export * from './bottom-sheet-action-button/bottom-sheet-action-button';
+export { BottomSheet } from './bottom-sheet';
+export { useBottomSheetController } from './use-bottom-sheet-controller';
+export { BottomSheetActionButton } from './bottom-sheet-action-button/bottom-sheet-action-button';
