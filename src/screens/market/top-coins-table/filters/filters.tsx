3e63ce7264208cc4ed0ea10632ca4986a572d8ff
Dmytro <herkodmytro@gmail.com>
2022-12-27 14:42:54 +0200

    TW-386 Extended switcher

--- src/screens/market/top-coins-table/filters/filters.tsx
+++ src/screens/market/top-coins-table/filters/filters.tsx
@@ -42,7 +42,7 @@ export const Filters: FC<Props> = ({
     <View style={styles.filtersContainer}>
       <TextSegmentControl
         selectedIndex={segmentControlIndex}
-        width={formatSize(154)}
+        width={formatSize(156)}
         values={['Market', 'Favorites']}
         onChange={onSelectorChange}
       />
