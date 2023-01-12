72a70da0d3eb1da5a43840e71cb446bc1690a7ba
Dmytro <herkodmytro@gmail.com>
2023-01-12 18:44:44 +0200

    TW-432 Improved manage account functionality

--- src/screens/manage-accounts/manage-accounts.tsx
+++ src/screens/manage-accounts/manage-accounts.tsx
@@ -1,9 +1,20 @@
 import React, { useState } from 'react';
 import { View } from 'react-native';
 
+import { BottomSheet } from '../../components/bottom-sheet/bottom-sheet';
+import { BottomSheetActionButton } from '../../components/bottom-sheet/bottom-sheet-action-button/bottom-sheet-action-button';
+import { useBottomSheetController } from '../../components/bottom-sheet/use-bottom-sheet-controller';
 import { Divider } from '../../components/divider/divider';
+import { generateScreenOptions } from '../../components/header/generate-screen-options.util';
+import { HeaderButton } from '../../components/header/header-button/header-button';
+import { HeaderTitle } from '../../components/header/header-title/header-title';
+import { useNavigationSetOptions } from '../../components/header/use-navigation-set-options.hook';
+import { IconNameEnum } from '../../components/icon/icon-name.enum';
 import { TextSegmentControl } from '../../components/segmented-control/text-segment-control/text-segment-control';
+import { ModalsEnum } from '../../navigator/enums/modals.enum';
 import { ScreensEnum } from '../../navigator/enums/screens.enum';
+import { useNavigation } from '../../navigator/hooks/use-navigation.hook';
+import { useShelter } from '../../shelter/use-shelter.hook';
 import { formatSize } from '../../styles/format-size';
 import { usePageAnalytic } from '../../utils/analytics/use-analytics.hook';
 import { useManageAccountsStyles } from './manage-accounts.styles';
@@ -12,13 +23,41 @@ import { ManageImportedAccounts } from './manage-imported-accounts/manage-import
 
 const manageHdAccountsIndex = 0;
 
+enum AccountTypes {
+  HD = 'HD',
+  Imported = 'Imported'
+}
+
+const accountsLabels: Record<AccountTypes, string> = {
+  [AccountTypes.HD]: 'Create new HD account',
+  [AccountTypes.Imported]: 'Import an account'
+};
+
+const accountsOptions: Array<AccountTypes> = [AccountTypes.HD, AccountTypes.Imported];
+
 export const ManageAccounts = () => {
+  const { navigate } = useNavigation();
+  const { createHdAccount } = useShelter();
+
   const styles = useManageAccountsStyles();
 
   const [segmentedControlIndex, setSegmentedControlIndex] = useState(0);
   const showManageHdAccounts = segmentedControlIndex === manageHdAccountsIndex;
+  const revealSelectBottomSheetController = useBottomSheetController();
 
   usePageAnalytic(ScreensEnum.ManageAccounts);
+  useNavigationSetOptions(
+    generateScreenOptions(
+      <HeaderTitle title="Manage Accounts" />,
+      <HeaderButton iconName={IconNameEnum.PlusIconOrange} onPress={() => revealSelectBottomSheetController.open()} />
+    ),
+    []
+  );
+
+  const handlers = {
+    [AccountTypes.HD]: () => createHdAccount(),
+    [AccountTypes.Imported]: () => navigate(ModalsEnum.ImportAccount)
+  };
 
   return (
     <>
@@ -34,6 +73,22 @@ export const ManageAccounts = () => {
       <Divider size={formatSize(8)} />
 
       {showManageHdAccounts ? <ManageHdAccounts /> : <ManageImportedAccounts />}
+      <BottomSheet
+        description="Select a method to add an account:"
+        contentHeight={formatSize(180)}
+        controller={revealSelectBottomSheetController}
+      >
+        {accountsOptions.map(value => (
+          <BottomSheetActionButton
+            key={value}
+            title={accountsLabels[value]}
+            onPress={() => {
+              handlers[value]();
+              revealSelectBottomSheetController.close();
+            }}
+          />
+        ))}
+      </BottomSheet>
     </>
   );
 };
--- src/screens/manage-accounts/manage-imported-accounts/manage-imported-accounts.tsx
+++ src/screens/manage-accounts/manage-imported-accounts/manage-imported-accounts.tsx
@@ -2,8 +2,6 @@ import React, { Fragment } from 'react';
 
 import { DataPlaceholder } from '../../../components/data-placeholder/data-placeholder';
 import { Divider } from '../../../components/divider/divider';
-import { IconTitleNoBg } from '../../../components/icon-title-no-bg/icon-title-no-bg';
-import { IconNameEnum } from '../../../components/icon/icon-name.enum';
 import { ScreenContainer } from '../../../components/screen-container/screen-container';
 import { SearchInput } from '../../../components/search-input/search-input';
 import { useFilteredAccountList } from '../../../hooks/use-filtered-account-list.hook';
@@ -40,11 +38,6 @@ export const ManageImportedAccounts = () => {
           </Fragment>
         ))}
         <Divider size={formatSize(10)} />
-        <IconTitleNoBg
-          icon={IconNameEnum.DownloadCloud}
-          text="import"
-          onPress={() => navigate(ModalsEnum.ImportAccount)}
-        />
         {accountsLength === 0 && <DataPlaceholder text="No found accounts" />}
       </ScreenContainer>
     </>
