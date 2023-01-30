6acf2db2ef40040c406099989a420dc871845005
Dmytro <herkodmytro@gmail.com>
2023-01-30 19:47:52 +0200

    TW-474 Fixed unit tests

--- src/utils/analytics/analytics.util.ts
+++ src/utils/analytics/analytics.util.ts
@@ -1,8 +1,10 @@
 import { jitsuClient } from '@jitsu/sdk-js';
+import fetch from 'cross-fetch';
 
 import { JITSU_ANALYTICS_KEY, JITSU_TRACKING_HOST } from '../env.utils';
 
 export const jitsu = jitsuClient({
   tracking_host: JITSU_TRACKING_HOST,
-  key: JITSU_ANALYTICS_KEY
+  key: JITSU_ANALYTICS_KEY,
+  fetch
 });
