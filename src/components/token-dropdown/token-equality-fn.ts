ad69e526541b23193c378bebbc21b77888e73330
alex-seleznov <alex.seleznov@gmail.com>
2023-02-22 06:23:33 +0200

    TW-440: Fix DataURI render in Collectibles' icons. Fix 'tokenEqualityFn'

--- src/components/token-dropdown/token-equality-fn.ts
+++ src/components/token-dropdown/token-equality-fn.ts
@@ -3,5 +3,14 @@ import { isDefined } from 'src/utils/is-defined';
 
 import { DropdownEqualityFn } from '../dropdown/dropdown';
 
-export const tokenEqualityFn: DropdownEqualityFn<TokenMetadataInterface> = (item, value) =>
-  isDefined(value) && item.address === value.address && item.id === value.id;
+export const tokenEqualityFn: DropdownEqualityFn<TokenMetadataInterface> = (item, value) => {
+  if (!isDefined(value)) {
+    return false;
+  }
+
+  if (!item.address && !value.address) {
+    return item.symbol === value.symbol;
+  }
+
+  return item.address === value.address && item.id === value.id;
+};
