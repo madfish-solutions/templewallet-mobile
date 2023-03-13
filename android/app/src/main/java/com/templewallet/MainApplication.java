94d3dff12d97801986b5b65860ee9a89e1298781
alex-seleznov <alex.seleznov@gmail.com>
2023-03-13 18:04:21 +0200

    TW-501: Cloud seed phrase back-up. Refactor

--- android/app/src/main/java/com/templewallet/MainApplication.java
+++ android/app/src/main/java/com/templewallet/MainApplication.java
@@ -31,14 +31,13 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
 
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // packages.add(new MyReactNativePackage());
-
           packages.add(
             new KeychainPackage(
               new KeychainModuleBuilder().withoutWarmUp()
             )
           );
+          // Packages that cannot be autolinked yet can be added manually here, for example:
+          // packages.add(new MyReactNativePackage());
 
           return packages;
         }
