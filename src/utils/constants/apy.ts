da123cbca3b030b78a6849d9adaa0d26028ab881
svyatoslavtt <stiven22235@gmail.com>
2023-03-21 13:47:06 +0200

    TW-368 Fix linter

--- src/utils/constants/apy.ts
+++ src/utils/constants/apy.ts
@@ -2,7 +2,7 @@ import { KNOWN_TOKENS_SLUGS } from 'src/token/data/token-slugs';
 
 export const YUPANA_LINK = 'https://app.yupana.finance/lending';
 export const KORDFI_LINK = 'https://kord.fi/lend';
-export const YOUVES_LINK = 'https://app.youves.com/earn';
+const YOUVES_LINK = 'https://app.youves.com/earn';
 
 export const TOKEN_APY_LINKS: Readonly<Record<string, string | undefined>> = {
   [KNOWN_TOKENS_SLUGS.KUSD]: YUPANA_LINK,
