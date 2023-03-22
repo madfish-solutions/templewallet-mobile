998b50b81af8172ad3c5afe565f8f774cd8d73bf
Alex Seleznov <alex.seleznov@gmail.com>
2023-03-22 19:38:48 +0200

    TW-501: Cloud seed phrase back-up. Refactor

--- src/screens/cloud-backup/form.ts
+++ src/screens/cloud-backup/form.ts
@@ -1,22 +1,17 @@
-import { Alert, AlertButton } from 'react-native';
+import { useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { firstValueFrom } from 'rxjs';
 import { object, SchemaOf } from 'yup';
 
-import { isAndroid } from 'src/config/system';
 import { passwordValidation } from 'src/form/validation/password';
 import { ScreensEnum } from 'src/navigator/enums/screens.enum';
 import { useNavigation } from 'src/navigator/hooks/use-navigation.hook';
 import { Shelter } from 'src/shelter/shelter';
 import { madeCloudBackupAction } from 'src/store/settings/settings-actions';
-import { showErrorToast, showSuccessToast } from 'src/toast/toast.utils';
-import {
-  cloudTitle,
-  fetchCloudBackupFileDetails,
-  requestSignInToCloud,
-  saveCloudBackup,
-  syncCloud
-} from 'src/utils/cloud-backup';
+import { showSuccessToast } from 'src/toast/toast.utils';
+import { fetchCloudBackupFileDetails, requestSignInToCloud, saveCloudBackup, syncCloud } from 'src/utils/cloud-backup';
+
+import { alertOnExistingBackup, assurePasswordIsCorrect, callWithErrorToast } from './utils';
 
 interface EnterCloudPasswordFormValues {
   password: string;
@@ -34,104 +29,45 @@ export const useHandleSubmit = () => {
   const { goBack, navigate } = useNavigation();
   const dispatch = useDispatch();
 
-  const handleSubmit = async ({ password }: EnterCloudPasswordFormValues): Promise<void> => {
-    const isPasswordCorrect = await firstValueFrom(Shelter.isPasswordCorrect$(password));
+  const proceedWithSaving = useCallback(
+    async (password: string) => {
+      const mnemonic = await firstValueFrom(Shelter.revealSeedPhrase$());
 
-    if (!isPasswordCorrect) {
-      return void showErrorToast({ description: 'Wrong password' });
-    }
-
-    try {
-      const loggedInToCloud = await requestSignInToCloud();
-
-      if (!loggedInToCloud) {
-        return;
-      }
-    } catch (error) {
-      return void showErrorToast({
-        title: 'Failed to log-in',
-        description: (error as Error)?.message ?? 'Unknown reason'
-      });
-    }
-
-    try {
-      await syncCloud();
-    } catch (error) {
-      return void showErrorToast({
-        title: 'Failed to sync cloud',
-        description: (error as Error)?.message ?? 'Unknown reason'
-      });
-    }
-
-    const backupFile = await fetchCloudBackupFileDetails();
-
-    if (backupFile) {
-      return void alertOnExistingBackup(
-        () => void handleSubmit({ password }),
-        proceedWithSaving,
-        () => void navigate(ScreensEnum.ManualBackup)
-      );
-    }
-
-    await proceedWithSaving();
-
-    async function proceedWithSaving() {
-      const mnemonic = await new Promise<string>(resolve =>
-        Shelter.revealSeedPhrase$().subscribe(m => void resolve(m))
-      );
-
-      try {
-        await saveCloudBackup(mnemonic, password);
-      } catch (error) {
-        return void showErrorToast({
-          title: 'Failed to back up to cloud',
-          description: (error as Error)?.message ?? ''
-        });
-      }
+      await callWithErrorToast(() => saveCloudBackup(mnemonic, password), 'Failed to back up to cloud', true);
 
       dispatch(madeCloudBackupAction());
       showSuccessToast({ description: 'Your wallet has been backed up successfully!' });
       goBack();
-    }
-  };
+    },
+    [goBack, dispatch]
+  );
+
+  const handleSubmit = useCallback(
+    async ({ password }: EnterCloudPasswordFormValues): Promise<void> => {
+      await callWithErrorToast(() => assurePasswordIsCorrect(password), 'Wrong password');
+
+      const loggedInToCloud = await callWithErrorToast(requestSignInToCloud, 'Failed to log-in', true);
+
+      if (!loggedInToCloud) {
+        return;
+      }
+
+      await callWithErrorToast(syncCloud, 'Failed to sync cloud');
+
+      const backupFile = await callWithErrorToast(fetchCloudBackupFileDetails, 'Failed to read from cloud', true);
+
+      if (backupFile) {
+        return void alertOnExistingBackup(
+          () => void handleSubmit({ password }),
+          () => void proceedWithSaving(password),
+          () => void navigate(ScreensEnum.ManualBackup)
+        );
+      }
+
+      await proceedWithSaving(password);
+    },
+    [navigate, proceedWithSaving]
+  );
 
   return handleSubmit;
-};
-
-const alertOnExistingBackup = (
-  onChangeAccountPress: () => void,
-  onReplaceBackupPress: () => void,
-  onBackupManuallyPress: () => void
-) => {
-  const title = `Your ${cloudTitle} account already has a wallet backup.`;
-
-  const message = [
-    'If you create a new backup, your previous one will be irretrievably deleted.',
-    ` Instead, you can reveal and manually save your seed phrase or change your ${cloudTitle} account to keep a new backup.`,
-    `\n\nRemember, one ${cloudTitle} account can only have one wallet backup.`
-  ].join();
-
-  const buttons: AlertButton[] = [
-    {
-      text: 'Create a new backup',
-      onPress: onReplaceBackupPress
-    },
-    {
-      text: 'Backup manually',
-      onPress: onBackupManuallyPress
-    }
-  ];
-
-  if (isAndroid) {
-    buttons.push({
-      text: 'Change an account',
-      onPress: onChangeAccountPress
-    });
-
-    buttons.reverse();
-  }
-
-  return void Alert.alert(title, message, buttons, {
-    cancelable: true
-  });
 };
---
+++ src/screens/cloud-backup/utils.ts
@@ -0,0 +1,67 @@
+import { Alert, AlertButton } from 'react-native';
+import { firstValueFrom } from 'rxjs';
+
+import { isAndroid } from 'src/config/system';
+import { Shelter } from 'src/shelter/shelter';
+import { showErrorToast } from 'src/toast/toast.utils';
+import { cloudTitle } from 'src/utils/cloud-backup';
+
+export const assurePasswordIsCorrect = async (password: string) => {
+  const isPasswordCorrect = await firstValueFrom(Shelter.isPasswordCorrect$(password));
+
+  if (!isPasswordCorrect) {
+    throw new Error('Wrong password');
+  }
+};
+
+export const callWithErrorToast = <R>(callback: () => R, title: string, takeDescriptionFromErrorMsg = false) => {
+  try {
+    return callback();
+  } catch (error) {
+    if (takeDescriptionFromErrorMsg) {
+      showErrorToast({ title, description: (error as Error)?.message });
+    } else {
+      showErrorToast({ description: title });
+    }
+
+    throw error;
+  }
+};
+
+export const alertOnExistingBackup = (
+  onChangeAccountPress: () => void,
+  onReplaceBackupPress: () => void,
+  onBackupManuallyPress: () => void
+) => {
+  const title = `Your ${cloudTitle} account already has a wallet backup.`;
+
+  const message = [
+    'If you create a new backup, your previous one will be irretrievably deleted.',
+    ` Instead, you can reveal and manually save your seed phrase or change your ${cloudTitle} account to keep a new backup.`,
+    `\n\nRemember, one ${cloudTitle} account can only have one wallet backup.`
+  ].join();
+
+  const buttons: AlertButton[] = [
+    {
+      text: 'Create a new backup',
+      onPress: onReplaceBackupPress
+    },
+    {
+      text: 'Backup manually',
+      onPress: onBackupManuallyPress
+    }
+  ];
+
+  if (isAndroid) {
+    buttons.push({
+      text: 'Change an account',
+      onPress: onChangeAccountPress
+    });
+
+    buttons.reverse();
+  }
+
+  return void Alert.alert(title, message, buttons, {
+    cancelable: true
+  });
+};
