4f6409801cc341ab7b8c53dfe3cd9c3edc480f6d
razbeikov <razbeikov.oleksandr@gmail.com>
2023-04-18 16:58:14 +0300

    delete useless styles

--- src/screens/collectibles-home/collectibles-list/collectibles-list.styles.ts
+++ src/screens/collectibles-home/collectibles-list/collectibles-list.styles.ts
@@ -1,12 +1,7 @@
 import { StyleSheet } from 'react-native';
 
-import { formatSize } from 'src/styles/format-size';
-
 export const CollectiblesListStyles = StyleSheet.create({
   rowContainer: {
     flexDirection: 'row'
-  },
-  collectiblesContainer: {
-    marginBottom: formatSize(300)
   }
 });
