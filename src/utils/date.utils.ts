000270f85cdcaffedd6931bd10bf72fb3a8599b4
herkoss <herkodmytro@gmail.com>
2023-05-16 15:44:55 +0300

    TW-664 Fixed comments

--- src/utils/date.utils.ts
+++ src/utils/date.utils.ts
@@ -35,10 +35,7 @@ const MS_IN_SECOND = 1000;
 export function toIntegerSeconds(ms: number): number;
 export function toIntegerSeconds(date: Date): number;
 export function toIntegerSeconds(data: number | Date) {
-  console.log('data: ', data);
-
   const ms = data instanceof Date ? data.getTime() : data;
-  console.log('ms: ', ms);
 
   return Math.floor(ms / MS_IN_SECOND);
 }
