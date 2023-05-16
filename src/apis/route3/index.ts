eec58181dcc732174bfbd4ef351fdb936e2dcdf0
lendihop <mishin.danil2017@gmail.com>
2023-05-16 17:37:47 +0200

    migration + react native minor update

--- src/apis/route3/index.ts
+++ src/apis/route3/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { TEMPLE_WALLET_ROUTE3_AUTH_TOKEN } from 'src/utils/env.utils';
 
 export const route3Api = axios.create({
-  baseURL: 'https://temple.3route.io',
+  baseURL: 'https://temple.3route.io/v3',
   headers: {
     Authorization: TEMPLE_WALLET_ROUTE3_AUTH_TOKEN
   }
