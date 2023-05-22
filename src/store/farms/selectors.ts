d2aad0a9562f776ac3dbe198e863da25572af88e
Inokentii Mazhara <keshan3262@gmail.com>
2023-05-22 19:04:19 +0300

    TW-666 Refactor farm selector

--- src/store/farms/selectors.ts
+++ src/store/farms/selectors.ts
@@ -1,11 +1,16 @@
+import { useMemo } from 'react';
+
 import { FarmVersionEnum } from 'src/apis/quipuswap/types';
 
 import { useSelector } from '../selector';
 
 export const useFarmsLoadingSelector = () => useSelector(({ farms }) => farms.allFarms.isLoading);
 
-export const useFarmSelector = (id: string, version: FarmVersionEnum) =>
-  useSelector(({ farms }) => farms.allFarms.data.find(({ item }) => item.id === id && item.version === version));
+export const useFarmSelector = (id: string, version: FarmVersionEnum) => {
+  const list = useSelector(({ farms }) => farms.allFarms.data);
+
+  return useMemo(() => list.find(({ item }) => item.id === id && item.version === version), [list, id, version]);
+};
 
 export const useStakeSelector = (farmAddress: string) => useSelector(({ farms }) => farms.lastStakes[farmAddress]);
 
