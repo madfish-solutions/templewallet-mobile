74b4317a500729ccc41f006f8bc4cb573aa2955a
Inokentii Mazhara <keshan3262@gmail.com>
2023-05-24 18:23:35 +0300

    TW-667 Refactor according to a comment

--- src/modals/manage-farming-pool-modal/index.tsx
+++ src/modals/manage-farming-pool-modal/index.tsx
@@ -42,10 +42,9 @@ export const ManageFarmingPoolModal: FC = () => {
 
   useEffect(() => {
     if (!isDefined(farm) || prevBlockLevelRef.current !== blockLevel) {
+      prevBlockLevelRef.current = blockLevel;
       dispatch(loadAllFarmsActions.submit());
     }
-
-    prevBlockLevelRef.current = blockLevel;
   }, [blockLevel, farm, dispatch]);
 
   useEffect(() => {
