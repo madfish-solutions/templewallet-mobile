3c3c67312474b16f4104bc8da64d776f41a1fa67
svyatoslavtt <stiven22235@gmail.com>
2023-05-30 13:38:51 +0300

    TW-706: Add url params

--- src/apis/optimal/index.ts
+++ src/apis/optimal/index.ts
@@ -1,10 +1,17 @@
 import { optimalApi } from '../../api.service';
 
 export const optimalFetchEnableAds = async (address: string) => {
+  const url = '/api/v1/decision/';
+
+  const params = {
+    publisher: 'templewallet',
+    ad_types: 'tw-fullview',
+    div_ids: 'ad',
+    wallets: '1729'
+  };
+
   try {
-    await optimalApi.get(
-      `/api/v1/decision/?publisher=templewallet&ad_types=tw-fullview&div_ids=ad&&wallets=1729%3A${address}`
-    );
+    await optimalApi.get(url, { params: { ...params, wallets: `${params.wallets}:${address}` } });
   } catch (err) {
     console.log('Failed to fetch optimal promotion api.');
   }
