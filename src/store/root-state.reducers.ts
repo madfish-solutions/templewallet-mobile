a1722c6dcdde0a314ca8376b216d9c8522dc07a7
Alex <alex.seleznov@gmail.com>
2023-06-06 00:08:16 +0300

    TW-503: Buy section. Binance Connect. Fix new provider in initial store state through persistance blacklisting fix(hack)

--- src/store/root-state.reducers.ts
+++ src/store/root-state.reducers.ts
@@ -3,7 +3,7 @@ import { combineReducers } from '@reduxjs/toolkit';
 import type { CombinedState } from '@reduxjs/toolkit';
 import { Action, AnyAction, ReducersMapObject } from 'redux';
 import type { Reducer } from 'redux';
-import { persistReducer } from 'redux-persist';
+import { createTransform, persistReducer } from 'redux-persist';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import { PersistConfig } from 'redux-persist/lib/types';
 import { on, reducer } from 'ts-action';
@@ -12,9 +12,12 @@ import { resetApplicationAction } from './root-state.actions';
 import { rootStateReducersMap } from './root-state.map';
 import type { RootState } from './types';
 
-const buildRootStateReducer =
-  <S, A extends Action = AnyAction>(reducers: ReducersMapObject<S, A>): Reducer<CombinedState<S>, A> =>
-  (appState, action) => {
+const buildRootStateReducer = <S, A extends Action = AnyAction>(
+  reducers: ReducersMapObject<S, A>
+): Reducer<CombinedState<S>, A> => {
+  const combinedReducers = combineReducers(reducers);
+
+  return (appState, action) => {
     const rootReducer = reducer(
       appState,
       on(resetApplicationAction.success, _ => undefined)
@@ -22,19 +25,33 @@ const buildRootStateReducer =
 
     const state = rootReducer(appState, action);
 
-    return combineReducers(reducers)(state, action);
+    return combinedReducers(state, action);
   };
+};
 
 const rootReducer = buildRootStateReducer(rootStateReducersMap);
 
-const persistConfigBlacklist: (keyof RootState)[] = ['buyWithCreditCard', 'exolix'];
+const persistRootBlacklist: (keyof RootState)[] = ['buyWithCreditCard', 'exolix'];
+
+const PersistBlacklistTransform = createTransform(
+  () => void 0,
+  () => void 0,
+  { whitelist: persistRootBlacklist }
+);
 
 const persistConfig: PersistConfig<RootState> = {
   key: 'root',
   version: 1,
   storage: AsyncStorage,
   stateReconciler: autoMergeLevel2,
-  blacklist: persistConfigBlacklist
+  /**
+   * Basic(out-of-the-box) config setting `blacklist: persistRootBlacklist`
+   * does not work as expected (presumably for `AsyncStorage` case).
+   *
+   * It does not respect new-added blacklisted slices for a while after app launch.
+   * Results in misrepresented store state.
+   */
+  transforms: [PersistBlacklistTransform]
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
