2a983811d4aa07d725eaec8b554c4e701d7fd0f8
Inokentii Mazhara <keshan3262@gmail.com>
2023-06-12 13:40:09 +0300

    TW-712 Update dependencies list for a callback

--- src/hooks/use-address-field-analytics.hook.ts
+++ src/hooks/use-address-field-analytics.hook.ts
@@ -58,7 +58,7 @@ export const useAddressFieldAnalytics = <T extends string, U extends Record<T, s
           trackNetworkEvent(networkSlug);
         }
       });
-  }, [value]);
+  }, [value, trackNetworkEvent]);
 
   const onBlur = useCallback(async () => {
     const currentValue = valueRef.current;
