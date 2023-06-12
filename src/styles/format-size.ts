924e0986cfad121400807aa1a41c1deec575f2c0
razbeikov <razbeikov.oleksandr@gmail.com>
2023-06-12 12:03:10 +0300

    refactor formatSize function

--- src/styles/format-size.ts
+++ src/styles/format-size.ts
@@ -4,5 +4,5 @@ import { isAndroid } from 'src/config/system';
 
 import { layoutScale } from '../config/styles';
 
-export const formatSize = (size: number): number => (isAndroid ? Math.floor(size * layoutScale) : size * layoutScale);
+export const formatSize = (size: number): number => (isAndroid ? Math.round(size * layoutScale) : size * layoutScale);
 export const formatSizeScaled = (size: number): number => size * layoutScale * Dimensions.get('window').fontScale;
