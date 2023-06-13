92a7bb9d9f36b0da4ff447dc6163e4f12f79fe46
lendihop <mishin.danil2017@gmail.com>
2023-06-13 13:47:53 +0200

    WALLET_AUTOLOCK_TIME revert

--- src/config/security.ts
+++ src/config/security.ts
@@ -2,7 +2,7 @@ import { ONE_MINUTE } from './fixed-times';
 
 export const MAX_PASSWORD_ATTEMPTS = 3;
 export const WRONG_PASSWORD_LOCK_TIME = ONE_MINUTE;
-export const WALLET_AUTOLOCK_TIME = 10000;
+export const WALLET_AUTOLOCK_TIME = 3 * ONE_MINUTE;
 export const INITIAL_ENTER_WRONG_PASSWORD_ATTEMPTS = 1;
 export const INITIAL_ENTER_PASSWORD_LOCKTIME = 0;
 export const RANDOM_DELAY_TIME = 1000;
