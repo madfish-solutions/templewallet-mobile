790bc1a61bb093c25e726ab92233b51aecc9eedd
svyatoslavtt <stiven22235@gmail.com>
2023-06-18 18:57:05 +0300

    TW-368: Fix jest mock

--- src/mocks/youves.mock.ts
+++ src/mocks/youves.mock.ts
@@ -1,12 +1,3 @@
 export const emptyObject = {};
 
-export const contracts = {
-  mainnet: [emptyObject],
-  ithacanet: [emptyObject]
-};
-
-jest.mock('youves-sdk', () => ({ contracts }));
-// jest.mock('youves-sdk/src/networks.mainnet', () => emptyObject);
-jest.mock('youves-sdk', () => emptyObject);
 jest.mock('youves-sdk/src/staking/unified-staking', () => emptyObject);
-// jest.mock('youves-sdk/src/networks.base', () => emptyObject);
