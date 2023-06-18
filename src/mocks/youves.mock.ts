a338644c20a1c9c28e31ee19fda3f482e76d562a
svyatoslavtt <stiven22235@gmail.com>
2023-06-18 18:38:59 +0300

    TW-368: Revert jest mock

--- src/mocks/youves.mock.ts
+++ src/mocks/youves.mock.ts
@@ -8,5 +8,5 @@ export const contracts = {
 jest.mock('youves-sdk', () => ({ contracts }));
 // jest.mock('youves-sdk/src/networks.mainnet', () => emptyObject);
 jest.mock('youves-sdk', () => emptyObject);
-jest.mock('youves-sdk', () => emptyObject);
+jest.mock('youves-sdk/src/staking/unified-staking', () => emptyObject);
 // jest.mock('youves-sdk/src/networks.base', () => emptyObject);
