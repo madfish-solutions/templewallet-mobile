aa344efcee25688c3a45f8bb9cf4790605df2ae8
Inokentii Mazhara <keshan3262@gmail.com>
2023-06-20 18:16:32 +0300

    TW-653 Remove commented out code

--- src/screens/earn/index.tsx
+++ src/screens/earn/index.tsx
@@ -6,7 +6,7 @@ import { IconNameEnum } from 'src/components/icon/icon-name.enum';
 import { useUserFarmingStats } from 'src/hooks/use-user-farming-stats';
 import { ScreensEnum } from 'src/navigator/enums/screens.enum';
 import { loadAllFarmsAndStakesAction } from 'src/store/farms/actions';
-import { useAllFarmsSelector /* , useLastStakesSelector */ } from 'src/store/farms/selectors';
+import { useAllFarmsSelector } from 'src/store/farms/selectors';
 import { usePageAnalytic } from 'src/utils/analytics/use-analytics.hook';
 
 import { OpportunityCategoryCard } from './opportunity-category-card';
@@ -17,7 +17,6 @@ export const Earn: FC = () => {
   const dispatch = useDispatch();
   const { netApy, totalStakedAmountInUsd, maxApy } = useUserFarmingStats();
   const farms = useAllFarmsSelector();
-  // const stakes = useLastStakesSelector();
   const styles = useEarnPageStyles();
 
   usePageAnalytic(ScreensEnum.Earn);
