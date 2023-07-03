be1557c3228b8ca95f87698f04402295b5985998
herkoss <herkodmytro@gmail.com>
2023-07-03 20:48:01 +0300

    TW-710 Fixed linter errors

--- src/store/newsletter/newsletter-state.ts
+++ src/store/newsletter/newsletter-state.ts
@@ -7,7 +7,3 @@ export const newsletterInitialState: NewsletterState = {
   emails: [],
   shouldShowNewsletterModal: true
 };
-
-export interface NewsletterRootState {
-  newsletter: NewsletterState;
-}
