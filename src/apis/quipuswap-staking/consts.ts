0b282f58bd1ac9deee1f0898daba25ec26fd6bcb
Inokentii Mazhara <keshan3262@gmail.com>
2023-07-04 19:01:55 +0300

    TW-651 Make constants have the same case

--- src/apis/quipuswap-staking/consts.ts
+++ src/apis/quipuswap-staking/consts.ts
@@ -1,7 +1,7 @@
 import { BigNumber } from 'bignumber.js';
 
-export const precision = new BigNumber(10).pow(18);
-export const aPrecision = new BigNumber(100);
-export const accumPrecision = new BigNumber(1e10);
-export const feeDenominator = new BigNumber(1e10);
+export const PRECISION = new BigNumber(10).pow(18);
+export const A_PRECISION = new BigNumber(100);
+export const ACCUM_PRECISION = new BigNumber(1e10);
+export const FEE_DENOMINATOR = new BigNumber(1e10);
 export const TEZOS_CONTRACT_ADDRESS = 'tez';
