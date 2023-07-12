803f810ef5acafeb1e03b614d4d96842a7af92f4
lendihop <mishin.danil2017@gmail.com>
2023-07-12 15:28:24 +0200

    analytics added + review fixes

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ import 'react-native-gesture-handler';
 import './shim.js';
 import './handle-js-exception';
 
-import { AppRegistry, PermissionsAndroid } from 'react-native';
+import { AppRegistry } from 'react-native';
 
 import { name as appName } from './app.json';
 import { App } from './src/app/app';
-import { isAndroid } from './src/config/system';
-
-(async () => {
-  if (isAndroid) {
-    await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
-  }
-})();
 
 AppRegistry.registerComponent(appName, () => App);
--- src/hooks/use-push-notifications.ts
+++ src/hooks/use-push-notifications.ts
@@ -1,27 +1,47 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import messaging from '@react-native-firebase/messaging';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { PermissionsAndroid } from 'react-native';
 /* eslint-disable-next-line import/no-named-as-default */
 import PushNotification from 'react-native-push-notification';
 
 import { isAndroid } from 'src/config/system';
+import { useAnalytics } from 'src/utils/analytics/use-analytics.hook';
 import { isDefined } from 'src/utils/is-defined';
 
+import { AnalyticsEventCategory } from '../utils/analytics/analytics-event.enum';
+
 export const usePushNotifications = () => {
+  const { trackEvent } = useAnalytics();
+
   useEffect(() => {
-    requestUserPermission();
+    requestUserPermission(getFcmToken);
     const unsubscribe = handleForegroundNotifications();
 
     return unsubscribe;
-  });
+  }, []);
+
+  const getFcmToken = useCallback(async () => {
+    const savedFcmToken = await AsyncStorage.getItem('fcmToken').catch(() => null);
+
+    if (!isDefined(savedFcmToken)) {
+      await messaging().registerDeviceForRemoteMessages();
+      const fcmToken = await messaging().getToken();
+
+      if (isDefined(fcmToken)) {
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+
+      await trackEvent('PUSH_NOTIFICATIONS_ENABLED', AnalyticsEventCategory.General);
+    }
+  }, []);
 };
 
-const requestUserPermission = async () => {
+const requestUserPermission = async (getFcmToken: () => void) => {
   let enabled = false;
 
   if (isAndroid) {
-    enabled = await PermissionsAndroid.check(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS);
+    enabled = (await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS)) === 'granted';
   } else {
     const iosAuthStatus = await messaging().requestPermission();
     enabled =
@@ -31,19 +51,6 @@ const requestUserPermission = async () => {
 
   if (enabled) {
     await getFcmToken();
-  }
-};
-
-const getFcmToken = async () => {
-  const savedFcmToken = await AsyncStorage.getItem('fcmToken').catch(() => null);
-
-  if (!isDefined(savedFcmToken)) {
-    await messaging().registerDeviceForRemoteMessages();
-    const fcmToken = await messaging().getToken();
-
-    if (isDefined(fcmToken)) {
-      await AsyncStorage.setItem('fcmToken', fcmToken);
-    }
   }
 };
 
