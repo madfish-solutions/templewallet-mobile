e555876a27ea58d62d26086057a0e158f299afd2
lendihop <mishin.danil2017@gmail.com>
2023-07-18 22:26:06 +0200

    strict privacy policy

--- src/utils/analytics/analytics.util.ts
+++ src/utils/analytics/analytics.util.ts
@@ -6,5 +6,6 @@ import { JITSU_ANALYTICS_KEY, JITSU_TRACKING_HOST } from '../env.utils';
 export const jitsu = jitsuClient({
   tracking_host: JITSU_TRACKING_HOST,
   key: JITSU_ANALYTICS_KEY,
-  fetch
+  fetch,
+  privacy_policy: 'strict'
 });
