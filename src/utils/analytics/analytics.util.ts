23cbe758d175aee3a393f1f831471e33f31bb483
lendihop <mishin.danil2017@gmail.com>
2023-07-21 15:23:52 +0200

    jitsu client fix

--- src/utils/analytics/analytics.util.ts
+++ src/utils/analytics/analytics.util.ts
@@ -1,4 +1,4 @@
-import { jitsuClient } from '@jitsu/sdk-js';
+import { jitsuClient } from '@jitsu/sdk-js/packages/javascript-sdk';
 import fetch from 'cross-fetch';
 
 import { JITSU_ANALYTICS_KEY, JITSU_TRACKING_HOST } from '../env.utils';
@@ -6,6 +6,5 @@ import { JITSU_ANALYTICS_KEY, JITSU_TRACKING_HOST } from '../env.utils';
 export const jitsu = jitsuClient({
   tracking_host: JITSU_TRACKING_HOST,
   key: JITSU_ANALYTICS_KEY,
-  fetch,
-  ip_policy: 'strict'
+  fetch
 });
