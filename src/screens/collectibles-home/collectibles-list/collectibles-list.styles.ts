74ee6312d52fa31c0c7270c6f2f1ce4d82a22127
razbeikov <razbeikov.oleksandr@gmail.com>
2023-07-21 11:01:30 +0300

    delete justify-content for fix nft list

--- src/screens/collectibles-home/collectibles-list/collectibles-list.styles.ts
+++ src/screens/collectibles-home/collectibles-list/collectibles-list.styles.ts
@@ -5,7 +5,6 @@ import { formatSize } from '../../../styles/format-size';
 export const CollectiblesListStyles = StyleSheet.create({
   rowContainer: {
     flexDirection: 'row',
-    justifyContent: 'space-between',
     flex: 1
   },
   collectible: {
