3675d004c63b76dba0a10e0edf6b570ff952928f
herkoss <herkodmytro@gmail.com>
2023-08-07 17:33:40 +0300

    TW-786 Fixed comments

--- src/interfaces/activity.interface.ts
+++ src/interfaces/activity.interface.ts
@@ -1,6 +1,8 @@
 import { Activity, ActivityType, TzktOperationStatus } from '@temple-wallet/transactions-parser';
 import { BigNumber } from 'bignumber.js';
 
+import { TEZ_TOKEN_SLUG } from 'src/token/data/tokens-metadata';
+
 import { emptyMember } from './member.interface';
 
 export type ActivityGroup = Array<Activity>;
@@ -9,7 +11,7 @@ export const emptyActivity: Activity = {
   type: ActivityType.Send,
   status: TzktOperationStatus.Applied,
   hash: '',
-  tokensDeltas: [{ atomicAmount: new BigNumber(0), tokenSlug: 'tez' }],
+  tokensDeltas: [{ atomicAmount: new BigNumber(0), tokenSlug: TEZ_TOKEN_SLUG }],
   id: 0,
   timestamp: '',
   from: emptyMember,
