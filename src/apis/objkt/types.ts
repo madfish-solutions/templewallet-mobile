bbc65475f6add99b43b285b445b54dedf0e959d0
razbeikov <razbeikov.oleksandr@gmail.com>
2023-08-09 22:42:55 +0300

    fix ts errors in epic branch

--- src/apis/objkt/types.ts
+++ src/apis/objkt/types.ts
@@ -167,7 +167,7 @@ interface CollectibleDetailsResponse {
   name: string;
   description: string;
   thumbnail_uri: string;
-  display_uri: string | null;
+  display_uri: string;
   creators: Creator[];
   fa: Fa;
   metadata: string;
--- src/token/interfaces/collectible-interfaces.interface.ts
+++ src/token/interfaces/collectible-interfaces.interface.ts
@@ -88,7 +88,7 @@ export interface CollectibleDetailsInterface extends CollectibleCommonInterface 
   galleries: Galleries[];
   artifactUri?: string;
   thumbnailUri?: string;
-  displayUri?: string | null;
+  displayUri?: string;
 }
 
 export interface CollectibleInterface
