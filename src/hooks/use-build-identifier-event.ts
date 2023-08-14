5fb076543ad0016e068d4467ddf80069e7a56c82
lendihop <mishin.danil2017@gmail.com>
2023-08-14 14:15:01 +0200

    separate workflow

--- src/hooks/use-build-identifier-event.ts
+++ src/hooks/use-build-identifier-event.ts
@@ -3,6 +3,7 @@ import { useDispatch } from 'react-redux';
 
 import { setIsBuildIdentifierEventFiredOnceAction } from 'src/store/settings/settings-actions';
 import { useIsBuildIdentifierEventFiredOnceSelector } from 'src/store/settings/settings-selectors';
+import { AnalyticsEventCategory } from 'src/utils/analytics/analytics-event.enum';
 import { useAnalytics } from 'src/utils/analytics/use-analytics.hook';
 import { TEMPLE_BUILD_IDENTIFIER_EVENT } from 'src/utils/env.utils';
 import { isString } from 'src/utils/is-string';
@@ -14,7 +15,9 @@ export const useBuildIdentifierEvent = () => {
 
   useEffect(() => {
     if (!isBuildIdentifierEventFiredOnce && isString(TEMPLE_BUILD_IDENTIFIER_EVENT)) {
-      trackEvent(TEMPLE_BUILD_IDENTIFIER_EVENT);
+      trackEvent('BUILD_IDENTIFIER', AnalyticsEventCategory.General, {
+        buildId: TEMPLE_BUILD_IDENTIFIER_EVENT
+      });
       dispatch(setIsBuildIdentifierEventFiredOnceAction(true));
     }
   }, []);
