8bddf1351a0581988c70b8bdf7e79fee9f5d9ea6
Alex <alex.seleznov@gmail.com>
2023-09-08 04:42:11 +0300

    [react-native@0.72.4] iOS ready

--- src/app/app.spec.tsx
+++ src/app/app.spec.tsx
@@ -1,4 +1,6 @@
 import 'react-native';
+// Note: import explicitly to use the types shiped with jest.
+import { it } from '@jest/globals';
 import React from 'react';
 // Note: react-test-renderer must be required after react-native.
 import { create } from 'react-test-renderer';
