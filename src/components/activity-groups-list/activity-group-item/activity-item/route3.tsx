b29c9941ea3e12d2bc3d70d2a01f49906f98572d
lendihop <mishin.danil2017@gmail.com>
2023-09-22 19:02:56 +0200

    icon fix

--- src/components/activity-groups-list/activity-group-item/activity-item/route3.tsx
+++ src/components/activity-groups-list/activity-group-item/activity-item/route3.tsx
@@ -4,12 +4,12 @@ import { isEmpty } from 'lodash-es';
 import React, { FC, memo, useMemo } from 'react';
 import { Text, View } from 'react-native';
 
-import { useAvatarImageStyles } from 'src/components/avatar-image/avatar-image.styles';
 import { Divider } from 'src/components/divider/divider';
 import { ExternalLinkButton } from 'src/components/icon/external-link-button/external-link-button';
 import { Icon } from 'src/components/icon/icon';
 import { IconNameEnum } from 'src/components/icon/icon-name.enum';
 import { PublicKeyHashText } from 'src/components/public-key-hash-text/public-key-hash-text';
+import { useRobotIconStyles } from 'src/components/robot-icon/robot-icon.styles';
 import { calculateFiatAmount, useNonZeroAmounts } from 'src/hooks/use-non-zero-amounts.hook';
 import { ActivityAmount } from 'src/interfaces/non-zero-amounts.interface';
 import { useUsdToTokenRates } from 'src/store/currency/currency-selectors';
@@ -48,12 +48,14 @@ export const Route3: FC<{ activity: Route3Activity }> = memo(({ activity }) => {
 
 const Face: FC<{ nonZeroAmounts: Array<ActivityAmount> }> = ({ nonZeroAmounts }) => {
   const styles = useActivityGroupItemStyles();
-  const avatarImageStyles = useAvatarImageStyles();
+  const iconStyles = useRobotIconStyles();
   const commonStyles = useActivityCommonStyles();
 
   return (
     <View style={[commonStyles.row, commonStyles.itemsCenter]}>
-      <Icon name={IconNameEnum.Route3} size={formatSize(36)} style={avatarImageStyles.icon} />
+      <View style={iconStyles.root}>
+        <Icon name={IconNameEnum.Route3} size={formatSize(36)} />
+      </View>
       <Divider size={formatSize(10)} />
       <View style={styles.flex}>
         <View style={[commonStyles.row, commonStyles.justifyBetween, commonStyles.itemsStart]}>
