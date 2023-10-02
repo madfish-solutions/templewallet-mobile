4bf2b9b38481f2226915284f51b498c018ae40c6
lendihop <mishin.danil2017@gmail.com>
2023-10-02 14:25:52 +0200

    new pairs added

--- src/store/buy-with-credit-card/epics.ts
+++ src/store/buy-with-credit-card/epics.ts
@@ -3,7 +3,7 @@ import { catchError, forkJoin, from, map, Observable, of, switchMap, withLatestF
 import { Action } from 'ts-action';
 import { ofType } from 'ts-action-operators';
 
-import { getTezUahPairInfo } from 'src/apis/alice-bob';
+import { getAliceBobPairsInfo } from 'src/apis/alice-bob';
 import { getMoonPayCurrencies } from 'src/apis/moonpay';
 import { getCurrenciesInfo as getUtorgCurrenciesInfo } from 'src/apis/utorg';
 import { TopUpProviderEnum } from 'src/enums/top-up-providers.enum';
@@ -29,6 +29,7 @@ const getCurrencies$ = <T>(
     catchError(err => {
       const errorMessage = getAxiosQueryErrorMessage(err);
       if (shouldShowErrorToast) {
+        console.log(errorMessage, 'err');
         showErrorToast({ description: errorMessage });
       }
 
@@ -45,8 +46,7 @@ const loadAllCurrenciesEpic = (action$: Observable<Action>) =>
       forkJoin([
         getCurrencies$(getMoonPayCurrencies, mapMoonPayProviderCurrencies),
         getCurrencies$(getUtorgCurrenciesInfo, mapUtorgProviderCurrencies),
-        // TODO: return showing error toast as soon as Alice&Bob API starts working
-        getCurrencies$(getTezUahPairInfo, mapAliceBobProviderCurrencies, false)
+        getCurrencies$(getAliceBobPairsInfo, mapAliceBobProviderCurrencies)
       ]).pipe(
         map(([moonpayCurrencies, utorgCurrencies, tezUahPairInfo]) =>
           loadAllCurrenciesActions.success({
