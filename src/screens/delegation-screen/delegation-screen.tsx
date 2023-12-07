065d17f93997183e7bbcdb1d6abaf184b595b55b
Alex <alexseleznov@madfish.solutions>
2023-12-07 11:13:53 +0200

    TW-1175: Fix getting delegate RPC fail (#996)

--- src/screens/delegation-screen/delegation-screen.tsx
+++ src/screens/delegation-screen/delegation-screen.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { ModalsEnum } from '../../navigator/enums/modals.enum';
-import { ScreensEnum } from '../../navigator/enums/screens.enum';
-import { useNavigation } from '../../navigator/hooks/use-navigation.hook';
-import { loadBakersListActions } from '../../store/baking/baking-actions';
-import { useBakerRewardsListSelector, useSelectedBakerSelector } from '../../store/baking/baking-selectors';
-import { usePageAnalytic } from '../../utils/analytics/use-analytics.hook';
+import { ModalsEnum } from 'src/navigator/enums/modals.enum';
+import { ScreensEnum } from 'src/navigator/enums/screens.enum';
+import { useNavigation } from 'src/navigator/hooks/use-navigation.hook';
+import { loadBakersListActions } from 'src/store/baking/baking-actions';
+import { useBakerRewardsListSelector, useSelectedBakerSelector } from 'src/store/baking/baking-selectors';
+import { usePageAnalytic } from 'src/utils/analytics/use-analytics.hook';
 
 import { AboutDelegationScreen } from './about-delegation-screen/about-delegation-screen';
 import { SelectedBakerScreen } from './selected-baker-screen/selected-baker-screen';
@@ -14,7 +14,7 @@ import { SelectedBakerScreen } from './selected-baker-screen/selected-baker-scre
 export const DelegationScreen = () => {
   const dispatch = useDispatch();
   const { navigate } = useNavigation();
-  const [selectedBaker, isBakerSelected] = useSelectedBakerSelector();
+  const currentBaker = useSelectedBakerSelector();
 
   const bakerRewardsList = useBakerRewardsListSelector();
 
@@ -26,9 +26,9 @@ export const DelegationScreen = () => {
 
   return (
     <>
-      {isBakerSelected ? (
+      {currentBaker ? (
         <SelectedBakerScreen
-          baker={selectedBaker}
+          baker={currentBaker}
           bakerRewardsList={bakerRewardsList}
           onRedelegatePress={handleDelegatePress}
         />
