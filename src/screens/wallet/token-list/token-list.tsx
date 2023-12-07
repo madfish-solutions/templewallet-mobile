2671900cd9d6f131a2618ab3ea45cfcf2d45f1c0
lendihop <mishin.danil2017@gmail.com>
2023-12-07 14:44:05 +0100

    tkey appears on talent network fix

--- src/screens/wallet/token-list/token-list.tsx
+++ src/screens/wallet/token-list/token-list.tsx
@@ -24,7 +24,11 @@ import { loadAdvertisingPromotionActions } from 'src/store/advertising/advertisi
 import { useTokensApyRatesSelector } from 'src/store/d-apps/d-apps-selectors';
 import { loadPartnersPromoActions } from 'src/store/partners-promotion/partners-promotion-actions';
 import { setZeroBalancesShown } from 'src/store/settings/settings-actions';
-import { useHideZeroBalancesSelector, useIsEnabledAdsBannerSelector } from 'src/store/settings/settings-selectors';
+import {
+  useHideZeroBalancesSelector,
+  useIsEnabledAdsBannerSelector,
+  useSelectedRpcUrlSelector
+} from 'src/store/settings/settings-selectors';
 import { useCurrentAccountPkhSelector } from 'src/store/wallet/wallet-selectors';
 import { formatSize } from 'src/styles/format-size';
 import { TEZ_TOKEN_SLUG } from 'src/token/data/tokens-metadata';
@@ -33,6 +37,7 @@ import { getTokenSlug } from 'src/token/utils/token.utils';
 import { AnalyticsEventCategory } from 'src/utils/analytics/analytics-event.enum';
 import { useAnalytics } from 'src/utils/analytics/use-analytics.hook';
 import { useAccountTkeyToken, useCurrentAccountTokens } from 'src/utils/assets/hooks';
+import { isDcpNode } from 'src/utils/network.utils';
 import { OptimalPromotionAdType } from 'src/utils/optimal.utils';
 import { useTezosTokenOfCurrentAccount } from 'src/utils/wallet.utils';
 
@@ -75,6 +80,7 @@ export const TokensList = memo(() => {
   const visibleTokensList = useCurrentAccountTokens(true);
   const isEnabledAdsBanner = useIsEnabledAdsBannerSelector();
   const publicKeyHash = useCurrentAccountPkhSelector();
+  const selectedRpcUrl = useSelectedRpcUrlSelector();
   const partnersPromoShown = useIsPartnersPromoShown();
 
   const handleHideZeroBalanceChange = useCallback((value: boolean) => {
@@ -103,7 +109,13 @@ export const TokensList = memo(() => {
     }
   }, [dispatch, partnersPromoShown]);
 
-  const leadingAssets = useMemo(() => [tezosToken, tkeyToken], [tezosToken, tkeyToken]);
+  const leadingAssets = useMemo(() => {
+    if (isDcpNode(selectedRpcUrl)) {
+      return [tezosToken];
+    }
+
+    return [tezosToken, tkeyToken];
+  }, [selectedRpcUrl, tezosToken, tkeyToken]);
 
   const { filteredAssetsList, searchValue, setSearchValue } = useFilteredAssetsList(
     visibleTokensList,
