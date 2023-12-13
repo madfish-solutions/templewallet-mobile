f76e86c9f8ac772535015ee4be6d57855ce0dad6
Alex <alex.seleznov@gmail.com>
2023-12-13 18:59:52 +0200

    TW-1221: Fix app freeze. Fix unit tests

--- src/mocks/react-native-config.mock.ts
+++ src/mocks/react-native-config.mock.ts
@@ -1,4 +1,10 @@
-export {};
+import mockDotEnv from 'dotenv';
+import mockFs from 'fs';
+import mockPath from 'path';
 
 // See: https://github.com/lugg/react-native-config?tab=readme-ov-file#testing
-jest.mock('react-native-config', () => jest.requireActual('react-native-config-node'));
+jest.mock('react-native-config', () => {
+  const envFile = mockFs.readFileSync(mockPath.resolve(process.cwd(), '.env.dist'), 'utf-8');
+
+  return mockDotEnv.parse(envFile);
+});
