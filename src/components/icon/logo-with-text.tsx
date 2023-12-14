9e568a779b245b51712a4fc3cfb202a1bf736b69
lendihop <mishin.danil2017@gmail.com>
2023-12-14 11:06:33 +0100

    memo

--- src/components/icon/logo-with-text.tsx
+++ src/components/icon/logo-with-text.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react';
+import React, { memo } from 'react';
 
 import { ThemesEnum } from 'src/interfaces/theme.enum';
 import { useThemeSelector } from 'src/store/settings/settings-selectors';
@@ -6,13 +6,11 @@ import { useThemeSelector } from 'src/store/settings/settings-selectors';
 import { Icon, IconProps } from './icon';
 import { IconNameEnum } from './icon-name.enum';
 
-export const LogoWithText: FC<Omit<IconProps, 'name'>> = props => {
+export const LogoWithText = memo<Omit<IconProps, 'name'>>(props => {
   const theme = useThemeSelector();
 
-  const logoIconName = useMemo(
-    () => (theme === ThemesEnum.dark ? IconNameEnum.TempleLogoWithTextDark : IconNameEnum.TempleLogoWithTextLight),
-    [theme]
-  );
+  const logoIconName =
+    theme === ThemesEnum.dark ? IconNameEnum.TempleLogoWithTextDark : IconNameEnum.TempleLogoWithTextLight;
 
   return <Icon name={logoIconName} {...props} />;
-};
+});
