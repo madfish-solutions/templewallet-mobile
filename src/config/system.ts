62439f3d5a050f082623d605f681a221c3abe32d
Alex <alex.seleznov@gmail.com>
2023-12-15 11:02:14 +0200

    TW-1221: Fix app freeze. Refactor

--- src/config/system.ts
+++ src/config/system.ts
@@ -6,6 +6,6 @@ export const isIOS = Platform.OS === 'ios';
 export const isAndroid = Platform.OS === 'android';
 
 /** @deprecated // BAD PRACTICE */
-
 export const EMPTY_PUBLIC_KEY_HASH = 'EMPTY_PUBLIC_KEY_HASH';
+
 export const manufacturer = getManufacturerSync();
