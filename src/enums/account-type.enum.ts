200e5fbca44babb53b6f78dc6ed16e299cc2ebe1
lendihop <mishin.danil2017@gmail.com>
2024-01-18 16:38:58 +0100

    fix ts-prune

--- src/enums/account-type.enum.ts
+++ src/enums/account-type.enum.ts
@@ -4,11 +4,6 @@ export enum AccountTypeEnum {
   WATCH_ONLY_DEBUG = 'WATCH_ONLY_DEBUG'
 }
 
-export enum ImportAccountTypeEnum {
-  PRIVATE_KEY = 'PRIVATE_KEY',
-  SEED_PHRASE = 'SEED_PHRASE'
-}
-
 export enum ImportAccountDerivationEnum {
   DEFAULT = 'DEFAULT',
   CUSTOM_PATH = 'CUSTOM_PATH'
