1336c63aa6515d5cf009471e628e88c4730af397
lendihop <mishin.danil2017@gmail.com>
2024-02-13 11:18:14 +0100

    audit fix

--- audit-deps.mjs
+++ audit-deps.mjs
@@ -6,7 +6,7 @@ exec("yarn audit --level high", (error, stdout) => {
 
     if (stdout.includes("High") || stdout.includes("Critical")) {
       // delete after ip package will be patched
-      if (stdout.includes("1096432") && stdout.includes("1 High") && stdout.includes("No patch available") && !stdout.includes("Critical")) return;
+      if (stdout.includes("@react-native-community/cli-doctor > ip") && stdout.includes("1 High") && stdout.includes("No patch available") && !stdout.includes("Critical")) return;
 
       throw new Error("Audit failed");
     }
