fe3cd2bd327fec68230030379c657ea871627a9f
Inokentii Mazhara <keshan3262@gmail.com>
2024-02-16 13:45:56 +0200

    TW-1216 Prevent 'onEmptyPromotionReceived' call before an attempt to fetch ads

--- src/components/optimal-promotion-item/optimal-promotion-item.tsx
+++ src/components/optimal-promotion-item/optimal-promotion-item.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from 'react';
+import React, { FC, useEffect, useRef, useState } from 'react';
 import { StyleProp, ViewStyle } from 'react-native';
 
 import { PromotionItem } from 'src/components/promotion-item/promotion-item';
@@ -10,6 +10,7 @@ import {
   usePartnersPromoLoadingSelector,
   usePartnersPromoSelector
 } from 'src/store/partners-promotion/partners-promotion-selectors';
+import { useTimeout } from 'src/utils/hooks';
 import { useIsEmptyPromotion } from 'src/utils/optimal.utils';
 
 import { TextPromotionItem } from '../text-promotion-item/text-promotion-item';
@@ -38,14 +39,23 @@ export const OptimalPromotionItem: FC<Props> = ({
   const partnersPromotionLoading = usePartnersPromoLoadingSelector();
   const partnersPromotionEnabled = useIsPartnersPromoEnabledSelector();
   const { isHiddenTemporarily, hidePromotion } = useAdTemporaryHiding(id);
+  const prevIsLoadingRef = useRef(partnersPromotionLoading);
+  const [shouldPreventShowingPrevAd, setShouldPreventShowingPrevAd] = useState(true);
 
   const promotionIsEmpty = useIsEmptyPromotion(partnersPromotion);
 
   useEffect(() => {
-    if (partnersPromotionEnabled && onEmptyPromotionReceived && promotionIsEmpty) {
+    if (prevIsLoadingRef.current && !partnersPromotionLoading) {
+      setShouldPreventShowingPrevAd(false);
+    }
+  }, [partnersPromotionLoading]);
+  useTimeout(() => setShouldPreventShowingPrevAd(false), 2000);
+
+  useEffect(() => {
+    if (partnersPromotionEnabled && onEmptyPromotionReceived && promotionIsEmpty && !shouldPreventShowingPrevAd) {
       onEmptyPromotionReceived();
     }
-  }, [partnersPromotionEnabled, onEmptyPromotionReceived, promotionIsEmpty]);
+  }, [partnersPromotionEnabled, onEmptyPromotionReceived, promotionIsEmpty, shouldPreventShowingPrevAd]);
 
   if (!partnersPromotionEnabled || promotionIsEmpty || isHiddenTemporarily) {
     return null;
@@ -59,7 +69,7 @@ export const OptimalPromotionItem: FC<Props> = ({
         headline={partnersPromotion?.copy?.headline ?? 'fallback'}
         imageUri={partnersPromotion.image}
         link={partnersPromotion.link}
-        loading={partnersPromotionLoading}
+        loading={partnersPromotionLoading || shouldPreventShowingPrevAd}
         shouldShowCloseButton={shouldShowCloseButton}
         style={style}
         onClose={hidePromotion}
