f6bbeeaae2a922a2ed1e0ae0a0a3ef8426e170c1
Inokentii Mazhara <keshan3262@gmail.com>
2024-11-04 16:46:44 +0200

    TW-1574 Refactoring according to comments

--- src/utils/universal-links.ts
+++ src/utils/universal-links.ts
@@ -4,8 +4,7 @@ import { UNIVERSAL_LINKS_DOMAIN_URI_PREFIX } from './env.utils';
 import { isDefined } from './is-defined';
 
 export const getTempleUniversalLink = (domain = '/', queryParams?: Partial<StringRecord>) => {
-  const url = new URL(UNIVERSAL_LINKS_DOMAIN_URI_PREFIX);
-  url.pathname = domain;
+  const url = new URL(domain, UNIVERSAL_LINKS_DOMAIN_URI_PREFIX);
   url.search = new URLSearchParams(pickBy(queryParams ?? {}, isDefined)).toString();
 
   return url.toString();
