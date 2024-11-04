ff9b46ab1ba813d42c2a2ce16a6ee5729f23172c
Inokentii Mazhara <keshan3262@gmail.com>
2024-11-04 16:45:49 +0200

    TW-1574 Refactoring according to comments

--- src/utils/nft-universal-links.ts
+++ src/utils/nft-universal-links.ts
@@ -1,27 +1,16 @@
-import { transform } from 'lodash-es';
-
 import { UNIVERSAL_LINKS_DOMAIN_URI_PREFIX } from './env.utils';
+import { isString } from './is-string';
 import { isTruthy } from './is-truthy';
 import { getTempleUniversalLink } from './universal-links';
 import { buildSafeURL } from './url.utils';
 
 export const buildCollectibleUniversalLink = (slug: string, image?: string, title?: string, description?: string) =>
-  getTempleUniversalLink(
-    '/nft',
-    transform<string | undefined, StringRecord>(
-      { slug, image, title, description },
-      (acc, value, key) => {
-        if (isTruthy(value) && key === 'description') {
-          acc[key] = value.length > 500 ? `${value.slice(0, 500)}...` : value;
-        } else if (isTruthy(value)) {
-          acc[key] = value;
-        }
-
-        return acc;
-      },
-      {}
-    )
-  );
+  getTempleUniversalLink('/nft', {
+    slug,
+    image,
+    title,
+    description: isString(description) && description.length > 500 ? `${description.slice(0, 500)}...` : description
+  });
 
 export const isCollectibleUniversalLink = (urlStr: string) => {
   const pathname = buildSafeURL(urlStr)?.pathname;
--- src/utils/universal-links.ts
+++ src/utils/universal-links.ts
@@ -1,9 +1,12 @@
-import { UNIVERSAL_LINKS_DOMAIN_URI_PREFIX } from './env.utils';
+import { pickBy } from 'lodash-es';
 
-export const getTempleUniversalLink = (domain = '/', queryParams?: StringRecord) => {
+import { UNIVERSAL_LINKS_DOMAIN_URI_PREFIX } from './env.utils';
+import { isDefined } from './is-defined';
+
+export const getTempleUniversalLink = (domain = '/', queryParams?: Partial<StringRecord>) => {
   const url = new URL(UNIVERSAL_LINKS_DOMAIN_URI_PREFIX);
   url.pathname = domain;
-  url.search = new URLSearchParams(queryParams).toString();
+  url.search = new URLSearchParams(pickBy(queryParams ?? {}, isDefined)).toString();
 
   return url.toString();
 };
