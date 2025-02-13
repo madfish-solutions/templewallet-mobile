cb85f0b21b170ad8fc98385a4eab7af95ed7f6b9
Inokentii Mazhara <keshan3262@gmail.com>
2025-02-13 16:26:14 +0200

    TW-1654 Revert adding 'Temple Tap' banner

--- src/screens/d-apps/promotion-carousel/promotion-carousel.selectors.ts
+++ src/screens/d-apps/promotion-carousel/promotion-carousel.selectors.ts
@@ -2,6 +2,5 @@ export enum PromotionCarouselSelectors {
   quipuSwapBanner = 'Promotion Carousel/QuipuSwap Banner',
   yupanaBanner = 'Promotion Carousel/Yupana Banner',
   supportUkraineBanner = 'Promotion Carousel/Support Ukraine Banner',
-  madFishBlogBanner = 'Promotion Carousel/MadFish Blog Banner',
-  templeTapBanner = 'Promotion Carousel/TempleTap Banner'
+  madFishBlogBanner = 'Promotion Carousel/MadFish Blog Banner'
 }
