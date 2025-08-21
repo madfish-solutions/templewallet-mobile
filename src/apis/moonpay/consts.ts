4bc4e946c71ee091ae7f967adfb9533327c20964
Inokentii Mazhara <keshan3262@gmail.com>
2025-08-21 12:11:35 +0300

    TW-1869 Update Moonpay API (#1147)

--- src/apis/moonpay/consts.ts
+++ src/apis/moonpay/consts.ts
@@ -1,4 +1,4 @@
 export const MOONPAY_DOMAIN = 'https://buy.moonpay.com';
 export const MOONPAY_ASSETS_BASE_URL = 'https://static.moonpay.com';
 export const MOONPAY_API_URL = 'https://api.moonpay.com';
-export const MOONPAY_API_KEY = 'pk_live_PrSDks3YtrreqFifd0BsIji7xPXjSGx';
+export const MOONPAY_API_KEY = 'pk_live_dhlzC1QmhXBL00W6mnUVC7jtv5oblCgU';
