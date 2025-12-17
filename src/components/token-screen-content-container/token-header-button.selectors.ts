4a36843eec5360135af9fe581b46a9af5dbc1fec
Inokentii Mazhara <keshan3262@gmail.com>
2025-12-17 15:40:25 +0200

    TW-2004 Delete links to Yupana (#1166)

--- src/components/token-screen-content-container/token-header-button.selectors.ts
+++ src/components/token-screen-content-container/token-header-button.selectors.ts
@@ -1,5 +1,5 @@
-import { YUPANA_LINK } from 'src/utils/constants/apy';
+import { YOUVES_LINK } from 'src/utils/constants/apy';
 
 export const apyLinkSelectors: Record<string, string> = {
-  [YUPANA_LINK]: 'TokenPage/Yupana/Apy'
+  [YOUVES_LINK]: 'TokenPage/Youves/Apy'
 };
