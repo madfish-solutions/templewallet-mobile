5d13ef2bc7d67b7d67d91b6b11ca7e0fc62edabe
Inokentii Mazhara <keshan3262@gmail.com>
2025-12-17 15:42:56 +0200

    TW-2035 Use future entrypoint for getting liquidity baking exchange rate (#1165)

--- src/apis/temple-wallet/index.ts
+++ src/apis/temple-wallet/index.ts
@@ -1,6 +1,9 @@
 import { catchError, from, map, of } from 'rxjs';
 
-import { templeAdsApi, templeWalletApi } from '../../api.service';
+import { templeAdsApi, templeWalletApi } from 'src/api.service';
+import { BlockInfo } from 'src/interfaces/block-info.interface';
+
+import { LiquidityBakingFarm } from '../liquidity-baking/types';
 
 export enum ABTestGroup {
   A = 'A',
@@ -20,3 +23,23 @@ export const getABGroup$ = () =>
 
 export const fetchEnableInternalHypelabAds = () =>
   templeAdsApi.get<boolean>('/ads-rules/2.0.13/enable-internal-hypelab-ads').then(response => response.data);
+
+interface LiquidityBakingStatsResponse {
+  stats: Pick<
+    LiquidityBakingFarm,
+    | 'contractAddress'
+    | 'apr'
+    | 'depositExchangeRate'
+    | 'dailyDistribution'
+    | 'dailyDistributionDollarEquivalent'
+    | 'earnExchangeRate'
+    | 'vestingPeriodSeconds'
+    | 'staked'
+    | 'tvlInUsd'
+    | 'tvlInStakedToken'
+  >;
+  blockInfo: BlockInfo;
+}
+
+export const getLiquidityBakingStats = () =>
+  templeWalletApi.get<LiquidityBakingStatsResponse>('/liquidity-baking/stats').then(response => response.data);
--- src/store/farms/epics.ts
+++ src/store/farms/epics.ts
@@ -8,11 +8,9 @@ import { getV3FarmsList } from 'src/apis/quipuswap-staking';
 import { FarmsProviderEnum } from 'src/enums/farms-provider.enum';
 import { showErrorToast } from 'src/toast/error-toast.utils';
 import { showFailedStakeLoadWarning } from 'src/toast/toast.utils';
-import { KNOWN_TOKENS_SLUGS } from 'src/token/data/token-slugs';
-import { TEZ_TOKEN_SLUG } from 'src/token/data/tokens-metadata';
 import { getAxiosQueryErrorMessage } from 'src/utils/get-axios-query-error-message';
 import { createReadOnlyTezosToolkit } from 'src/utils/rpc/tezos-toolkit.utils';
-import { withSelectedAccount, withSelectedRpcUrl } from 'src/utils/wallet.utils';
+import { withSelectedRpcUrl } from 'src/utils/wallet.utils';
 
 import type { RootState } from '../types';
 
@@ -23,7 +21,7 @@ import {
   loadFarmsByProviderActions,
   loadSingleFarmStakeActions
 } from './actions';
-import { getFarmStake, GetFarmStakeError, toUserStakeValueInterface, withExchangeRates } from './utils';
+import { getFarmStake, GetFarmStakeError, toUserStakeValueInterface } from './utils';
 
 const loadSingleFarmLastStake: Epic<Action, Action, RootState> = (action$, state$) =>
   action$.pipe(
@@ -54,18 +52,10 @@ const loadSingleFarmLastStake: Epic<Action, Action, RootState> = (action$, state
     })
   );
 
-const loadAllFarms: Epic<Action, Action, RootState> = (action$, state$) =>
+const loadAllFarms: Epic = action$ =>
   action$.pipe(
     ofType(loadAllFarmsAction),
-    withExchangeRates(state$),
-    withSelectedAccount(state$),
-    withSelectedRpcUrl(state$),
-    switchMap(([[[, exchangeRates], selectedAccount], rpcUrl]) => {
-      const tezos = createReadOnlyTezosToolkit(rpcUrl, selectedAccount);
-      const { [KNOWN_TOKENS_SLUGS.tzBTC]: tzbtcExchangeRate, [TEZ_TOKEN_SLUG]: tezExchangeRate } = exchangeRates;
-
-      return forkJoin([getLiquidityBakingFarm(tezos, tezExchangeRate, tzbtcExchangeRate), getV3FarmsList()]);
-    }),
+    switchMap(() => forkJoin([getLiquidityBakingFarm(), getV3FarmsList()])),
     switchMap(([lbFarm, v3Farms]) =>
       of(
         loadFarmsByProviderActions.success({ data: [lbFarm], provider: FarmsProviderEnum.LiquidityBaking }),
@@ -82,19 +72,13 @@ const loadAllFarms: Epic<Action, Action, RootState> = (action$, state$) =>
     })
   );
 
-const loadFarmsByProvider: Epic<Action, Action, RootState> = (action$, state$) =>
+const loadFarmsByProvider: Epic = action$ =>
   action$.pipe(
     ofType(loadFarmsByProviderActions.submit),
-    withExchangeRates(state$),
-    withSelectedAccount(state$),
-    withSelectedRpcUrl(state$),
-    switchMap(([[[{ payload: farmsProvider }, exchangeRates], selectedAccount], rpcUrl]) => {
-      const tezos = createReadOnlyTezosToolkit(rpcUrl, selectedAccount);
-      const { [KNOWN_TOKENS_SLUGS.tzBTC]: tzbtcExchangeRate, [TEZ_TOKEN_SLUG]: tezExchangeRate } = exchangeRates;
-
+    switchMap(({ payload: farmsProvider }) => {
       return from(
         farmsProvider === FarmsProviderEnum.LiquidityBaking
-          ? getLiquidityBakingFarm(tezos, tezExchangeRate, tzbtcExchangeRate).then(lbFarm => [lbFarm])
+          ? getLiquidityBakingFarm().then(lbFarm => [lbFarm])
           : getV3FarmsList()
       ).pipe(
         map(data => loadFarmsByProviderActions.success({ data, provider: farmsProvider })),
@@ -107,15 +91,10 @@ const loadFarmsByProvider: Epic<Action, Action, RootState> = (action$, state$) =
     })
   );
 
-const loadAllFarmsAndStakes: Epic<Action, Action, RootState> = (action$, state$) =>
+const loadAllFarmsAndStakes: Epic = action$ =>
   action$.pipe(
     ofType(loadAllFarmsAndStakesAction),
-    withExchangeRates(state$),
-    withSelectedRpcUrl(state$),
-    exhaustMap(([[{ payload: accountPkh }, exchangeRates], rpcUrl]) => {
-      const tezos = createReadOnlyTezosToolkit(rpcUrl);
-      const { [KNOWN_TOKENS_SLUGS.tzBTC]: tzbtcExchangeRate, [TEZ_TOKEN_SLUG]: tezExchangeRate } = exchangeRates;
-
+    exhaustMap(({ payload: accountPkh }) => {
       const makeFarmsListErrorHandler = (farmsProvider: FarmsProviderEnum) => (err: unknown) => {
         showErrorToast({ description: getAxiosQueryErrorMessage(err) });
 
@@ -136,7 +115,7 @@ const loadAllFarmsAndStakes: Epic<Action, Action, RootState> = (action$, state$)
           ),
           catchError(makeFarmsListErrorHandler(FarmsProviderEnum.Quipuswap))
         ),
-        from(getLiquidityBakingFarm(tezos, tezExchangeRate, tzbtcExchangeRate)).pipe(
+        from(getLiquidityBakingFarm()).pipe(
           switchMap(lbFarm =>
             of(
               loadFarmsByProviderActions.success({ data: [lbFarm], provider: FarmsProviderEnum.LiquidityBaking }),
