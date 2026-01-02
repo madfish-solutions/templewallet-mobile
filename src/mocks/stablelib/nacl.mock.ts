7d708c29540d5edd230774bcc6816360daad1120
Inokentii Mazhara <keshan3262@gmail.com>
2026-01-02 11:52:25 +0200

    TW-2069 Fix tests

---
+++ src/mocks/stablelib/nacl.mock.ts
@@ -0,0 +1,4 @@
+export const box = jest.fn();
+export const openBox = jest.fn();
+export const secretBox = jest.fn();
+export const openSecretBox = jest.fn();
---
+++ src/mocks/stablelib/wipe.mock.ts
@@ -0,0 +1,1 @@
+export const wipe = jest.fn();
