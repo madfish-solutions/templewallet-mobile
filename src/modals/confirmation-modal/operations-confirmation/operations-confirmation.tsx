5af9fa6360898c5a459f62bd31ab29a91bd946c2
lendihop <mishin.danil2017@gmail.com>
2026-01-19 14:07:27 +0100

    TW-2081: allow bakers with disabled delegation

--- src/modals/confirmation-modal/operations-confirmation/operations-confirmation.tsx
+++ src/modals/confirmation-modal/operations-confirmation/operations-confirmation.tsx
@@ -1,13 +1,14 @@
 import { OpKind } from '@taquito/rpc';
 import { ParamsWithKind } from '@taquito/taquito';
 import { FormikProvider, useFormik } from 'formik';
-import React, { FC, ReactNode, useEffect } from 'react';
+import React, { FC, ReactNode, useEffect, useMemo } from 'react';
 import { Text, View } from 'react-native';
 
 import { AccountDropdownItem } from 'src/components/account-dropdown/account-dropdown-item/account-dropdown-item';
 import { ButtonLargePrimary } from 'src/components/button/button-large/button-large-primary/button-large-primary';
 import { ButtonLargeSecondary } from 'src/components/button/button-large/button-large-secondary/button-large-secondary';
 import { DelegateDisclaimer } from 'src/components/delegate-disclaimer/delegate-disclaimer';
+import { Disclaimer } from 'src/components/disclaimer/disclaimer';
 import { Divider } from 'src/components/divider/divider';
 import { LoadingPlaceholder } from 'src/components/loading-placeholder/loading-placeholder';
 import { ModalButtonsContainer } from 'src/components/modal-buttons-container/modal-buttons-container';
@@ -17,6 +18,7 @@ import { useNetworkInfo } from 'src/hooks/use-network-info.hook';
 import { AccountInterface } from 'src/interfaces/account.interface';
 import { TestIdProps } from 'src/interfaces/test-id.props';
 import { useNavigation } from 'src/navigator/hooks/use-navigation.hook';
+import { useBakersListSelector } from 'src/store/baking/baking-selectors';
 import { formatSize } from 'src/styles/format-size';
 import { AnalyticsEventCategory } from 'src/utils/analytics/analytics-event.enum';
 import { useAnalytics } from 'src/utils/analytics/use-analytics.hook';
@@ -34,8 +36,11 @@ import { useFeeForm } from './hooks/use-fee-form.hook';
 import { useOperationsConfirmationStyles } from './operations-confirmation.styles';
 import { OperationsPreview } from './operations-preview/operations-preview';
 
-const disclaimerMessage =
+const supportUkraineDisclaimerMessage =
   'By delegating for this Baker, you agree that all of your rewards will go to support Ukraine. Thank you for your contribution!';
+
+const delegationDisabledDisclaimerMessage =
+  'This baker doesn’t offer rewards for delegation. If you want to earn delegation rewards, please choose a different baker.';
 
 interface Props extends TestIdProps {
   sender: AccountInterface;
@@ -75,6 +80,8 @@ export const OperationsConfirmation: FC<Props> = ({
     formValidationSchema,
     formInitialValues
   } = useFeeForm(opParams, estimations.data);
+
+  const knownBakers = useBakersListSelector();
 
   const handleSubmit = ({ gasFeeSum, storageLimitSum }: FeeFormInputValues) => {
     if (isTruthy(testID)) {
@@ -120,6 +127,17 @@ export const OperationsConfirmation: FC<Props> = ({
     [estimations.error, onEstimationError]
   );
 
+  const shouldShowDelegationDisabledDisclaimer = useMemo(() => {
+    const targetBakerAddress =
+      opParams[0]?.kind === OpKind.DELEGATION && isDefined(opParams[0]?.delegate) ? opParams[0].delegate : null;
+
+    const targetBaker = isDefined(targetBakerAddress)
+      ? knownBakers.find(baker => baker.address === targetBakerAddress)
+      : null;
+
+    return isDefined(targetBaker) && !targetBaker.delegation.enabled;
+  }, [knownBakers, opParams]);
+
   return (
     <FormikProvider value={formik}>
       <ScreenContainer>
@@ -131,7 +149,7 @@ export const OperationsConfirmation: FC<Props> = ({
             {opParams[0]?.kind === OpKind.DELEGATION && opParams[0]?.delegate === HELP_UKRAINE_BAKER_ADDRESS && (
               <>
                 <Divider size={formatSize(12)} />
-                <DelegateDisclaimer title="This Baker helps Ukraine 🇺🇦" text={disclaimerMessage} />
+                <DelegateDisclaimer title="This Baker helps Ukraine 🇺🇦" text={supportUkraineDisclaimerMessage} />
                 <Divider size={formatSize(28)} />
               </>
             )}
@@ -149,7 +167,16 @@ export const OperationsConfirmation: FC<Props> = ({
             <Divider size={formatSize(8)} />
 
             <OperationsPreview opParams={opParamsWithEstimations} />
-            {disclaimer}
+
+            {shouldShowDelegationDisabledDisclaimer ? (
+              <>
+                <Divider size={formatSize(8)} />
+                <Disclaimer title="Delegation is not available" texts={[delegationDisabledDisclaimerMessage]} />
+              </>
+            ) : (
+              disclaimer
+            )}
+
             <Divider />
 
             <FeeFormInput
