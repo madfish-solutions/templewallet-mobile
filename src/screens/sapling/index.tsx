d83f695e42d7cb8a5c395ea65e72ef0ca09e8107
Inokentii Mazhara <keshan3262@gmail.com>
2026-03-16 10:43:20 +0200

    TW-1955 Implement sending operations using proxy

--- src/screens/sapling/index.tsx
+++ src/screens/sapling/index.tsx
@@ -1,11 +1,18 @@
+import { nanoid } from '@reduxjs/toolkit';
+import { OpKind } from '@taquito/rpc';
 import BigNumber from 'bignumber.js';
 import React, { FC, memo, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Text, View } from 'react-native';
 import { WebView, WebViewMessageEvent } from 'react-native-webview';
 import type { WebViewErrorEvent } from 'react-native-webview/lib/WebViewTypes';
+import { useDispatch } from 'react-redux';
 import { Subject } from 'rxjs';
 
+import { AddressInput } from 'src/components/address-input/address-input';
+import { AssetAmountInput, AssetAmountInterface } from 'src/components/asset-amount-input/asset-amount-input';
+import { ButtonLargePrimary } from 'src/components/button/button-large/button-large-primary/button-large-primary';
 import { ButtonSmallSecondary } from 'src/components/button/button-small/button-small-secondary/button-small-secondary';
+import { Divider } from 'src/components/divider/divider';
 import { OVERLAY_SHOW_TIMEOUT } from 'src/components/mnemonic/mnemonic.config';
 import { MnemonicStyles } from 'src/components/mnemonic/mnemonic.styles';
 import { RevealSecretView } from 'src/components/mnemonic/reveal-secret-view/reveal-secret-view';
@@ -13,8 +20,12 @@ import { ScreenContainer } from 'src/components/screen-container/screen-containe
 import { StyledTextInput } from 'src/components/styled-text-input/styled-text-input';
 import { StyledTextInputStyles } from 'src/components/styled-text-input/styled-text-input.styles';
 import { AccountTypeEnum } from 'src/enums/account-type.enum';
+import { VisibilityEnum } from 'src/enums/visibility.enum';
 import { useActiveTimer } from 'src/hooks/use-active-timer.hook';
+import { ConfirmationTypeEnum } from 'src/interfaces/confirm-payload/confirmation-type.enum';
+import { ModalsEnum } from 'src/navigator/enums/modals.enum';
 import { useShelter } from 'src/shelter/use-shelter.hook';
+import { navigateAction } from 'src/store/root-state.actions';
 import { useSelectedRpcUrlSelector } from 'src/store/settings/settings-selectors';
 import {
   useCurrentAccountTezosBalance,
@@ -24,10 +35,18 @@ import {
 import { formatSize } from 'src/styles/format-size';
 import { showErrorToast } from 'src/toast/toast.utils';
 import { TEZ_TOKEN_METADATA } from 'src/token/data/tokens-metadata';
-import { SaplingIncomingTransaction, SaplingOutgoingTransaction } from 'src/types/sapling-contract-transaction';
+import { TokenInterface } from 'src/token/interfaces/token.interface';
+import {
+  SaplingContractTransaction,
+  SaplingIncomingTransaction,
+  SaplingOutgoingTransaction
+} from 'src/types/sapling-contract-transaction';
 import { copyStringToClipboard } from 'src/utils/clipboard.utils';
 import { useInterval } from 'src/utils/hooks';
+import { ZERO } from 'src/utils/number.util';
 import { mutezToTz } from 'src/utils/tezos.util';
+
+const saplingContract = 'KT1KzAPQdpziH3bxxJXQNmNQA46oo8tnDQfj';
 
 export const Sapling: FC = () => {
   const { revealSeedPhrase, revealSecretKey } = useShelter();
@@ -35,6 +54,7 @@ export const Sapling: FC = () => {
   const hdAccounts = useHdAccountListSelector();
   const webViewRef = useRef<WebView>(null);
   const [proxyError, setProxyError] = useState(false);
+  const [derivationInput, setDerivationInput] = useState<Record<string, unknown>>({});
   const [saplingCredentials, setSaplingCredentials] = useState<SaplingCredentials | null>(null);
   const webViewMessageSubject = useMemo(() => new Subject<WebViewMessageEvent>(), []);
 
@@ -43,49 +63,62 @@ export const Sapling: FC = () => {
     [hdAccounts, account.publicKeyHash]
   );
 
-  const handleWebViewLoad = useCallback(() => {
-    const handleSaplingCredentialsMessage = () => {
+  const doSaplingProxyOperation = useCallback(
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    (operation: Record<string, unknown>, errorTitle: string, onSuccess: SyncFn<any>) => {
+      const messageId = nanoid();
       const sub = webViewMessageSubject.subscribe(event => {
-        sub.unsubscribe();
         try {
           const data = JSON.parse(event.nativeEvent.data);
+
+          if (data.id !== messageId) {
+            return;
+          }
+
+          sub.unsubscribe();
+
           if ('error' in data) {
             throw new Error(data.error);
           }
-          setSaplingCredentials(data);
+          onSuccess(data);
         } catch (err) {
           console.error(err);
           showErrorToast({
-            title: 'Failed to get sapling credentials',
+            title: errorTitle,
             description: err instanceof Error ? err.message : 'Unknown error'
           });
-          setProxyError(true);
         }
+      });
+
+      webViewRef.current!.postMessage(JSON.stringify({ ...operation, id: messageId }));
+    },
+    [webViewMessageSubject]
+  );
+
+  const handleWebViewLoad = useCallback(() => {
+    const doSaplingCredentialsOperation = (payload: Record<string, unknown>) => {
+      doSaplingProxyOperation({ method: 'saplingCredentials', payload }, 'Failed to get sapling credentials', data => {
+        setSaplingCredentials(data);
       });
     };
 
     if (account.type === AccountTypeEnum.HD_ACCOUNT) {
       revealSeedPhrase({
         successCallback: mnemonic => {
-          handleSaplingCredentialsMessage();
-          webViewRef.current!.postMessage(
-            JSON.stringify({
-              method: 'saplingCredentials',
-              payload: { mnemonic, hdIndex: hdIndex === 0 ? undefined : hdIndex }
-            })
-          );
+          setDerivationInput({ mnemonic, hdIndex: hdIndex === 0 ? undefined : hdIndex });
+          doSaplingCredentialsOperation({ mnemonic, hdIndex: hdIndex === 0 ? undefined : hdIndex });
         }
       });
     } else {
       revealSecretKey({
         publicKeyHash: account.publicKeyHash,
         successCallback: privateKey => {
-          handleSaplingCredentialsMessage();
-          webViewRef.current!.postMessage(JSON.stringify({ method: 'saplingCredentials', payload: { privateKey } }));
+          setDerivationInput({ privateKey });
+          doSaplingCredentialsOperation({ privateKey });
         }
       });
     }
-  }, [account.type, account.publicKeyHash, hdIndex, webViewMessageSubject, revealSeedPhrase, revealSecretKey]);
+  }, [account.type, account.publicKeyHash, hdIndex, doSaplingProxyOperation, revealSeedPhrase, revealSecretKey]);
 
   const handleWebViewMessage = useCallback(
     (event: WebViewMessageEvent) => webViewMessageSubject.next(event),
@@ -107,7 +140,7 @@ export const Sapling: FC = () => {
         <WebView
           ref={webViewRef}
           source={{
-            uri: 'https://ipfs.io/ipfs/bafybeieam7mx3vtpjshwlnmnff7xidalxqkwfegs4tkcsprsa3spp6e2ga/index.html'
+            uri: 'https://ipfs.io/ipfs/bafybeiapyc6odu5pospewaagwbw54hporwzee4t5g7npsj6j7uto74ucx4/index.html'
           }}
           onLoad={handleWebViewLoad}
           onError={handleWebViewError}
@@ -117,8 +150,8 @@ export const Sapling: FC = () => {
       {saplingCredentials ? (
         <SaplingPageContent
           saplingCredentials={saplingCredentials}
-          webViewMessageSubject={webViewMessageSubject}
-          webViewRef={webViewRef}
+          doSaplingProxyOperation={doSaplingProxyOperation}
+          derivationInput={derivationInput}
         />
       ) : proxyError ? (
         <Text>Failed to load credentials</Text>
@@ -136,8 +169,9 @@ interface SaplingCredentials {
 
 interface SaplingPageContentProps {
   saplingCredentials: SaplingCredentials;
-  webViewMessageSubject: Subject<WebViewMessageEvent>;
-  webViewRef: React.RefObject<WebView>;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  doSaplingProxyOperation: (operation: Record<string, unknown>, errorTitle: string, onSuccess: SyncFn<any>) => void;
+  derivationInput: Record<string, unknown>;
 }
 
 interface AccountState {
@@ -149,14 +183,135 @@ interface AccountState {
 }
 
 const SaplingPageContent = memo(
-  ({ saplingCredentials, webViewMessageSubject, webViewRef }: SaplingPageContentProps) => {
+  ({ saplingCredentials, doSaplingProxyOperation, derivationInput }: SaplingPageContentProps) => {
     const { viewingKey, saplingAddress } = saplingCredentials;
+    const dispatch = useDispatch();
+    const account = useRawCurrentAccountSelector()!;
     const [displayedViewingKey, setDisplayedViewingKey] = useState<string>();
     const { activeTimer, clearActiveTimer } = useActiveTimer();
     const rawUnshieldedBalance = useCurrentAccountTezosBalance();
-    const saplingContract = 'KT1KzAPQdpziH3bxxJXQNmNQA46oo8tnDQfj';
     const rpcUrl = useSelectedRpcUrlSelector();
     const [accountState, setAccountState] = useState<AccountState | null>(null);
+    const assetToShield = useMemo(
+      () => ({ ...TEZ_TOKEN_METADATA, balance: rawUnshieldedBalance, visibility: VisibilityEnum.Visible }),
+      [rawUnshieldedBalance]
+    );
+    const assetToUnshield = useMemo(
+      () => ({
+        ...TEZ_TOKEN_METADATA,
+        balance: (accountState ? new BigNumber(accountState.shieldedBalance) : ZERO).toFixed(),
+        visibility: VisibilityEnum.Visible
+      }),
+      [accountState]
+    );
+    const [shieldInputValue, setShieldInputValue] = useState<AssetAmountInterface>({
+      asset: assetToShield,
+      amount: ZERO
+    });
+    const [saplingInputValue, setSaplingInputValue] = useState<AssetAmountInterface>({
+      asset: assetToUnshield,
+      amount: ZERO
+    });
+    const [unshieldInputValue, setUnshieldInputValue] = useState<AssetAmountInterface>({
+      asset: assetToUnshield,
+      amount: ZERO
+    });
+    const [saplingDestinationAddress, setSaplingDestinationAddress] = useState<string>('');
+
+    const shieldAmountInputAssetsList = useMemo<TokenInterface[]>(() => [assetToShield], [assetToShield]);
+    const unshieldAmountInputAssetsList = useMemo<TokenInterface[]>(() => [assetToUnshield], [assetToUnshield]);
+
+    const prepareSaplingContractTransaction = useCallback(
+      (transaction: SaplingContractTransaction, errorTitle: string, onSuccess: (data: { txData: string }) => void) => {
+        doSaplingProxyOperation(
+          { method: 'prepareTransaction', payload: { transaction, saplingContract, rpcUrl, ...derivationInput } },
+          errorTitle,
+          onSuccess
+        );
+      },
+      [doSaplingProxyOperation, derivationInput, rpcUrl]
+    );
+
+    const goToSaplingConfirmation = useCallback(
+      (amountMutez: BigNumber, txData: string, testID: string) => {
+        dispatch(
+          navigateAction(ModalsEnum.Confirmation, {
+            type: ConfirmationTypeEnum.InternalOperations,
+            opParams: [
+              {
+                kind: OpKind.TRANSACTION,
+                to: saplingContract,
+                amount: amountMutez.toNumber(),
+                mutez: true,
+                parameter: { entrypoint: 'default', value: [{ bytes: txData }] }
+              }
+            ],
+            testID
+          })
+        );
+      },
+      [dispatch]
+    );
+
+    const handleShieldPress = useCallback(() => {
+      prepareSaplingContractTransaction(
+        {
+          type: 'shielded',
+          params: [
+            {
+              to: saplingAddress,
+              amount: (shieldInputValue.amount ?? ZERO).toNumber(),
+              mutez: true
+            }
+          ]
+        },
+        'Failed to prepare shielded transaction',
+        response => {
+          goToSaplingConfirmation(shieldInputValue.amount ?? ZERO, response.txData, 'SHIELD_TRANSACTION_SENT');
+        }
+      );
+    }, [prepareSaplingContractTransaction, saplingAddress, shieldInputValue.amount, goToSaplingConfirmation]);
+
+    const handleSaplingTxPress = useCallback(() => {
+      prepareSaplingContractTransaction(
+        {
+          type: 'sapling',
+          params: [
+            {
+              to: saplingDestinationAddress,
+              amount: (saplingInputValue.amount ?? ZERO).toNumber(),
+              mutez: true
+            }
+          ]
+        },
+        'Failed to prepare sapling transaction',
+        response => {
+          goToSaplingConfirmation(ZERO, response.txData, 'SAPLING_TRANSACTION_SENT');
+        }
+      );
+    }, [
+      goToSaplingConfirmation,
+      prepareSaplingContractTransaction,
+      saplingDestinationAddress,
+      saplingInputValue.amount
+    ]);
+
+    const handleUnshieldPress = useCallback(() => {
+      prepareSaplingContractTransaction(
+        {
+          type: 'unshielded',
+          params: {
+            to: account.publicKeyHash,
+            amount: (unshieldInputValue.amount ?? ZERO).toNumber(),
+            mutez: true
+          }
+        },
+        'Failed to prepare unshielded transaction',
+        response => {
+          goToSaplingConfirmation(ZERO, response.txData, 'UNSHIELD_TRANSACTION_SENT');
+        }
+      );
+    }, [account.publicKeyHash, goToSaplingConfirmation, prepareSaplingContractTransaction, unshieldInputValue.amount]);
 
     const unshieldedBalance = useMemo(
       () =>
@@ -168,31 +323,14 @@ const SaplingPageContent = memo(
 
     useInterval(
       () => {
-        const sub = webViewMessageSubject.subscribe(event => {
-          sub.unsubscribe();
-          try {
-            const data = JSON.parse(event.nativeEvent.data);
-            if ('error' in data) {
-              throw new Error(data.error);
-            }
-            setAccountState(data);
-          } catch (err) {
-            console.error(err);
-            showErrorToast({
-              title: 'Failed to get account state',
-              description: err instanceof Error ? err.message : 'Unknown error'
-            });
-          }
-        });
-        webViewRef.current!.postMessage(
-          JSON.stringify({
-            method: 'accountState',
-            payload: { viewingKey, saplingContract, rpcUrl }
-          })
+        doSaplingProxyOperation(
+          { method: 'accountState', payload: { viewingKey, saplingContract, rpcUrl } },
+          'Failed to get account state',
+          setAccountState
         );
       },
       10000,
-      [],
+      [doSaplingProxyOperation, viewingKey, saplingContract, rpcUrl],
       true
     );
 
@@ -229,11 +367,54 @@ const SaplingPageContent = memo(
           </View>
         </View>
         {unshieldedBalance ? <Text>Unshielded balance: {unshieldedBalance} TEZ</Text> : null}
-        {!!accountState?.shieldedBalance && (
-          <Text>
-            Shielded balance:{' '}
-            {mutezToTz(new BigNumber(accountState.shieldedBalance), TEZ_TOKEN_METADATA.decimals).toFixed()} TEZ
-          </Text>
+        {!!accountState && (
+          <>
+            <Divider size={formatSize(12)} />
+
+            <AssetAmountInput
+              value={shieldInputValue}
+              label="Shield"
+              assetsList={shieldAmountInputAssetsList}
+              balanceLabel="TEZ balance:"
+              toUsdToggle={false}
+              isSingleAsset={true}
+              onValueChange={setShieldInputValue}
+            />
+            <ButtonLargePrimary title="Shield" onPress={handleShieldPress} />
+
+            <Divider size={formatSize(12)} />
+
+            <AssetAmountInput
+              balance={accountState.shieldedBalance}
+              value={saplingInputValue}
+              label="Sapling"
+              assetsList={unshieldAmountInputAssetsList}
+              balanceLabel="Shielded TEZ balance:"
+              toUsdToggle={false}
+              isSingleAsset={true}
+              onValueChange={setSaplingInputValue}
+            />
+            <AddressInput
+              value={saplingDestinationAddress}
+              placeholder="Destination address"
+              onChangeText={setSaplingDestinationAddress}
+            />
+            <ButtonLargePrimary title="Sapling" onPress={handleSaplingTxPress} />
+
+            <Divider size={formatSize(12)} />
+
+            <AssetAmountInput
+              balance={accountState.shieldedBalance}
+              value={unshieldInputValue}
+              label="Unshield"
+              assetsList={unshieldAmountInputAssetsList}
+              balanceLabel="Shielded TEZ balance:"
+              toUsdToggle={false}
+              isSingleAsset={true}
+              onValueChange={setUnshieldInputValue}
+            />
+            <ButtonLargePrimary title="Unshield" onPress={handleUnshieldPress} />
+          </>
         )}
       </>
     );
