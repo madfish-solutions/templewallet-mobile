111e1ad77f298927d389db9e322fc316451c1775
lendihop <mishin.danil2017@gmail.com>
2026-05-20 17:06:22 +0200

    TW-2254: fix android build

--- postinstall.sh
+++ postinstall.sh
@@ -1,8 +1,16 @@
-if [ "$(expr substr $(uname -s) 1 5)" != "Linux" ]; then
+if [ "$(uname -s)" != "Linux" ]; then
   sed_mac_arg=true
 fi
 
 find node_modules -type f -name 'build.gradle' -exec sed -i ${sed_mac_arg:+""} 's/jcenter()/mavenCentral()/g' {} +
+
+# https://github.com/facebook/react-native/issues/56287
+# Gradle 9 removed JvmVendorSpec.IBM_SEMERU. React Native 0.85.3 ships Foojay
+# resolver 0.5.0, which still references it and crashes during Android builds.
+rn_gradle_plugin_settings="node_modules/@react-native/gradle-plugin/settings.gradle.kts"
+if [ -f "$rn_gradle_plugin_settings" ]; then
+  sed -i ${sed_mac_arg:+""} 's/org.gradle.toolchains.foojay-resolver-convention").version("0.5.0")/org.gradle.toolchains.foojay-resolver-convention").version("1.0.0")/' "$rn_gradle_plugin_settings"
+fi
 
 search_string="compile 'com.facebook.react:react-native:+'"
 replace_string="implementation 'com.facebook.react:react-native:+'"
