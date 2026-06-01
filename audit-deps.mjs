41903fc826399ca88f195ddce4f27fbf1fc1cc24
lendihop <mishin.danil2017@gmail.com>
2026-06-01 20:14:34 +0200

    TW-2294: adjust audit check

--- audit-deps.mjs
+++ audit-deps.mjs
@@ -1,16 +1,8 @@
 import { exec } from 'child_process';
 
-exec('yarn npm audit  --severity high', (error, stdout) => {
+exec('yarn npm audit --severity high', (error, stdout) => {
   if (error) {
     console.log(stdout);
-
-    if (
-      (stdout.includes('Axios is vulnerable to DoS attack through lack of data size') ||
-        stdout.includes('minimatch')) &&
-      !stdout.includes('critical')
-    ) {
-      return;
-    }
 
     if (stdout.includes('high') || stdout.includes('critical')) {
       throw new Error('Audit failed');
