91e3013308566687db602680782c4f0ef885e472
Illia Pospielov <illia.pospielov@gmail.com>
2026-08-25 18:56:19 +0300

    TW-2302: remove legacy tezos activity pipeline

--- src/components/activity-groups-list/activity-group-item/activity-time/activity-time.styles.ts
+++
@@ -1,8 +0,0 @@
-import { createUseStyles } from 'src/styles/create-use-styles';
-
-export const useActivityTimeStyles = createUseStyles(({ colors, typography }) => ({
-  text: {
-    ...typography.numbersRegular11,
-    color: colors.gray2
-  }
-}));
