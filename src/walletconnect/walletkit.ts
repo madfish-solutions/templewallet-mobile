0465a87347902b8f957e11ca05148d8c9f5ff366
Inokentii Mazhara <keshan3262@gmail.com>
2026-08-26 08:55:05 +0300

    TW-2296 Change wallet icon URL for WC

--- src/walletconnect/walletkit.ts
+++ src/walletconnect/walletkit.ts
@@ -11,7 +11,9 @@ export const walletKitPromise = WalletKit.init({
     name: 'Temple Wallet Mobile',
     description: 'Dedicated mobile app to cover all your Tezos and EVM DeFi needs',
     url: 'www.templewallet.com',
-    icons: ['https://ipfs.io/ipfs/bafkreigwikllka4svf345zilxj6lq6glpe4djsqummmjfhigszwqaufjum'],
+    icons: [
+      'https://emerald-emotional-sailfish-309.mypinata.cloud/ipfs/bafkreigwikllka4svf345zilxj6lq6glpe4djsqummmjfhigszwqaufjum'
+    ],
     redirect: {
       native: 'temple://',
       universal: UNIVERSAL_LINKS_DOMAIN_URI_PREFIX
