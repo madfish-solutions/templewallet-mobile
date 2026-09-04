c73aac985257671f9f1447833442f4fbf01fb2dd
lendihop <mishin.danil2017@gmail.com>
2026-09-04 16:27:35 +0200

    TW-2359: fix lint

--- src/screens/manage-accounts/manage-accounts.styles.ts
+++ src/screens/manage-accounts/manage-accounts.styles.ts
@@ -1,7 +1,7 @@
 import { createUseStyles } from 'src/styles/create-use-styles';
 import { formatSize } from 'src/styles/format-size';
 
-export const useManageAccountsStyles = createUseStyles(({ colors }) => ({
+export const useManageAccountsStyles = createUseStyles(() => ({
   segmentControlContainer: {
     padding: formatSize(2),
     marginHorizontal: formatSize(16),
