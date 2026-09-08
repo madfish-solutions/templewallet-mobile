823a307450df0d3383b7b5692b3e3559e9ffe016
lendihop <mishin.danil2017@gmail.com>
2026-09-08 12:37:27 +0200

    TW-2359: refactoring + UI/UX adjustments

--- src/modals/reveal-seed-phrase-modal/reveal-seed-phrase-modal.tsx
+++ src/modals/reveal-seed-phrase-modal/reveal-seed-phrase-modal.tsx
@@ -1,15 +1,68 @@
 import React from 'react';
 
+import { AccountCard } from 'src/components/account-card';
+import { CryptoLogoNameEnum } from 'src/components/crypto-logo/logo-name.enum';
+import { DisclaimerV2 } from 'src/components/disclaimer/disclaimer';
+import { Divider } from 'src/components/divider/divider';
+import { Label } from 'src/components/label/label';
+import { ModalStatusBar } from 'src/components/modal-status-bar/modal-status-bar';
+import { ScreenContainer } from 'src/components/screen-container/screen-container';
 import { ModalsEnum } from 'src/navigator/enums/modals.enum';
 import { useHDAccounts } from 'src/store/wallet/wallet-selectors';
+import { formatSize } from 'src/styles/format-size';
+import { getAccountAddressForEvm, getAccountAddressForTezos } from 'src/utils/account.utils';
 import { usePageAnalytic } from 'src/utils/analytics/use-analytics.hook';
+import { getEvmDerivationPath, getTezosDerivationPath } from 'src/utils/keys.utils';
 
-import { RevealSeedPhraseContent } from './reveal-seed-phrase-content';
+import { CopyableDerivationPath } from './reveal-seed-phrase-form-content/copyable-derivation-path';
+import { RevealSeedPhraseView } from './reveal-seed-phrase-form-content/reveal-seed-phrase-view/reveal-seed-phrase-view';
+import { RevealSeedPharaseSelectors } from './reveal-seed-phrase.selectors';
 
 export const RevealSeedPhraseModal = () => {
-  const hdAccounts = useHDAccounts();
+  const account = useHDAccounts()[0];
+
+  const tezosAddress = getAccountAddressForTezos(account)!;
+  const evmAddress = getAccountAddressForEvm(account)!;
 
   usePageAnalytic(ModalsEnum.RevealSeedPhrase);
 
-  return <RevealSeedPhraseContent account={hdAccounts[0]} />;
+  return (
+    <ScreenContainer>
+      <ModalStatusBar />
+      <Divider size={formatSize(8)} />
+      <Label label="Account" description="Reveal a seed phrase from your HD account" />
+      <AccountCard account={account} showAllAddresses />
+      <Divider size={formatSize(24)} />
+
+      <Label
+        label="Derivation path"
+        description="This is key hierarchy blueprint to regenerate account from seed phrase."
+      />
+      <CopyableDerivationPath
+        value={getTezosDerivationPath(account.hdIndex)}
+        network={CryptoLogoNameEnum.Tezos}
+        testID={RevealSeedPharaseSelectors.tezosDerivationPath}
+      />
+      <Divider size={formatSize(16)} />
+      <CopyableDerivationPath
+        value={getEvmDerivationPath(account.hdIndex)}
+        network={CryptoLogoNameEnum.Etherlink}
+        testID={RevealSeedPharaseSelectors.evmDerivationPath}
+      />
+      <Divider size={formatSize(24)} />
+
+      <DisclaimerV2
+        title="Attention!"
+        texts={['DO NOT share this set of chars with anyone!', 'It can be used to steal your current account.']}
+      />
+      <Divider size={formatSize(16)} />
+
+      <Label
+        label="Seed phrase"
+        description="Access to your wallet if you switch browser or device. Keep it in secret."
+      />
+      <RevealSeedPhraseView publicKeyHash={tezosAddress ?? evmAddress ?? ''} />
+      <Divider size={formatSize(24)} />
+    </ScreenContainer>
+  );
 };
--- src/screens/manage-accounts/manage-hd-accounts/manage-account-item/index.tsx
+++ src/screens/manage-accounts/manage-hd-accounts/manage-account-item/index.tsx
@@ -1,18 +1,22 @@
 import React, { FC } from 'react';
 import { View } from 'react-native';
-import { useDispatch } from 'react-redux';
 
 import { AccountCard } from 'src/components/account-card';
 import { ButtonSmallSecondary } from 'src/components/button/button-small/button-small-secondary/button-small-secondary';
 import { Switch } from 'src/components/switch/switch';
 import { Account } from 'src/interfaces/account.interfaces';
+import { dispatch } from 'src/store';
 import { setAccountVisibility } from 'src/store/wallet/wallet-actions';
 import { useIsAccountVisibleSelector } from 'src/store/wallet/wallet-selectors';
 import { showWarningToast } from 'src/toast/toast.utils';
-import { getAccountAddressForTezos } from 'src/utils/account.utils';
 
 import { ManageAccountItemSelectors } from './selectors.ts';
 import { useManageAccountItemStyles } from './styles.ts';
+
+const VISIBILITY_WARNING = {
+  title: 'Could not hide your selected account',
+  description: 'Switch to another account and try again'
+};
 
 interface Props {
   account: Account;
@@ -21,27 +25,16 @@ interface Props {
 }
 
 export const ManageAccountItem: FC<Props> = ({ account, selectedAccount, onManageButtonPress }) => {
-  const dispatch = useDispatch();
   const styles = useManageAccountItemStyles();
 
-  const tezosAddress = getAccountAddressForTezos(account);
   const isVisible = useIsAccountVisibleSelector(account.id) ?? true;
 
-  const isVisibilitySwitchDisabled = !tezosAddress || account.id === selectedAccount.id;
-  const visibilityWarning = tezosAddress
-    ? {
-        title: 'Could not hide your selected account',
-        description: 'Switch to another account and try again'
-      }
-    : {
-        title: 'Could not hide this account',
-        description: 'EVM-only accounts stay visible until EVM account management is supported'
-      };
+  const isVisibilitySwitchDisabled = account.id === selectedAccount.id;
 
   return (
     <AccountCard
-      account={account}
       showAllAddresses
+      account={account}
       detailsContainerStyle={styles.accountDetails}
       footer={
         <View style={styles.footer}>
@@ -50,7 +43,7 @@ export const ManageAccountItem: FC<Props> = ({ account, selectedAccount, onManag
             onPress={() => onManageButtonPress(account)}
             testID={ManageAccountItemSelectors.manageButton}
           />
-          <View onTouchStart={() => void (isVisibilitySwitchDisabled && showWarningToast(visibilityWarning))}>
+          <View onTouchStart={() => void (isVisibilitySwitchDisabled && showWarningToast(VISIBILITY_WARNING))}>
             <Switch
               value={isVisible}
               disabled={isVisibilitySwitchDisabled}
