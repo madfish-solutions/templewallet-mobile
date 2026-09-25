be4f119eabc7d324c91889bef3e4fa7358c11564
lendihop <mishin.danil2017@gmail.com>
2026-09-25 13:20:26 +0200

    TW-2420: fix unlock by fingerprint if more than 3 attempts

--- src/hooks/use-password-delay.hook.ts
+++ src/hooks/use-password-delay.hook.ts
@@ -1,8 +1,13 @@
+import { useMemo } from 'react';
+
 import { CONSTANT_DELAY_TIME, MAX_PASSWORD_ATTEMPTS, RANDOM_DELAY_TIME } from '../config/security';
 import { usePasswordAttempt } from '../store/security/security-selectors';
 
 export const usePasswordDelay = () => {
   const attempt = usePasswordAttempt();
 
-  return attempt > MAX_PASSWORD_ATTEMPTS ? Math.random() * RANDOM_DELAY_TIME + CONSTANT_DELAY_TIME : 0;
+  return useMemo(
+    () => (attempt > MAX_PASSWORD_ATTEMPTS ? Math.random() * RANDOM_DELAY_TIME + CONSTANT_DELAY_TIME : 0),
+    [attempt]
+  );
 };
