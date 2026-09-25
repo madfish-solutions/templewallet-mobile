0d831ae48a561618a2e7def6fb2bbe5f58182e86
lendihop <mishin.danil2017@gmail.com>
2026-09-25 15:17:14 +0200

    TW-2420: if no uri or it is loading - show fallback

--- src/modals/collectible-modal/components/collection-image/index.tsx
+++ src/modals/collectible-modal/components/collection-image/index.tsx
@@ -1,12 +1,12 @@
 import FastImage from '@d11/react-native-fast-image';
-import React, { FC, useEffect, useState } from 'react';
-import { Image, StyleSheet } from 'react-native';
+import React, { FC, useCallback, useState } from 'react';
+import { Image, StyleSheet, View } from 'react-native';
 import { SvgUri, SvgXml } from 'react-native-svg';
 
 import { CryptoLogo } from 'src/components/crypto-logo';
 import { CryptoLogoNameEnum } from 'src/components/crypto-logo/logo-name.enum.ts';
 import { formatSize } from 'src/styles/format-size.ts';
-import { formatImgUri, isSvgDataUriInBase64Encoding } from 'src/utils/image.utils.ts';
+import { formatImgUri, isImgUriSvg, isSvgDataUriInBase64Encoding } from 'src/utils/image.utils.ts';
 
 import { COLLECTION_ICON_SIZE } from '../../constants.ts';
 
@@ -17,8 +17,10 @@ interface Props {
 }
 
 export const CollectionImage: FC<Props> = ({ uri }) => {
-  const [isFailed, setIsFailed] = useState(!uri);
-  const handleError = () => setIsFailed(true);
+  const [loadedUri, setLoadedUri] = useState<string | null>(null);
+  const [failedUri, setFailedUri] = useState<string | null>(null);
+  const handleLoad = useCallback(() => setLoadedUri(uri ?? null), [uri]);
+  const handleError = useCallback(() => setFailedUri(uri ?? null), [uri]);
   const fallback = (
     <CryptoLogo
       name={CryptoLogoNameEnum.CollectiblePlaceholder}
@@ -28,41 +30,62 @@ export const CollectionImage: FC<Props> = ({ uri }) => {
     />
   );
 
-  useEffect(() => setIsFailed(!uri), [uri]);
-
-  if (!uri || isFailed) {
+  if (!uri || failedUri === uri) {
     return fallback;
-  }
-
-  if (uri.endsWith('.svg')) {
-    return <SvgUri uri={uri} height={size} width={size} style={styles.logo} fallback={fallback} />;
   }
 
   if (isSvgDataUriInBase64Encoding(uri)) {
     const base64Data = uri.replace(/^data:image\/svg\+xml;base64,/, '');
     const svgXml = Buffer.from(base64Data, 'base64').toString('utf8');
 
-    return <SvgXml xml={svgXml} width={size} height={size} style={styles.logo} fallback={fallback} />;
+    return (
+      <View style={styles.container}>
+        <SvgXml xml={svgXml} width={size} height={size} style={styles.logo} fallback={fallback} />
+      </View>
+    );
+  }
+
+  const imageStyle = [styles.logo, loadedUri !== uri && styles.hidden];
+
+  if (isImgUriSvg(uri)) {
+    return (
+      <View style={styles.container}>
+        {loadedUri !== uri && fallback}
+        <SvgUri uri={uri} height={size} width={size} style={imageStyle} onLoad={handleLoad} onError={handleError} />
+      </View>
+    );
   }
 
   const formattedUri = formatImgUri(uri);
 
-  return formattedUri == null ? (
-    <Image source={{ uri }} style={styles.logo} onError={handleError} />
-  ) : (
-    <FastImage source={{ uri: formattedUri }} style={styles.logo} onError={handleError} />
+  return (
+    <View style={styles.container}>
+      {loadedUri !== uri && fallback}
+      {formattedUri == null ? (
+        <Image source={{ uri }} style={imageStyle} onLoad={handleLoad} onError={handleError} />
+      ) : (
+        <FastImage source={{ uri: formattedUri }} style={imageStyle} onLoad={handleLoad} onError={handleError} />
+      )}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    height: size,
+    width: size,
+    marginRight: formatSize(8)
+  },
   logo: {
     height: size,
     width: size,
-    marginRight: formatSize(8),
-    borderRadius: formatSize(8)
+    borderRadius: formatSize(8),
+    position: 'absolute'
+  },
+  hidden: {
+    opacity: 0
   },
   fallback: {
-    margin: 0,
     marginRight: formatSize(8)
   }
 });
